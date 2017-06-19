6d5245a3d7447e82a5919f86d47df1979c9fae1a
Avishai Dernis <avid29@live.com>
2017-06-19 08:22:47 +0000

    Revert "Deleted CacheModels"

---
+++ CacheModels/DMCache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_UWP.CacheModels
+{
+    public class DmCache
+    {
+        public DmCache(TempDmCache input)
+        {
+            Raw = input.Raw;
+            foreach (TempMessage message in input.Messages)
+            {
+                Messages.Add(message.Id, new Message(message));
+            }
+        }
+
+        public DmCache(SharedModels.DirectMessageChannel input)
+        {
+            Raw = input;
+        }
+
+
+        public SharedModels.DirectMessageChannel Raw = new SharedModels.DirectMessageChannel();
+
+        public Dictionary<string, Message> Messages = new Dictionary<string, Message>();
+    }
+}
