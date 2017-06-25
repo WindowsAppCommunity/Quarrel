90fd2bd3c2a96ddbf58183a26d64d7e2698d627d
Avishai Dernis <avid29@live.com>
2017-06-25 18:47:28 +0100

    Added User status updates

--- DiscordAPI/Gateway/UpstreamEvents/StatusUpdate.cs
+++ DiscordAPI/Gateway/UpstreamEvents/StatusUpdate.cs
@@ -4,18 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord_UWP.SharedModels;
 
 namespace Discord_UWP.Gateway.UpstreamEvents
 {
 
     public struct StatusUpdate
     {
-
         [JsonProperty("status")]
         public string Status { get; set; }
-        [JsonProperty("idle_since")]
-        public int? IdleSince { get; set; }
+        [JsonProperty("since")]
+        public long? IdleSince { get; set; }
+        [JsonProperty("afk")]
+        public bool IsAFK { get; set; }
         [JsonProperty("game")]
-        public Discord_UWP.SharedModels.Game? Game { get; set; }
+        public Game? Game { get; set; }
     }
 }
