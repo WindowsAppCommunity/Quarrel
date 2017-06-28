50983c4471367257c27faf33c09412f4a35bc2ec
Avishai Dernis <avid29@live.com>
2017-06-28 10:43:16 +0100

    Added some loading messages

--- EntryMessages.cs
+++ EntryMessages.cs
@@ -11,14 +11,25 @@ namespace Discord_UWP
         public static string GetMessage()
         {
             Random rand = new Random();
-            switch (rand.Next(0, 5))
+            switch (rand.Next(0, 8))
+            //switch (x)
             {
                 case 0:
                     return "HUZZAH";
                 case 1:
                     return "Beam me up Scottie!!!";
                 case 3:
-                    return "";
+                    return "Catch Phrase";
+                case 4:
+                    return "Microsoft's working on Windows Mobile !?!?!? (Submitted by @Dikas#8802)";
+                case 5:
+                    return "Splash screen* (Submitted by @ToonWK#5841)";
+                case 6:
+                    return "I don't mean to be rude but...";
+                case 7:
+                    return "Food doesn't grow on trees";
+                case 8:
+                    return "😬";
                 default:
                     return "Waiting for Ready Packet";
             }
