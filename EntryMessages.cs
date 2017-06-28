958f370fa15728e0842509d370d1c477ccdb34f7
Avishai Dernis <avid29@live.com>
2017-06-28 10:21:55 +0100

    Editing loading preview

---
+++ EntryMessages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_UWP
+{
+    class EntryMessages
+    {
+        string GetMessage()
+        {
+            Random rand = new Random();
+            switch (rand.Next(0, 5))
+            {
+                case 0:
+                    return "HUZZAH";
+                case 1:
+                    return "Beam me up Scottie!!!";
+                case 3:
+                    return "";
+                default:
+                    return "Waiting for Ready Packet";
+            }
+        }
+    }
+}
