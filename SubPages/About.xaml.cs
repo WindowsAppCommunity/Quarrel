2a127b4f408255289369cea3b0c29f851850a69f
Avishai Dernis <avid29@live.com>
2017-07-13 10:43:28 -0400

    Fixed even more permissions

--- SubPages/About.xaml.cs
+++ SubPages/About.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -48,6 +38,11 @@ namespace Discord_UWP.SubPages
             scale.CenterX = this.ActualWidth / 2;
             NavAway.Begin();
             App.SubpageClosed();
+        }
+
+        private void joinDiscordUWPServer(object sender, RoutedEventArgs e)
+        {
+            Session.AcceptInvite("wQmQgtq");
         }
     }
 }
