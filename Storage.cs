a75a5c3c12ce8034de06325623fe6172be9bb3be
Avishai Dernis <avid29@live.com>
2017-09-25 00:41:44 -0400

    Added (unimplimented) settings

--- Storage.cs
+++ Storage.cs
@@ -9,14 +9,35 @@ namespace Discord_UWP
 {
     public static class Storage
     {
-        //public static Settings Settings = new Settings(); //this just represents the storage
-        //public static ApplicationDataContainer SavedSettings = ApplicationData.Current.LocalSettings;
+        public static Settings Settings = new Settings(); //this just represents the storage
+        public static ApplicationDataContainer SavedSettings = ApplicationData.Current.LocalSettings;
         public static LocalState State = new LocalState();
         public static PasswordVault PasswordVault = new PasswordVault();
     }
 
+    public enum Theme { Dark, Light, Windows, Discord }
     public class Settings
     {
-        Tuple<string, User> ActiveUser;
+        Tuple<string, User> ActiveUser; public bool LockChannels = false;
+        public bool AutoHideChannels = true;
+        public bool AutoHidePeople = false;
+        public bool HighlightEveryone = true;
+        public bool FriendsNotifyDMs = true;
+        public bool FriendsNotifyFriendRequest = false;
+        //public bool FriendsNotifyIncoming = true;
+        //public bool FriendsNotifyOutgoing = false;
+        public bool Toasts = false;
+        public double RespUiM = 569;
+        public double RespUiL = 768;
+        public double RespUiXl = 1024;
+        public bool AppBarAtBottom = false;
+        public bool ExpensiveRender = false;
+        public bool ShowOfflineMembers = false;
+        public bool DevMode = false;
+        public bool DiscordLightTheme = false;
+        public bool CompactMode = false;
+        public Theme Theme = Theme.Dark;
+        public bool AccentBrush = false; //If false use Blurple if true use System.AccentColor
+        public bool Vibrate = false;
     }
 }
