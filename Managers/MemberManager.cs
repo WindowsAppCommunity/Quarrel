f25c6cbd118f1cfb7981a8850a3eaeca1acbc76f
Avishai Dernis <avid29@live.com>
2017-09-29 23:12:54 -0400

    Clumsy MemberList

--- Managers/MemberManager.cs
+++ Managers/MemberManager.cs
@@ -5,14 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Media;
 
+using Discord_UWP.LocalModels;
+using Discord_UWP.SharedModels;
+
 namespace Discord_UWP.Managers
 {
     class MemberManager
     {
+
+
+        private static List<DisplayedRole> TempRoleCache = new List<DisplayedRole>(); //This is as a temporary cache of roles to improve performance and not call Storage for every member
+        public static DisplayedRole GetRole(string roleid, string guildid, int everyonecounter)
+        {
+            var cachedRole = TempRoleCache.FirstOrDefault(x => x.Id == roleid);
+            if (cachedRole != null) return cachedRole;
+            else
+            {
+                DisplayedRole role;
+                if (roleid == null || !LocalState.Guilds[guildid].roles[roleid].Hoist)
+                {
+
+                    role = new DisplayedRole(null, 0, App.GetString("/Main/Everyone"), everyonecounter, (SolidColorBrush)App.Current.Resources["Foreground"]);
+                    TempRoleCache.Add(role);
+                }
+                else
+                {
+                    var storageRole = LocalState.Guilds[guildid].roles[roleid];
+                    role = new DisplayedRole(roleid, storageRole.Position, storageRole.Name.ToUpper(), storageRole.MemberCount, Common.IntToColor(storageRole.Color));
+                    TempRoleCache.Add(role);
+                }
+                return role;
+            }
+        }
     }
 
     public class Member
     {
+        public Member(SharedModels.GuildMember input)
+        {
+            Raw = input;
+            //avatar = new ImageBrush() { ImageSource = new BitmapImage(new Uri("https://cdn.discordapp.com/avatars/" + input.User.Id + "/" + input.User.Avatar + ".jpg")) };
+        }
+
         public SharedModels.GuildMember Raw;
 
         public SharedModels.Role HighRole = new SharedModels.Role();
