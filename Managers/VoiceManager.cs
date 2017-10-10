aefade823a34db9d2ad27642fb35193d9021387a
Avishai Dernis <avid29@live.com>
2017-10-09 21:08:11 -0400

    Added VoiceConnection to voice manager

--- Managers/VoiceManager.cs
+++ Managers/VoiceManager.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Discord_UWP.Voice;
+
 namespace Discord_UWP.Managers
 {
     public class VoiceManager
     {
-
+        public static VoiceConnection VoiceConnection;
     }
 }
