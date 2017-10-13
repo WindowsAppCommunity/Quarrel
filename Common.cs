eaf94374b28cbb63f0fd3955b3869b05d773b622
karmaecrivain94 <lightmagicapps@outlook.com>
2017-10-13 15:55:37 +0100

    Started adding infinite scrolling (no need to pull for refresh)

--- Common.cs
+++ Common.cs
@@ -11,6 +11,8 @@ using Windows.UI;
 using Windows.UI.Xaml.Media;
 using Discord_UWP.SharedModels;
 using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
 
 namespace Discord_UWP
 {
@@ -109,6 +111,30 @@ namespace Discord_UWP
                 return new Uri("ms-appx:///Assets/DiscordAssets/default_avatar.png");
             else
                 return new Uri("https://cdn.discordapp.com/avatars/" + userid + "/" + s + ".jpg" + suffix);
+        }
+
+        public static ScrollViewer GetScrollViewer(DependencyObject o)
+        {
+            // Return the DependencyObject if it is a ScrollViewer
+            if (o is ScrollViewer)
+            {
+                return o as ScrollViewer;
+            }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(o); i++)
+            {
+                var child = VisualTreeHelper.GetChild(o, i);
+                var result = GetScrollViewer(child);
+                if (result == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+            return null;
         }
     }
 }
