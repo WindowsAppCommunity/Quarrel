c031e26c157a02c98cd0c8e72c905d792446e11f
Avishai Dernis <avid29@live.com>
2017-12-28 12:36:49 -0500

    More Giphy

--- GiphyAPI/Models/Meta.cs
+++ GiphyAPI/Models/Meta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace GiphyAPI.Models
 {
-    struct Meta
+    public struct Meta
     {
+        [JsonProperty("msg")]
+        public string Message { get; set; }
+
+        [JsonProperty("status")]
+        public int Status { get; set; }
+
+        [JsonProperty("response_id")]
+        public string ResponseId { get; set; }
     }
 }
--- GiphyAPI/Models/SearchResult.cs
+++ GiphyAPI/Models/SearchResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,13 @@ namespace GiphyAPI.Models
 {
     public struct SearchResult
     {
+        [JsonProperty("data")]
+        public IEnumerable<Gif> Gif { get; set; }
 
+        [JsonProperty("pagination")]
+        public Pagination Pagination { get; set; }
+
+        [JsonProperty("meta")]
+        public Meta Meta { get; set; }
     }
 }
