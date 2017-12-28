886962a8c66aad20350bde08997d001a65b3708f
Avishai Dernis <avid29@live.com>
2017-12-28 12:36:49 -0500

    More Giphy

--- GiphyAPI/Models/Pagination.cs
+++ GiphyAPI/Models/Pagination.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace GiphyAPI.Models
 {
-    struct Pagination
+    public struct Pagination
     {
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+
+        [JsonProperty("total_count")]
+        public int TotalCount { get; set; }
+
+        [JsonProperty("count")]
+        public int Count { get; set; }
     }
 }
