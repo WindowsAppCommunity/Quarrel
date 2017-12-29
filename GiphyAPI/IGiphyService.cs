e2fa5ed9f1be6dfb203b2171207ac31b5cc5489a
Avishai Dernis <avid29@live.com>
2017-12-28 20:17:14 -0500

    Fixed Giphy API

--- GiphyAPI/IGiphyService.cs
+++ GiphyAPI/IGiphyService.cs
@@ -11,10 +11,10 @@ namespace GiphyAPI
 {
     public interface IGiphyService
     {
-        [Get("/v1/gifs/search?api_key={"+GiphyAPI.GiphyKey+"}&q={query}&limit={limit}&offset={offset}&fmt=json")]
+        [Get("/v1/gifs/search?api_key=erGe4TVabEDlDPOkHFc389gQPvx4ze9Z&q={query}&limit={limit}&offset={offset}&fmt=json")]
         Task<SearchResult> Search( [AliasAs("query")] string query, [AliasAs("limit")]int limit = 20, [AliasAs("offset")] int offset = 0);
 
-        [Get("/v1/gifs/trending&api_key={" + GiphyAPI.GiphyKey + "}&limit={limit}&offset={offset}")]
+        [Get("/v1/gifs/trending&api_key=erGe4TVabEDlDPOkHFc389gQPvx4ze9Z&limit={limit}&offset={offset}")]
         Task<SearchResult> Trending([AliasAs("limit")] int limit = 20, [AliasAs("offset")] int offset = 0);
     }
 }
