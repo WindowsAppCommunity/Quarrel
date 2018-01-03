81edf584403cceead587566157f5675f2336d9fd
Avishai Dernis <avid29@live.com>
2018-01-03 15:50:15 -0500

    Salsa work

--- Salsa20/Salsa20.cpp
+++ Salsa20/Salsa20.cpp
@@ -1,239 +1,161 @@
-﻿// Copyright (c) 2015 Nezametdinov E. Ildus
+// Copyright (c) 2015 Nezametdinov E. Ildus
 // See LICENSE.TXT for licensing details
 
-//TODO: Replace read file code with byte[] (byte*) to istream
-//TODO: Replace write file code with istream to byte[] (byte*)
-//TODO: Place in referncable namespace to access from DiscordAPI in C#
-
-#include "pch.h"
 #include "Salsa20.h"
 
-#include <iostream>
-#include <cstdlib>
-#include <fstream>
-#include <string>
-
-using namespace ucstk;
-
-/**
-* Represents program.
-*/
-class Salsa
+namespace Salsa20ns
 {
-public:
-	Salsa() : inputFileName_(), outputFileName_(), shouldShowHelp_(false)
+
+	Salsa20::Salsa20(const uint8_t* key)
 	{
-		std::memset(key_, 0, sizeof(key_));
-	}
-	Salsa(const Salsa&) = delete;
-	Salsa(Salsa&&) = delete;
-	~Salsa() {}
-	Salsa& operator =(const Salsa&) = delete;
-	Salsa& operator =(Salsa&&) = delete;
-
-	/**
-	* \brief Reads parameters from command line and validates them.
-	* \param[in] argc number of command line arguments passed
-	* \param[in] argv array of command line arguments
-	* \return true on success
-	*/
-	bool initialize(int argc, char* argv[])
-	{
-		std::string key;
-		shouldShowHelp_ = false;
-
-		for (int i = 0; i < argc; ++i)
-		{
-			std::string parameter = argv[i];
-
-			if (parameter == "-p")
-			{
-				if ((argc - i - 1) != 3)
-					break;
-
-				inputFileName_ = argv[++i];
-				outputFileName_ = argv[++i];
-				key = argv[++i];
-				break;
-			}
-
-			if (parameter == "-h")
-			{
-				shouldShowHelp_ = true;
-				return true;
-			}
-		}
-
-		if (inputFileName_.empty())
-		{
-			std::cout << "E: Input file name was not specified." << std::endl;
-			return false;
-		}
-
-		if (outputFileName_.empty())
-		{
-			std::cout << "E: Output file name was not specified." << std::endl;
-			return false;
-		}
-
-		if (inputFileName_ == outputFileName_)
-		{
-			std::cout << "E: Input and output files should be distinct." << std::endl;
-			return false;
-		}
-
-		if (key.empty())
-		{
-			std::cout << "E: Key was not specified." << std::endl;
-			return false;
-		}
-
-		if (!readKeyFromString(key))
-		{
-			std::cout << "E: Invalid key value." << std::endl;
-			return false;
-		}
-
-		return true;
+		std::memset(vector_, 0, sizeof(vector_));
+		setKey(key);
 	}
 
-	/**
-	* \brief Encrypts or decrypts the file.
-	* \return true on success
-	*/
-	bool execute()
+	//----------------------------------------------------------------------------------
+	void Salsa20::setKey(const uint8_t* key)
 	{
-		if (shouldShowHelp_)
-		{
-			std::cout << "Usage: salsa20 -p INPUT OUTPUT KEY" << std::endl;
-			std::cout << "       salsa20 -h" << std::endl;
-			std::cout << std::endl << "Salsa20 is a stream cypher (see http://cr.yp.to/snuffle.html).";
-			std::cout << std::endl << std::endl;
-			std::cout << "Options:" << std::endl;
-			std::cout << "  -h Shows this help text." << std::endl;
-			std::cout << "  -p Encrypts or decrypts file INPUT with KEY and outputs result to file OUTPUT.";
-			std::cout << std::endl;
-			std::cout << "     KEY is a 32-byte key concatenated with 8-byte IV written in HEX.";
-			std::cout << std::endl;
-			return true;
-		}
+		static const char constants[] = "expand 32-byte k";
 
-		std::ifstream inputStream(inputFileName_, std::ios_base::binary);
-		if (!inputStream)
-		{
-			std::cout << "E: Could not open input file." << std::endl;
-			return false;
-		}
+		if (key == nullptr)
+			return;
 
-		std::ofstream outputStream(outputFileName_, std::ios_base::binary);
-		if (!outputStream)
-		{
-			std::cout << "E: Could not create output file." << std::endl;
-			return false;
-		}
+		vector_[0] = convert(reinterpret_cast<const uint8_t*>(&constants[0]));
+		vector_[1] = convert(&key[0]);
+		vector_[2] = convert(&key[4]);
+		vector_[3] = convert(&key[8]);
+		vector_[4] = convert(&key[12]);
+		vector_[5] = convert(reinterpret_cast<const uint8_t*>(&constants[4]));
 
-		const auto chunkSize = NUM_OF_BLOCKS_PER_CHUNK * Salsa20::BLOCK_SIZE;
-		uint8_t chunk[chunkSize];
+		std::memset(&vector_[6], 0, 4 * sizeof(uint32_t));
 
-		// determine size of the file
-		inputStream.seekg(0, std::ios_base::end);
-		auto fileSize = inputStream.tellg();
-		inputStream.seekg(0, std::ios_base::beg);
-
-		// compute number of chunks and size of the remainder
-		auto numChunks = fileSize / chunkSize;
-		auto remainderSize = fileSize % chunkSize;
-
-		// process file
-		Salsa20 salsa20(key_);
-		salsa20.setIv(&key_[IV_OFFSET]);
-		std::cout << "Processing file \"" << inputFileName_ << '"' << std::endl;
-
-		for (decltype(numChunks) i = 0; i < numChunks; ++i)
-		{
-			inputStream.read(reinterpret_cast<char*>(chunk), sizeof(chunk));
-			salsa20.processBlocks(chunk, chunk, NUM_OF_BLOCKS_PER_CHUNK);
-			outputStream.write(reinterpret_cast<const char*>(chunk), sizeof(chunk));
-
-			float percentage = 100.0f * static_cast<float>(i + 1) / static_cast<float>(numChunks);
-			std::printf("[%3.2f]\r", percentage);
-		}
-
-		if (remainderSize != 0)
-		{
-			inputStream.read(reinterpret_cast<char*>(chunk), remainderSize);
-			salsa20.processBytes(chunk, chunk, remainderSize);
-			outputStream.write(reinterpret_cast<const char*>(chunk), remainderSize);
-			std::cout << "[100.00]";
-		}
-
-		std::cout << std::endl << "OK" << std::endl;
-		return true;
+		vector_[10] = convert(reinterpret_cast<const uint8_t*>(&constants[8]));
+		vector_[11] = convert(&key[16]);
+		vector_[12] = convert(&key[20]);
+		vector_[13] = convert(&key[24]);
+		vector_[14] = convert(&key[28]);
+		vector_[15] = convert(reinterpret_cast<const uint8_t*>(&constants[12]));
 	}
 
-private:
-	/// Helper constants
-	enum : size_t
+	//----------------------------------------------------------------------------------
+	void Salsa20::setIv(const uint8_t* iv)
 	{
-		NUM_OF_BLOCKS_PER_CHUNK = 8192,
-		IV_OFFSET = Salsa20::KEY_SIZE,
-		KEY_SIZE = Salsa20::KEY_SIZE + Salsa20::IV_SIZE
-	};
+		if (iv == nullptr)
+			return;
 
-	/**
-	* \brief Reads byte from string.
-	* \param[in] string string
-	* \param[out] byte byte
-	* \return true on success
-	*/
-	bool readByte(const char* string, uint8_t& byte)
-	{
-		byte = 0;
-
-		for (uint32_t i = 0; i < 2; ++i)
-		{
-			uint8_t value = 0;
-			char c = string[i];
-
-			if (c >= '0' && c <= '9')
-				value = c - '0';
-			else if (c >= 'A' && c <= 'F')
-				value = c - 'A' + 0x0A;
-			else if (c >= 'a' && c <= 'f')
-				value = c - 'a' + 0x0A;
-			else
-				return false;
-
-			byte |= (value << (4 - i * 4));
-		}
-
-		return true;
+		vector_[6] = convert(&iv[0]);
+		vector_[7] = convert(&iv[4]);
+		vector_[8] = vector_[9] = 0;
 	}
 
-	/**
-	* \brief Reads key from string.
-	* \param[in] string string
-	* \return true on success
-	*/
-	bool readKeyFromString(const std::string& string)
+	//----------------------------------------------------------------------------------
+	void Salsa20::generateKeyStream(uint8_t output[BLOCK_SIZE])
 	{
-		auto stringLength = string.length();
+		uint32_t x[VECTOR_SIZE];
+		std::memcpy(x, vector_, sizeof(vector_));
 
-		if (stringLength != 2 * KEY_SIZE)
-			return false;
-
-		for (decltype(stringLength) i = 0; i < stringLength; i += 2)
+		for (int32_t i = 20; i > 0; i -= 2)
 		{
-			if (!readByte(&string[i], key_[i / 2]))
-				return false;
+			x[4] ^= rotate(static_cast<uint32_t>(x[0] + x[12]), 7);
+			x[8] ^= rotate(static_cast<uint32_t>(x[4] + x[0]), 9);
+			x[12] ^= rotate(static_cast<uint32_t>(x[8] + x[4]), 13);
+			x[0] ^= rotate(static_cast<uint32_t>(x[12] + x[8]), 18);
+			x[9] ^= rotate(static_cast<uint32_t>(x[5] + x[1]), 7);
+			x[13] ^= rotate(static_cast<uint32_t>(x[9] + x[5]), 9);
+			x[1] ^= rotate(static_cast<uint32_t>(x[13] + x[9]), 13);
+			x[5] ^= rotate(static_cast<uint32_t>(x[1] + x[13]), 18);
+			x[14] ^= rotate(static_cast<uint32_t>(x[10] + x[6]), 7);
+			x[2] ^= rotate(static_cast<uint32_t>(x[14] + x[10]), 9);
+			x[6] ^= rotate(static_cast<uint32_t>(x[2] + x[14]), 13);
+			x[10] ^= rotate(static_cast<uint32_t>(x[6] + x[2]), 18);
+			x[3] ^= rotate(static_cast<uint32_t>(x[15] + x[11]), 7);
+			x[7] ^= rotate(static_cast<uint32_t>(x[3] + x[15]), 9);
+			x[11] ^= rotate(static_cast<uint32_t>(x[7] + x[3]), 13);
+			x[15] ^= rotate(static_cast<uint32_t>(x[11] + x[7]), 18);
+			x[1] ^= rotate(static_cast<uint32_t>(x[0] + x[3]), 7);
+			x[2] ^= rotate(static_cast<uint32_t>(x[1] + x[0]), 9);
+			x[3] ^= rotate(static_cast<uint32_t>(x[2] + x[1]), 13);
+			x[0] ^= rotate(static_cast<uint32_t>(x[3] + x[2]), 18);
+			x[6] ^= rotate(static_cast<uint32_t>(x[5] + x[4]), 7);
+			x[7] ^= rotate(static_cast<uint32_t>(x[6] + x[5]), 9);
+			x[4] ^= rotate(static_cast<uint32_t>(x[7] + x[6]), 13);
+			x[5] ^= rotate(static_cast<uint32_t>(x[4] + x[7]), 18);
+			x[11] ^= rotate(static_cast<uint32_t>(x[10] + x[9]), 7);
+			x[8] ^= rotate(static_cast<uint32_t>(x[11] + x[10]), 9);
+			x[9] ^= rotate(static_cast<uint32_t>(x[8] + x[11]), 13);
+			x[10] ^= rotate(static_cast<uint32_t>(x[9] + x[8]), 18);
+			x[12] ^= rotate(static_cast<uint32_t>(x[15] + x[14]), 7);
+			x[13] ^= rotate(static_cast<uint32_t>(x[12] + x[15]), 9);
+			x[14] ^= rotate(static_cast<uint32_t>(x[13] + x[12]), 13);
+			x[15] ^= rotate(static_cast<uint32_t>(x[14] + x[13]), 18);
 		}
 
-		return true;
+		for (size_t i = 0; i < VECTOR_SIZE; ++i)
+		{
+			x[i] += vector_[i];
+			convert(x[i], &output[4 * i]);
+		}
+
+		++vector_[8];
+		vector_[9] += vector_[8] == 0 ? 1 : 0;
 	}
 
-	// Data members
-	std::string inputFileName_, outputFileName_;
-	uint8_t key_[KEY_SIZE];
-	bool shouldShowHelp_;
+	//----------------------------------------------------------------------------------
+	void Salsa20::processBlocks(const uint8_t* input, uint8_t* output, size_t numBlocks)
+	{
+		assert(input != nullptr && output != nullptr);
 
-};
+		uint8_t keyStream[BLOCK_SIZE];
+
+		for (size_t i = 0; i < numBlocks; ++i)
+		{
+			generateKeyStream(keyStream);
+
+			for (size_t j = 0; j < BLOCK_SIZE; ++j)
+				*(output++) = keyStream[j] ^ *(input++);
+		}
+	}
+
+	//----------------------------------------------------------------------------------
+	void Salsa20::processBytes(const uint8_t* input, uint8_t* output, size_t numBytes)
+	{
+		assert(input != nullptr && output != nullptr);
+
+		uint8_t keyStream[BLOCK_SIZE];
+		size_t numBytesToProcess;
+
+		while (numBytes != 0)
+		{
+			generateKeyStream(keyStream);
+			numBytesToProcess = numBytes >= BLOCK_SIZE ? BLOCK_SIZE : numBytes;
+
+			for (size_t i = 0; i < numBytesToProcess; ++i, --numBytes)
+				*(output++) = keyStream[i] ^ *(input++);
+		}
+	}
+
+	//----------------------------------------------------------------------------------
+	uint32_t Salsa20::rotate(uint32_t value, uint32_t numBits)
+	{
+		return (value << numBits) | (value >> (32 - numBits));
+	}
+
+	//----------------------------------------------------------------------------------
+	void Salsa20::convert(uint32_t value, uint8_t* array)
+	{
+		array[0] = static_cast<uint8_t>(value >> 0);
+		array[1] = static_cast<uint8_t>(value >> 8);
+		array[2] = static_cast<uint8_t>(value >> 16);
+		array[3] = static_cast<uint8_t>(value >> 24);
+	}
+
+	//----------------------------------------------------------------------------------
+	uint32_t Salsa20::convert(const uint8_t* array)
+	{
+		return ((static_cast<uint32_t>(array[0]) << 0) |
+			(static_cast<uint32_t>(array[1]) << 8) |
+			(static_cast<uint32_t>(array[2]) << 16) |
+			(static_cast<uint32_t>(array[3]) << 24));
+	}
+
+}
