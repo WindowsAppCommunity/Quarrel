87eb24a3aa375bf7f4cd6a4c1a605a94b9884104
Avishai Dernis <avid29@live.com>
2018-01-06 14:59:22 -0500

    Pinvoke take 1

--- SodiumC/dllmain.cpp
+++ SodiumC/dllmain.cpp
@@ -1,5 +1,21 @@
 ﻿#include "pch.h"
 
+#define SodiumC_EXPORTS  
+#ifdef SodiumC_EXPORTS  
+#define SodiumC_API __declspec(dllexport)  
+#else  
+#define TRADITIONALDLL_API __declspec(dllimport)  
+#endif 
+//TODO: Basic function call
+
+extern "C" {
+	SodiumC_API int add(int a, int b);
+}
+
+ int add(int a, int b) {
+	return a + b;
+}
+
 //BOOL APIENTRY DllMain(HMODULE /* hModule */, DWORD ul_reason_for_call, LPVOID /* lpReserved */)
 //{
 //    switch (ul_reason_for_call)
