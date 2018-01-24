d97de5f49d16f826aa249d5e9a48e0a6387e46a6
Avishai Dernis <avid29@live.com>
2018-01-24 03:39:37 -0500

    What must be done to get 6.4.0 out (comment voice and fix badgelogo)

--- Voice/Sodium.cs
+++ Voice/Sodium.cs
@@ -41,24 +41,24 @@ namespace Discord_UWP.Voice
         }
 
 //#if X32
-        [DllImport("SodiumCWin32.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int Encrypt32(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
-        [DllImport("SodiumCWin32.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int Decrypt32(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
+        //[DllImport("SodiumCWin32.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
+        //private static extern int Encrypt32(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
+        //[DllImport("SodiumCWin32.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
+        //private static extern int Decrypt32(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
 //#endif
 
 //#if X64
-        [DllImport("SodiumCx64.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int Encrypt64(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
-        [DllImport("SodiumCx64.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int Decrypt64(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
+        //[DllImport("SodiumCx64.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
+        //private static extern int Encrypt64(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
+        //[DllImport("SodiumCx64.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
+        //private static extern int Decrypt64(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
 //#endif
 
 //#if ARM
-        [DllImport("SodiumCARM.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int EncryptARM(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
-        [DllImport("SodiumCARM.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int DecryptARM(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
+        //[DllImport("SodiumCARM.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
+        //private static extern int EncryptARM(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
+        //[DllImport("SodiumCARM.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
+        //private static extern int DecryptARM(byte* output, byte* input, long inputLength, byte[] nonce, byte[] secret);
 //#endif
 
         public static int encrypt(byte[] input, int inputOffset, int inputLength, byte[] output, int outputOffset, byte[] nonce, byte[] secret)
@@ -67,9 +67,9 @@ namespace Discord_UWP.Voice
             fixed (byte* inPtr = input)
             fixed (byte* outPtr = output)
             {
-                int error = Encrypt(outPtr + outputOffset, inPtr + inputOffset, inputLength, nonce, secret);
-                if (error != 0)
-                    throw new Exception($"Sodium Error: {error}");
+                //int error = Encrypt(outPtr + outputOffset, inPtr + inputOffset, inputLength, nonce, secret);
+                //if (error != 0)
+                //    throw new Exception($"Sodium Error: {error}");
                 return inputLength + 16;
             }
             #endif
@@ -80,9 +80,9 @@ namespace Discord_UWP.Voice
             fixed (byte* inPtr = input)
             fixed (byte* outPtr = output)
             {
-                int error = Decrypt(outPtr + outputOffset, inPtr + inputOffset, inputLength, nonce, secret);
-                if (error != 0)
-                    throw new Exception($"Sodium Error: {error}");
+                //int error = Decrypt(outPtr + outputOffset, inPtr + inputOffset, inputLength, nonce, secret);
+                //if (error != 0)
+                //    throw new Exception($"Sodium Error: {error}");
                 return inputLength - 16;
             }
         }
