ce00653663810a5475e13695acdb49bd15bd3ed9
Avishai Dernis <avid29@live.com>
2018-03-07 20:33:52 -0500

    7.1 Release

--- RuntimeComponent/Sodium.cs
+++ RuntimeComponent/Sodium.cs
@@ -18,26 +18,31 @@ namespace RuntimeComponent
         }
 #endif
 
-#if X86
-        [DllImport("SodiumCWin32.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int Encrypt(byte* output, byte* input, long inputLength, byte* nonce, Byte* secret);
-        [DllImport("SodiumCWin32.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int Decrypt(byte* output, byte* input, long inputLength, byte* nonce, byte* secret);
-#endif
+        //#if X86
+        //        [DllImport("SodiumCWin32.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
+        //        private static extern int Encrypt(byte* output, byte* input, long inputLength, byte* nonce, Byte* secret);
+        //        [DllImport("SodiumCWin32.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
+        //        private static extern int Decrypt(byte* output, byte* input, long inputLength, byte* nonce, byte* secret);
+        //#endif
 
-#if X64
-        [DllImport("SodiumCx64.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
+        //#if X64
+        //        [DllImport("SodiumCx64.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
+        //        private static extern int Encrypt(byte* output, byte* input, long inputLength, byte* nonce, byte* secret);
+        //        [DllImport("SodiumCx64.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
+        //        private static extern int Decrypt(byte* output, byte* input, long inputLength, byte* nonce, byte* secret);
+        //#endif
+
+        //#if ARM
+        //        [DllImport("SodiumCARM.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
+        //        private static extern int Encrypt(byte* output, byte* input, long inputLength, byte* nonce, Byte* secret);
+        //        [DllImport("SodiumCARM.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
+        //        private static extern int Decrypt(byte* output, byte* input, long inputLength, byte* nonce, byte* secret);
+        //#endif
+
+        [DllImport("SodiumC.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
         private static extern int Encrypt(byte* output, byte* input, long inputLength, byte* nonce, byte* secret);
-        [DllImport("SodiumCx64.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("SodiumC.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
         private static extern int Decrypt(byte* output, byte* input, long inputLength, byte* nonce, byte* secret);
-#endif
-
-#if ARM
-        [DllImport("SodiumCARM.dll", EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int Encrypt(byte* output, byte* input, long inputLength, byte* nonce, Byte* secret);
-        [DllImport("SodiumCARM.dll", EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int Decrypt(byte* output, byte* input, long inputLength, byte* nonce, byte* secret);
-#endif
 
         public static int encrypt([ReadOnlyArray()]byte[] input, int inputOffset, int inputLength, [WriteOnlyArray()]byte[] output, int outputOffset, [ReadOnlyArray()]byte[] nonce, [ReadOnlyArray()]byte[] secret)
         {
