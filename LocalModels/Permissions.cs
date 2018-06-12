bd9ec4e0b7833d8add25ffa41ee7b36e4a3e8db2
Avishai Dernis <avid29@live.com>
2018-06-13 01:18:50 +0300

    || Administrator

--- LocalModels/Permissions.cs
+++ LocalModels/Permissions.cs
@@ -81,17 +81,17 @@ namespace Discord_UWP.LocalModels
 
         public bool CreateInstantInvite
         {
-            get { return Convert.ToBoolean(Perms & 0x1); }
+            get { return Convert.ToBoolean(Perms & 0x1) || Administrator; }
             set { Perms = value ? Perms | 0x1 : Perms & ~0x1; }
         }
         public bool KickMembers
         {
-            get { return Convert.ToBoolean(Perms & 0x2); }
+            get { return Convert.ToBoolean(Perms & 0x2) || Administrator; }
             set { Perms = value ? Perms | 0x2 : Perms & ~0x2; }
         }
         public bool BanMembers
         {
-            get { return Convert.ToBoolean(Perms & 0x4); }
+            get { return Convert.ToBoolean(Perms & 0x4) || Administrator; }
             set { Perms = value ? Perms | 0x4 : Perms & ~0x4; }
         }
         public bool Administrator
@@ -101,42 +101,42 @@ namespace Discord_UWP.LocalModels
         }
         public bool ManageChannels
         {
-            get { return Convert.ToBoolean(Perms & 0x10); }
+            get { return Convert.ToBoolean(Perms & 0x10) || Administrator; }
             set { Perms = value ? Perms | 0x10 : Perms & ~0x10; }
         }
         public bool ManangeGuild
         {
-            get { return Convert.ToBoolean(Perms & 0x20); }
+            get { return Convert.ToBoolean(Perms & 0x20) || Administrator; }
             set { Perms = value ? Perms | 0x20 : Perms & ~0x20; }
         }
         public bool AddReactions
         {
-            get { return Convert.ToBoolean(Perms & 0x40); }
+            get { return Convert.ToBoolean(Perms & 0x40) || Administrator; }
             set { Perms = value ? Perms | 0x40 : Perms & ~0x40; }
         }
         public bool ViewAuditLog
         {
-            get { return Convert.ToBoolean(Perms & 0x80); }
+            get { return Convert.ToBoolean(Perms & 0x80) || Administrator; }
             set { Perms = value ? Perms | 0x80 : Perms & ~0x80; }
         }
         public bool ReadMessages
         {
-            get { return Convert.ToBoolean(Perms & 0x400); }
+            get { return Convert.ToBoolean(Perms & 0x400) || Administrator; }
             set { Perms = value ? Perms | 0x400 : Perms & ~0x400; }
         }
         public bool SendMessages
         {
-            get { return Convert.ToBoolean(Perms & 0x800); }
+            get { return Convert.ToBoolean(Perms & 0x800) || Administrator; }
             set { Perms = value ? Perms | 0x800 : Perms & ~0x800; }
         }
         public bool SendTtsMessages
         {
-            get { return Convert.ToBoolean(Perms & 0x1000); }
+            get { return Convert.ToBoolean(Perms & 0x1000) || Administrator; }
             set { Perms = value ? Perms | 0x1000 : Perms & ~0x1000; }
         }
         public bool ManageMessages
         {
-            get { return Convert.ToBoolean(Perms & 0x2000); }
+            get { return Convert.ToBoolean(Perms & 0x2000) || Administrator; }
             set { Perms = value ? Perms | 0x2000 : Perms & ~0x2000; }
         }
         public bool EmbedLinks
@@ -146,77 +146,77 @@ namespace Discord_UWP.LocalModels
         }
         public bool AttachFiles
         {
-            get { return Convert.ToBoolean(Perms & 0x8000); }
+            get { return Convert.ToBoolean(Perms & 0x8000) || Administrator; }
             set { Perms = value ? Perms | 0x8000 : Perms & ~0x8000; }
         }
         public bool ReadMessageHistory
         {
-            get { return Convert.ToBoolean(Perms & 0x10000); }
+            get { return Convert.ToBoolean(Perms & 0x10000) || Administrator; }
             set { Perms = value ? Perms | 0x10000 : Perms & ~0x10000; }
         }
         public bool MentionEveryone
         {
-            get { return Convert.ToBoolean(Perms & 0x20000); }
+            get { return Convert.ToBoolean(Perms & 0x20000) || Administrator; }
             set { Perms = value ? Perms | 0x20000 : Perms & ~0x20000; }
         }
         public bool UseExternalEmojis
         {
-            get { return Convert.ToBoolean(Perms & 0x40000); }
+            get { return Convert.ToBoolean(Perms & 0x40000) || Administrator; }
             set { Perms = value ? Perms | 0x40000 : Perms & ~0x40000; }
         }
         public bool Connect
         {
-            get { return Convert.ToBoolean(Perms & 0x100000); }
+            get { return Convert.ToBoolean(Perms & 0x100000) || Administrator; }
             set { Perms = value ? Perms | 0x100000 : Perms & ~0x100000; }
         }
         public bool Speak
         {
-            get { return Convert.ToBoolean(Perms & 0x200000); }
+            get { return Convert.ToBoolean(Perms & 0x200000) || Administrator; }
             set { Perms = value ? Perms | 0x200000 : Perms & ~0x200000; }
         }
         public bool MuteMembers
         {
-            get { return Convert.ToBoolean(Perms & 0x400000); }
+            get { return Convert.ToBoolean(Perms & 0x400000) || Administrator; }
             set { Perms = value ? Perms | 0x400000 : Perms & ~0x400000; }
         }
         public bool DeafenMembers
         {
-            get { return Convert.ToBoolean(Perms & 0x800000); }
+            get { return Convert.ToBoolean(Perms & 0x800000) || Administrator; }
             set { Perms = value ? Perms | 0x800000 : Perms & ~0x800000; }
         }
         public bool MoveMembers
         {
-            get { return Convert.ToBoolean(Perms & 0x1000000); }
+            get { return Convert.ToBoolean(Perms & 0x1000000) || Administrator; }
             set { Perms = value ? Perms | 0x1000000 : Perms & ~0x1000000; }
         }
         public bool UseVad
         {
-            get { return Convert.ToBoolean(Perms & 0x2000000); }
+            get { return Convert.ToBoolean(Perms & 0x2000000) || Administrator; }
             set { Perms = value ? Perms | 0x2000000 : Perms & ~0x2000000; }
         }
         public bool ChangeNickname
         {
-            get { return Convert.ToBoolean(Perms & 0x4000000); }
+            get { return Convert.ToBoolean(Perms & 0x4000000) || Administrator; }
             set { Perms = value ? Perms | 0x4000000 : Perms & ~0x4000000; }
         }
         public bool ManageNicknames
         {
-            get { return Convert.ToBoolean(Perms & 0x8000000); }
+            get { return Convert.ToBoolean(Perms & 0x8000000) || Administrator; }
             set { Perms = value ? Perms | 0x8000000 : Perms & ~0x8000000; }
         }
         public bool ManageRoles
         {
-            get { return Convert.ToBoolean(Perms & 0x10000000); }
+            get { return Convert.ToBoolean(Perms & 0x10000000) || Administrator; }
             set { Perms = value ? Perms | 0x10000000 : Perms & ~0x10000000; }
         }
         public bool ManageWebhooks
         {
-            get { return Convert.ToBoolean(Perms & 0x20000000); }
+            get { return Convert.ToBoolean(Perms & 0x20000000) || Administrator; }
             set { Perms = value ? Perms | 0x20000000 : Perms & ~0x20000000; }
         }
         public bool ManageEmojis
         {
-            get { return Convert.ToBoolean(Perms & 0x40000000); }
+            get { return Convert.ToBoolean(Perms & 0x40000000) || Administrator; }
             set { Perms = value ? Perms | 0x40000000 : Perms & ~0x40000000; }
         }
 
