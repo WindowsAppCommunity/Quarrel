e0524d79a59a34d89fa3cf208ce7da2d6633ff63
karmaecrivain94 <karmaecrivain94@users.noreply.github.com>
2018-07-06 12:31:02 +0200

    Added "call" field to SharedModels.Message

--- DiscordAPI/SharedModels/Message.cs
+++ DiscordAPI/SharedModels/Message.cs
@@ -47,6 +47,8 @@ namespace Discord_UWP.SharedModels
         public User User { get; set; }
         [JsonProperty("content")]
         public string Content { get; set; }
+        [JsonProperty("call")]
+        public Call Call { get; set; }
         [JsonProperty("timestamp")]
         public DateTime Timestamp { get; set; }
         [JsonProperty("edited_timestamp")]
@@ -87,5 +89,13 @@ namespace Discord_UWP.SharedModels
         public string Id { get; set; }
         [JsonProperty("channel_id")]
         public string ChannelId { get; set; }
+    }
+
+    public class Call
+    {
+        [JsonProperty("participants")]
+        public IEnumerable<string> Participants { get; set; }
+        [JsonProperty("ended_timestamp")]
+        public string EndedTimestamp { get; set; }
     }
 }
