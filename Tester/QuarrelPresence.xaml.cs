a492f3501e9dc45d23eea2f8375506b1cfb0220e
Avishai Dernis <avid29@live.com>
2018-09-02 11:17:07 -0400

    Start on AppService Connection nuget project

---
+++ Tester/QuarrelPresence.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using QuarrelPresence;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Tester
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class QuarrelPresence : Page
+    {
+        public QuarrelPresence()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            QuarrelAppService service = new QuarrelAppService("485827719947354131");
+            await service.TryConnectAsync(true);
+            await service.SetActivity(new QuarrelAppService.Activity() { Details = "Testing Quarrel Presence"});
+        }
+    }
+}
