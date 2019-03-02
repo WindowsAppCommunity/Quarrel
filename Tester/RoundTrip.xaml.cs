0115c5d6b1442c62c0fa3f95ce841adc735baf6a
Avishai Dernis <avid29@live.com>
2019-03-02 03:05:33 -0500

    Revert

--- Tester/RoundTrip.xaml.cs
+++ Tester/RoundTrip.xaml.cs
@@ -13,9 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-using RuntimeComponent.Opus;
-
-//using Concentus.Structs;
+using Concentus.Structs;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -31,13 +29,11 @@ namespace Tester
             this.InitializeComponent();
         }
 
-        private OpusEncoder encoder;// = new OpusEncoder(48000, 2, Concentus.Enums.OpusApplication.OPUS_APPLICATION_VOIP);
-        private OpusDecoder decoder;// = new OpusDecoder(48000, 2);
+        private OpusEncoder encoder = new OpusEncoder(48000, 2, Concentus.Enums.OpusApplication.OPUS_APPLICATION_VOIP);
+        private OpusDecoder decoder = new OpusDecoder(48000, 2);
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            encoder = OpusEncoder.Create(48000, 2, RuntimeComponent.Application.Voip);
-            decoder = OpusDecoder.Create(48000, 2);
             await AudioManager.CreateAudioGraphs();
             
             if ((sender as ToggleButton).IsChecked == true)
@@ -49,8 +45,6 @@ namespace Tester
             }
         }
 
-
-
         private void RTProcess(object sender, float[] e)
         {
             try
@@ -58,20 +52,14 @@ namespace Tester
                 byte[] nonce = new byte[] { 128, 120, 192, 46, 6, 144, 172, 128, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //Length of 24
                 byte[] key = new byte[] { 40, 221, 122, 207, 253, 63, 24, 97, 28, 168, 80, 250, 98, 165, 166, 32, 161, 61, 248, 51, 84, 26, 171, 14, 139, 17, 174, 121, 9, 74, 181, 33 }; //Length of 32
 
-                int encodedSize;
-                byte[] opus = encoder.Encode(e, 960, out encodedSize);
-                //int encodedSize = encoder.Encode(e, 0, 960, opus, 0, 1820 * sizeof(float));
+                byte[] opus = new byte[1820 * sizeof(float) + 16];
+                int encodedSize = encoder.Encode(e, 0, 960, opus, 0, 1820 * sizeof(float));
                 RuntimeComponent.Cypher.encrypt(opus, 0, encodedSize, opus, 0, nonce, key);
 
                 RuntimeComponent.Cypher.decrypt(opus, 0, encodedSize+16, opus, 0, nonce, key);
 
-                //float[] frame = new float[e.Length];
-                //int samples = decoder.Decode(opus, 0, encodedSize, frame, 0, (20 * 48 * 2));
-
-                int samples;
-                float[] frame = decoder.Decode(opus, encodedSize, out samples);
-                
-
+                float[] frame = new float[e.Length];
+                int samples = decoder.Decode(opus, 0, encodedSize, frame, 0, (20 * 48 * 2));
                 AudioManager.AddFrame(frame, (uint)samples);
             }
             catch (Exception exc)
