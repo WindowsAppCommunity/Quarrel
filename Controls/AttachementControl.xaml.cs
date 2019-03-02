34bea4fd129108f7bb5423818fd0c56a037cf1c2
Avishai Dernis <avid29@live.com>
2019-03-02 06:21:48 -0500

    AttachmentControl comments

--- Controls/AttachementControl.xaml.cs
+++ Controls/AttachementControl.xaml.cs
@@ -25,7 +25,9 @@ namespace Discord_UWP.Controls
 {
     public sealed partial class AttachementControl : UserControl
     {
-
+        /// <summary>
+        /// The API data Attachment to display
+        /// </summary>
         public SharedModels.Attachment DisplayedAttachement
         {
             get { return (SharedModels.Attachment)GetValue(AttachementProperty); }
@@ -36,7 +38,10 @@ namespace Discord_UWP.Controls
             typeof(SharedModels.Attachment),
             typeof(AttachementControl),
             new PropertyMetadata(null, OnPropertyChangedStatic));
-
+        
+        /// <summary>
+        /// True if the Attachement is in the MessageEditor
+        /// </summary>
         public bool IsFake
         {
             get { return (bool)GetValue(IsFakeProperty); }
@@ -50,8 +55,14 @@ namespace Discord_UWP.Controls
 
         public event EventHandler<EventArgs> Delete;
 
+        /// <summary>
+        /// Types of attachements
+        /// </summary>
         private enum Type { Unknown, Image, Audio, Video, PDF};
 
+        /// <summary>
+        /// The type of this Attachment
+        /// </summary>
         private Type type = Type.Unknown;
 
         private static void OnPropertyChangedStatic(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -60,9 +71,11 @@ namespace Discord_UWP.Controls
             instance?.OnPropertyChanged(d, e.Property);
         }
 
+        #region Raw FileType lists
         readonly string[] ImageFiletypes = { ".jpg", ".jpeg", ".gif", ".tif", ".tiff", ".png", ".bmp", ".gif", ".ico", ".jxr", ".hdp", ".wdp" };
         readonly string[] AudioFiletypes = { ".mp3", ".wav", ".m4a", ".ac3", ".ec3", ".flac", ".3gp", ".amr"};
         readonly string[] VideoFiletypes = { ".3g2", ".3gp2", ".3gp", ".m4v", ".mp4v", ".mp4", ".mov", ".m2ts", ".asf", ".wm", ".wmv", ".avi"};
+        #endregion
 
         private void OnPropertyChanged(DependencyObject d, DependencyProperty property)
         {
@@ -72,13 +85,20 @@ namespace Discord_UWP.Controls
             }
         }
 
+
         private void AttachedImageViewer_Tapped(object sender, TappedRoutedEventArgs e)
         {
+            // Open the Attachment to be viewed in full windows
             App.OpenAttachement(DisplayedAttachement);
         }
 
+        /// <summary>
+        /// Load the attachement
+        /// </summary>
+        /// <param name="images">True if the preview should be shown</param>
         private void LoadAttachement(bool images)
         {
+            // Clear Attachment
             AttachedImageViewer.Source = null;
             AttachedImageViewbox.Visibility = Visibility.Collapsed;
             LoadingImage.IsActive = false;
@@ -91,6 +111,7 @@ namespace Discord_UWP.Controls
             
             if (images)
             {
+                // Set the FileType icon and show preview if applicable
                 string extension = "." + DisplayedAttachement.Filename.Split('.').Last().ToLower();
                 if (ImageFiletypes.Contains(extension))
                 {
@@ -100,7 +121,8 @@ namespace Discord_UWP.Controls
                     {
                         ShowPreview();
                     }
-                } else if (AudioFiletypes.Contains(extension))
+                }
+                else if (AudioFiletypes.Contains(extension))
                 {
                     type = Type.Audio;
                     PreviewIcon.Glyph = "";
@@ -111,7 +133,8 @@ namespace Discord_UWP.Controls
                         ShowPreview();
                     }
 
-                } else if (VideoFiletypes.Contains(extension))
+                }
+                else if (VideoFiletypes.Contains(extension))
                 {
                     type = Type.Video;
                     PreviewIcon.Glyph = "";
@@ -120,31 +143,41 @@ namespace Discord_UWP.Controls
                     {
                         ShowPreview();
                     }
-                } else if (".pdf" == extension)
+                }
+                else if (".pdf" == extension)
                 {
                     type = Type.PDF;
                     PreviewIcon.Glyph = "";
                 }
             }
+
+            // Enable navigation if applicable
             if (!IsFake)
                 FileName.NavigateUri = new Uri(DisplayedAttachement.Url);
+            
+            // Set FileName and byte size info
             FileName.Content = DisplayedAttachement.Filename;
             FileSize.Text = Common.HumanizeFileSize(DisplayedAttachement.Size);
+
             if (type == Type.Unknown || type == Type.PDF || NetworkSettings.GetTTL())
             { 
+                // Show FileType Icon
                 if (type != Type.Unknown) { PreviewButton.Visibility = Visibility.Visible; FileIcon.Visibility = Visibility.Collapsed; }
                 AttachedFileViewer.Visibility = Visibility.Visible;
                 player.Visibility = Visibility.Collapsed;
             }
             else if (type == Type.Audio || type == Type.Video)
             {
+                // Show MediaPlayer
                 player.Visibility = Visibility.Visible;
                 if(type == Type.Audio)
                     player.Height = 48;
-                
             }
         }
 
+        /// <summary>
+        /// Image successfully loaded
+        /// </summary>
         private void AttachedImageViewer_ImageLoaded(object sender, RoutedEventArgs e)
         {
             AttachedImageViewer.ImageOpened -= AttachedImageViewer_ImageLoaded;
@@ -152,7 +185,10 @@ namespace Discord_UWP.Controls
             LoadingImage.IsActive = false;
             LoadingImage.Visibility = Visibility.Collapsed;
         }
-
+        
+        /// <summary>
+        /// Image Failed to load
+        /// </summary>
         private void AttachementImageViewer_ImageFailed(object sender, ExceptionRoutedEventArgs e)
         {
             AttachedImageViewer.ImageOpened -= AttachedImageViewer_ImageLoaded;
@@ -168,11 +204,17 @@ namespace Discord_UWP.Controls
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Invoke remove Attachment event
+        /// </summary>
+        private void RemoveAttachement(object sender, RoutedEventArgs e)
         {
             Delete?.Invoke(this, null);
         }
 
+        /// <summary>
+        /// Open file
+        /// </summary>
         private async void FileName_Click(object sender, RoutedEventArgs e)
         {
             if (IsFake)
@@ -180,7 +222,10 @@ namespace Discord_UWP.Controls
                 await Launcher.LaunchUriAsync(new Uri("file:///" + Uri.EscapeUriString(DisplayedAttachement.Url.Replace('\\','/'))));
             }
         }
-
+        
+        /// <summary>
+        /// Show "Save Image" flyout (Right Tapped)
+        /// </summary>
         private void AttachedImageViewer_RightTapped(object sender, RightTappedRoutedEventArgs e)
         {
             e.Handled = true;
@@ -190,6 +235,9 @@ namespace Discord_UWP.Controls
             }
         }
 
+        /// <summary>
+        /// Show "Save Image" flyout (Holding)
+        /// </summary>
         private void AttachedImageViewer_Holding(object sender, HoldingRoutedEventArgs e)
         {
             e.Handled = true;
@@ -199,35 +247,19 @@ namespace Discord_UWP.Controls
             }
         }
 
-        private void player_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            /*
-            if(DisplayedAttachement.Width.HasValue && DisplayedAttachement.Height.HasValue)
-            {
-                double aspectratio = (double)DisplayedAttachement.Width.Value / (double)DisplayedAttachement.Height.Value;
-                if (aspectratio < 1)
-                {
-                    //Player should be higher than it is wide
-                    player.Height = player.ActualWidth / aspectratio;
-                }
-                else
-                {
-                    //Player should be wider than it is high
-                    player.Width = player.ActualHeight / aspectratio;
-                }
-            }*/
-            
-        }
+        /// <summary>
+        /// Show Attachment Preview
+        /// </summary>
+        private void ShowPreview(object sender, RoutedEventArgs e) => ShowPreview();
 
-        private void ShowPreview(object sender, RoutedEventArgs e)
-        {
-            ShowPreview();
-        }
-
+        /// <summary>
+        /// Show Attachment Preview
+        /// </summary>
         private async void ShowPreview()
         {
             switch (type)
             {
+                // If it's an Image, begin Loading image and show Loading indicator
                 case Type.Image:
                     if (DisplayedAttachement.Filename.EndsWith(".svg"))
                     {
@@ -242,6 +274,8 @@ namespace Discord_UWP.Controls
                     LoadingImage.Visibility = Visibility.Visible;
                     LoadingImage.IsActive = true;
                     break;
+
+                // If it's Audio, begin loading audio and show MediaPlayer
                 case Type.Audio:
                     FileIcon.Visibility = Visibility.Collapsed;
                     FileName.FontSize = 14;
@@ -252,6 +286,8 @@ namespace Discord_UWP.Controls
                     player.Visibility = Visibility.Visible;
                     player.Height = 48;
                     break;
+
+                // If it's a Video, begin loading video and show MediaPlayer
                 case Type.Video:
                     FileIcon.Visibility = Visibility.Collapsed;
                     FileName.FontSize = 14;
@@ -261,10 +297,14 @@ namespace Discord_UWP.Controls
                     player.Visibility = Visibility.Visible;
                     player.HorizontalAlignment = HorizontalAlignment.Left;
                     break;
+
+                // If it's a PDF, show PDF Image preview
                 case Type.PDF:
                     AttachedFileViewer.Visibility = Visibility.Collapsed;
                     LoadingImage.Visibility = Visibility.Visible;
                     LoadingImage.IsActive = true;
+
+                    // Get PDF (stream) from URL
                     HttpClient client = new HttpClient();
                     var stream = await
                         client.GetStreamAsync(DisplayedAttachement.Url);
@@ -275,21 +315,24 @@ namespace Discord_UWP.Controls
                     LoadPDF(doc);
                     break;
             }
-            
-            //AttachedFileViewer.Visibility = Visibility.Collapsed;
         }
+
+        /// <summary>
+        /// Load a PDF preview image
+        /// </summary>
+        /// <param name="pdfDoc">PDF to load</param>
         async void LoadPDF(PdfDocument pdfDoc)
         {
+            // Get PDF image preview from PdfDocument
             BitmapImage image = new BitmapImage();
-
             var page = pdfDoc.GetPage(0); //TODO: PasswordProtected PDF support
-
             using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
             {
                 await page.RenderToStreamAsync(stream);
                 await image.SetSourceAsync(stream);
             }
 
+            // Set the Attachmenet image to PDF preview
             AttachedImageViewer.Source = image;
             PDFPages.Text = App.GetString("/Controls/PDFPage").Replace("<page>", "1").Replace("<pagecount>", pdfDoc.PageCount.ToString());
             AttachedImageViewbox.Visibility = Visibility.Visible;
