705c52a82709c838d8067b96889e848c15215d71
Avishai Dernis <avid29@live.com>
2019-03-03 06:39:01 -0500

    LoadingControl comments

--- Controls/LoadingControl.xaml.cs
+++ Controls/LoadingControl.xaml.cs
@@ -12,68 +12,88 @@ namespace Discord_UWP.Controls
 {
     public sealed partial class LoadingControl : UserControl
     {
+        /// <summary>
+        /// Cure message
+        /// </summary>
         public string Message
         { get => MessageBlock.Text; set => MessageBlock.Text = value; }
 
+        /// <summary>
+        /// Loading status
+        /// </summary>
         public string Status
-        { get => StatusBlock.Text; set =>UpdateStatus(value); }
+        { get => StatusBlock.Text; set => UpdateStatus(value); }
+
         private async void UpdateStatus(string val)
         {
+            // Run on UI thread
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
+                // Update the status text
                 StatusBlock.Text = val;
             });
        }
         public LoadingControl()
         {
             InitializeComponent();
+
+            // Initialize animation
             initialize();
+
+            // Timer for assuming issues
             DispatcherTimer timer = new DispatcherTimer {Interval = TimeSpan.FromSeconds(6)};
             timer.Tick += ShowReset;
             timer.Start();
+
+            // Event Subscriptions
             App.Splash.Dismissed += Splash_Dismissed;
         }
 
+        /// <summary>
+        /// Adjust the size of SVG image
+        /// </summary>
         private async void Splash_Dismissed(SplashScreen sender, object args)
         {
             if (App.shareop == null)
             {
+                // Run on UI thread
                 await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, AdjustSize);
             }
             else
+            {
                 AdjustSize();
+            }
         }
 
+        /// <summary>
+        /// Display Reset button
+        /// </summary>
         private void ShowReset(object sender, object e)
         {
             ResetButton.Visibility = Visibility.Visible;
             ResetButton.Fade(1).Start();
         }
 
+        /// <summary>
+        /// Set up display
+        /// </summary>
         public async void initialize()
         {
-            var message = await EntryMessages.GetMessage();
-
-            //else if (message.Key.Substring(0, 7) == "(Audio)")
-            //{
-            //    App.mediaPlayer = new MediaPlayer();
-            //    App.mediaPlayer.SystemMediaTransportControls.IsEnabled = false;
-            //    App.mediaPlayer.Source = MediaSource.CreateFromUri((new Uri("ms-appx:///Assets/EntryMessages/" + message.Key.Substring(7))));
-            //    App.mediaPlayer.Play();
-            //    App.mediaPlayer.CurrentStateChanged += MediaPlayer_CurrentStateChanged;
-            //    MessageBlock.Visibility = Visibility.Collapsed;
-            //}
-
+            // Setup icon
             AdjustSize();
-
             Animation.Begin();
 
+            // Hide message if applicable
             if (!Storage.Settings.ShowWelcomeMessage)
             {
                 MessageBlock.Visibility = Visibility.Collapsed;
                 return;
             }
 
-            #region Special Shit
+
+            // Setup message
+            var message = await EntryMessages.GetMessage();
+
+            #region Changes by message
             switch (message.Key)
             {
                 case "Now with Comic Sans":
@@ -94,17 +114,23 @@ namespace Discord_UWP.Controls
                     CreditBlock.Text = "-" + message.Value;
                 }
             }
-            Animation.Begin();
-            App.StatusChangedHandler += App_StatusChangedHandler;
 
+            // Event Subscriptions
+            App.StatusChangedHandler += App_StatusChangedHandler;
         }
         
-
+        /// <summary>
+        /// Update the status
+        /// </summary>
+        /// <param name="e">New status</param>
         private void App_StatusChangedHandler(object sender, string e)
         {
             MessageBlock.Text = e;
         }
 
+        /// <summary>
+        /// Adjust ViewBox size
+        /// </summary>
         public void AdjustSize()
         {
             try
@@ -121,16 +147,28 @@ namespace Discord_UWP.Controls
                 // ignored
             }
         }
+
+        /// <summary>
+        /// Show Control
+        /// </summary>
+        /// <param name="animate">Fade in</param>
         public async void Show(bool animate)
         {
+            // Run on UI thread
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
                 Visibility = Visibility.Visible;
                 if (animate) LoadIn.Begin();
             });
         }
+
+        /// <summary>
+        /// Hide entire Controls
+        /// </summary>
+        /// <param name="animate">Fade out</param>
         public async void Hide(bool animate)
         {
+            /// Run on UI thread
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
                 if (animate)
@@ -141,27 +179,43 @@ namespace Discord_UWP.Controls
             });
 
         }
+
+        /// <summary>
+        /// Finish Load in
+        /// </summary>
         private void LoadIn_Completed(object sender, object e)
         {
             Animation.Begin();
         }
 
+        /// <summary>
+        /// Finish Load out
+        /// </summary>
         private void LoadOut_Completed(object sender, object e)
         {
             Visibility = Visibility.Collapsed;
             Animation.Stop();
         }
 
+        /// <summary>
+        /// Adjust size when size changes (duh)
+        /// </summary>
         private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             AdjustSize();
         }
-
+        
+        /// <summary>
+        /// Prompt app reset
+        /// </summary>
         private async void ResetButton_Click(object sender, RoutedEventArgs e)
         {
             await App.RequestReset();
         }
 
+        /// <summary>
+        /// Dispose of this object
+        /// </summary>
         public void Dipose()
         {
             App.Splash.Dismissed -= Splash_Dismissed;
