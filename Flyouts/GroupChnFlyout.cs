83809ef4e57fbeb3de6ac7f782ec0d3715f1714b
Avishai Dernis <avid29@live.com>
2019-05-10 20:38:38 -0400

    Flyout comments

--- Flyouts/GroupChnFlyout.cs
+++ Flyouts/GroupChnFlyout.cs
@@ -20,14 +20,18 @@ namespace Discord_UWP.Flyouts
         {
             MenuFlyout menu = new MenuFlyout();
             menu.MenuFlyoutPresenterStyle = (Style)App.Current.Resources["MenuFlyoutPresenterStyle1"];
+
+            // Add users
             int x = 0;
             foreach (User user in LocalState.DMs[dm.Id].Users)
             {
+                // Create SubItem for user
                 MenuFlyoutSubItem item = new MenuFlyoutSubItem()
                 {
                     Text = user.Username
                 };
 
+                // Add "Profile" button to subitem
                 MenuFlyoutItem profile = new MenuFlyoutItem()
                 {
                     Text = App.GetString("/Flyouts/Profile"),
@@ -35,13 +39,17 @@ namespace Discord_UWP.Flyouts
                     Tag = user
                 };
                 profile.Click += FlyoutManager.OpenProfile;
-
                 item.Items.Add(profile);
+
+
+                // Add Seperator
                 MenuFlyoutSeparator sep1 = new MenuFlyoutSeparator();
                 item.Items.Add(sep1);
 
+                // If current user owns the dm
                 if (dm.OwnerId == LocalState.CurrentUser.Id)
                 {
+                    // Add "Remove from DM" button
                     MenuFlyoutItem removeFromDm = new MenuFlyoutItem()
                     {
                         Text = App.GetString("/Flyouts/RemoveFromGroup"),
@@ -52,10 +60,12 @@ namespace Discord_UWP.Flyouts
                     removeFromDm.Click += FlyoutManager.RemoveGroupUser;
                     item.Items.Add(removeFromDm);
 
+                    // Add Seperator
                     MenuFlyoutSeparator sep2 = new MenuFlyoutSeparator();
                     item.Items.Add(sep2);
                 }
 
+                // Create "Add Friend" button
                 MenuFlyoutItem addFriend = new MenuFlyoutItem()
                 {
                     Text = App.GetString("/Flyouts/AddFriend"),
@@ -64,6 +74,7 @@ namespace Discord_UWP.Flyouts
                 };
                 addFriend.Click += FlyoutManager.AddFriend;
 
+                // Create "Accept Friend Request" button
                 MenuFlyoutItem acceptFriendRequest = new MenuFlyoutItem()
                 {
                     Text = App.GetString("/Flyouts/AcceptFriendRequest"),
@@ -72,6 +83,7 @@ namespace Discord_UWP.Flyouts
                 };
                 acceptFriendRequest.Click += FlyoutManager.AddFriend;
 
+                // Create "Remove Friend" button
                 MenuFlyoutItem removeFriend = new MenuFlyoutItem()
                 {
                     Text = App.GetString("/Flyouts/RemoveFriend"),
@@ -81,6 +93,7 @@ namespace Discord_UWP.Flyouts
                 };
                 removeFriend.Click += FlyoutManager.RemoveFriend;
 
+                // Create "Block" button
                 MenuFlyoutItem block = new MenuFlyoutItem()
                 {
                     Text = App.GetString("/Flyouts/Block"),
@@ -90,6 +103,7 @@ namespace Discord_UWP.Flyouts
                 };
                 block.Click += FlyoutManager.BlockUser;
 
+                // Create "Unblock" button
                 MenuFlyoutItem unBlock = new MenuFlyoutItem()
                 {
                     Text = App.GetString("/Flyouts/Unblock"),
@@ -98,42 +112,63 @@ namespace Discord_UWP.Flyouts
                 };
                 unBlock.Click += FlyoutManager.RemoveFriend;
 
+                // Choose buttons to add
                 if (LocalState.Friends.ContainsKey(dm.Users.FirstOrDefault().Id))
                 {
                     switch (LocalState.Friends[dm.Users.FirstOrDefault().Id].Type)
                     {
+                        // No relation
                         case 0:
+                            // Add "Add Friend" and "Block" buttons
                             item.Items.Add(addFriend);
                             item.Items.Add(block);
                             break;
+
+                        // Friends
                         case 1:
+                            // Add "Remove Friend" and "Block" buttons
                             item.Items.Add(removeFriend);
                             item.Items.Add(block);
                             break;
+
+                        // Blocked
                         case 2:
+                            // Add "Unblock" button
                             item.Items.Add(unBlock);
                             break;
+
+                        // Incoming Friend Request
                         case 3:
+                            // Add "Accept Friend Request" and "Block" buttons
                             item.Items.Add(acceptFriendRequest);
                             item.Items.Add(block);
                             break;
+
+                        // Outgoing Friend Request
                         case 4:
+                            // Add "Block" button
                             menu.Items.Add(block);
                             break;
                     }
                 }
                 else
                 {
+                    // Default to no relation
                     item.Items.Add(addFriend);
                     item.Items.Add(block);
                 }
+                
+                // Add subitems
                 menu.Items.Add(item);
                 x++;
             }
 
+
+            // Add seperator
             MenuFlyoutSeparator sep3 = new MenuFlyoutSeparator();
             menu.Items.Add(sep3);
 
+            // Add "Leave DM" button
             MenuFlyoutItem leaveDm = new MenuFlyoutItem()
             {
                 Text = App.GetString("/Flyouts/LeaveGroup"),
