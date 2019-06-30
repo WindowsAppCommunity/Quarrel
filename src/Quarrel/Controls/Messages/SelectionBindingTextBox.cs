2f5475aceffc71be72f27ab722a482fb06be6766
Matthew <40522069+matthew4850@users.noreply.github.com>
2019-06-30 20:10:30 +0100

    Use messagebox with mvvm.

---
+++ src/Quarrel/Controls/Messages/SelectionBindingTextBox.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Quarrel.Controls.Messages
+{
+    public class SelectionBindingTextBox : TextBox
+    {
+        public static readonly DependencyProperty BindableSelectionStartProperty =
+            DependencyProperty.Register(
+            "BindableSelectionStart",
+            typeof(int),
+            typeof(SelectionBindingTextBox),
+            new PropertyMetadata(0, OnBindableSelectionStartChanged));
+
+        public static readonly DependencyProperty BindableSelectionLengthProperty =
+            DependencyProperty.Register(
+            "BindableSelectionLength",
+            typeof(int),
+            typeof(SelectionBindingTextBox),
+            new PropertyMetadata(0, OnBindableSelectionLengthChanged));
+
+        private bool changeFromUI;
+
+        public SelectionBindingTextBox() : base()
+        {
+            this.SelectionChanged += this.OnSelectionChanged;
+        }
+
+        public int BindableSelectionStart
+        {
+            get
+            {
+                return (int)this.GetValue(BindableSelectionStartProperty);
+            }
+
+            set
+            {
+                this.SetValue(BindableSelectionStartProperty, value);
+            }
+        }
+
+        public int BindableSelectionLength
+        {
+            get
+            {
+                return (int)this.GetValue(BindableSelectionLengthProperty);
+            }
+
+            set
+            {
+                this.SetValue(BindableSelectionLengthProperty, value);
+            }
+        }
+
+        private static void OnBindableSelectionStartChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            var textBox = dependencyObject as SelectionBindingTextBox;
+
+            if (!textBox.changeFromUI)
+            {
+                int newValue = (int)args.NewValue;
+                textBox.SelectionStart = newValue;
+            }
+            else
+            {
+                textBox.changeFromUI = false;
+            }
+        }
+
+        private static void OnBindableSelectionLengthChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            var textBox = dependencyObject as SelectionBindingTextBox;
+
+            if (!textBox.changeFromUI)
+            {
+                int newValue = (int)args.NewValue;
+                textBox.SelectionLength = newValue;
+            }
+            else
+            {
+                textBox.changeFromUI = false;
+            }
+        }
+
+        private void OnSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.BindableSelectionStart != this.SelectionStart)
+            {
+                this.changeFromUI = true;
+                this.BindableSelectionStart = this.SelectionStart;
+            }
+
+            if (this.BindableSelectionLength != this.SelectionLength)
+            {
+                this.changeFromUI = true;
+                this.BindableSelectionLength = this.SelectionLength;
+            }
+        }
+    }
+}
