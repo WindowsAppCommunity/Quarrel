6a5eebaa4156bed89f41cdc9adead46ce94638e0
Avishai Dernis <avid29@live.com>
2019-07-04 21:37:31 -0400

    User Joined message (other message types unsupported)

---
+++ src/Quarrel/TemplateSelectors/MessageTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using DiscordAPI.Models;
+using Quarrel.Models.Bindables;
+
+namespace Quarrel.TemplateSelectors
+{
+    public sealed class MessageTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (container is FrameworkElement parent && item is BindableMessage msg)
+            {
+                return parent.FindResource<DataTemplate>(msg.Model.Type == 0 ? "MessageTemplate" : "ActionMessageTemplate");
+            }
+            return null;
+        }
+    }
+}
