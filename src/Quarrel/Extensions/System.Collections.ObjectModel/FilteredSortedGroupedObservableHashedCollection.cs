57e9f03843593b5b5fe7f25005ebf9442e344f0f
Matthew <40522069+matthew4850@users.noreply.github.com>
2019-07-07 15:01:25 +0100

    FilteredSortedGroupedObservableHashedCollection.

---
+++ src/Quarrel/Extensions/System.Collections.ObjectModel/FilteredSortedGroupedObservableHashedCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace System.Collections.ObjectModel
+{
+    public class FilteredSortedGroupedObservableHashedCollection<TKey, TGroup, TElement>
+        : SortedGroupedObservableHashedCollection<TKey, TGroup, TElement>
+        where TGroup : IEquatable<TGroup>, IComparable<TGroup>
+        where TElement : class, IEquatable<TElement>, IComparable<TElement>
+    {
+
+        private readonly Func<HashedGrouping<TKey, TGroup, TElement>, bool> Filter;
+
+        public FilteredSortedGroupedObservableHashedCollection([NotNull] Func<TElement, TGroup> keyReader, [NotNull] Func<HashedGrouping<TKey, TGroup, TElement>, int> sorter, [NotNull] Func<HashedGrouping<TKey, TGroup, TElement>, bool> filter, ICollection<KeyValuePair<TKey, HashedGrouping<TKey, TGroup, TElement>>> collection) : base(keyReader, sorter, collection)
+        {
+            PropertiesToUpdate.Add(nameof(FilteredValues));
+            PropertiesToUpdate.Add(nameof(FilteredSortedValues));
+            Filter = filter;
+        }
+
+        [NotNull]
+
+        public ICollection<HashedGrouping<TKey, TGroup, TElement>> FilteredValues => Values.Distinct().Where(Filter).ToList();
+        public ICollection<HashedGrouping<TKey, TGroup, TElement>> FilteredSortedValues => ValuesSorted.Distinct().Where(Filter).ToList();
+    }
+}
