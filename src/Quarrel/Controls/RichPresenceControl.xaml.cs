9b32a71eff040311a02a1a27ecd697de6d35fb15
Avishai Dernis <avid29@live.com>
2019-07-13 05:12:53 -0400

    Basic Rich Presence Control

---
+++ src/Quarrel/Controls/RichPresenceControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using DiscordAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Quarrel.Controls
+{
+    public sealed partial class RichPresenceControl : UserControl
+    {
+        public RichPresenceControl()
+        {
+            this.InitializeComponent();
+            this.DataContextChanged += (s, e) =>
+            {
+                this.Bindings.Update();
+            };
+        }
+
+        public Game ViewModel => DataContext as Game;
+    }
+}
--- src/_APIs/DiscordAPI/Models/Game.cs
+++ src/_APIs/DiscordAPI/Models/Game.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 
 namespace DiscordAPI.Models
 {
@@ -19,26 +20,54 @@ namespace DiscordAPI.Models
     {
         [JsonProperty("url")]
         public string Url { get; set; }
+
         [JsonProperty("timestamps")]
         public timestamps TimeStamps{get;set; }
+
         [JsonProperty("application_id")]
         public string ApplicationId { get; set; }
+
         [JsonProperty("details")]
         public string Details { get; set; }
+
         [JsonProperty("state")]
         public string State { get; set; }
+
         [JsonProperty("party")]
         public party Party { get; set; }
+
         [JsonProperty("assets")]
         public assets Assets { get; set; }
+
         [JsonProperty("secrets")]
         public secrets Secrets { get; set; }
+
         [JsonProperty("flags")]
         public int Flags { get; set; }
+
         [JsonProperty("instance")]
         public bool Instance { get; set; }
+
         [JsonProperty("session_id")]
         public string SessionId { get; set; }
+
+        public bool IsXboxGame { get => ApplicationId != null && ApplicationId == "438122941302046720"; }
+
+        public string SmallImageUrl => Assets != null ? GetImageUrl(Assets.SmallImage, ApplicationId) : "";
+        public Uri SmallImageUri => Assets != null ? new Uri(GetImageUrl(Assets.SmallImage, ApplicationId)) : null;
+
+        public string LargeImageUrl => Assets != null ? GetImageUrl(Assets.LargeImage, ApplicationId) : "";
+        public Uri LargeImageUri => Assets != null ? new Uri(GetImageUrl(Assets.LargeImage, ApplicationId)) : null;
+
+        public string GetImageUrl(string id, string gameid, bool game = false, string append = "?size=512")
+        {
+            // Set type in query 
+            string type = "app";
+            if (game) type = "game";
+
+            // Query URL
+            return "https://cdn.discordapp.com/" + type + "-assets/" + gameid + "/" + id + ".png" + append;
+        }
     }
     public class timestamps
     {
