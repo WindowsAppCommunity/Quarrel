06a5306c20fcd19619dd2293c5ae541d47cd6d44
Avishai Dernis <avid29@live.com>
2019-12-17 17:00:56 -0500

    Server emojis in reactions

--- src/_APIs/DiscordAPI/Models/Emoji.cs
+++ src/_APIs/DiscordAPI/Models/Emoji.cs
@@ -11,15 +11,27 @@ namespace DiscordAPI.Models
     {
         [JsonProperty("id")]
         public string Id { get; set; }
+
         [JsonProperty("name")]
         public string Name { get; set; }
+
         [JsonProperty("roles")]
         public IEnumerable<string> Roles { get; set; }
+
         [JsonProperty("require_colons")]
         public bool RequireColons { get; set; }
+
         [JsonProperty("managed")]
         public bool Managed { get; set; }
+
         [JsonProperty("animated")]
         public bool Animated { get; set; }
+
+        [JsonIgnore]
+        public bool IsServer { get => Id != null; }
+
+        [JsonIgnore]
+        public string DisplayUrl { get => "https://cdn.discordapp.com/emojis/" + Id 
+                + (Animated ? ".gif" : ".png"); }
     }
 }
