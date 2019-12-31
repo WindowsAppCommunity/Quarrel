b76de718d46fc35fba0ebbdaf59d61c6c794b548
Avishai Dernis <avid29@live.com>
2019-12-31 12:31:25 -0500

    Further CommandBar improvment

---
+++ src/Quarrel/Converters/Base/RemoveNewLineConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Quarrel.Converters.Base
+{
+    public sealed class RemoveNewLineConveter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string sValue)
+            {
+                sValue = sValue.Replace('\n', ' ');
+                sValue = sValue.Replace('\r', ' ');
+                return sValue;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
