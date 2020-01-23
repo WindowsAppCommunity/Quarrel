8e20308a13020c807c06cb6370a77a697371446f
Avishai Dernis <avid29@live.com>
2020-01-23 12:37:24 -0500

    TemplateSelector for EmojiPicker

---
+++ src/Quarrel/TemplateSelectors/EmojiPickerTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using Quarrel.ViewModels.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Quarrel.TemplateSelectors
+{
+    public sealed class EmojiPickerTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate UnicodeEmoji { get; set; }
+        public DataTemplate GuildEmoji { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (container is FrameworkElement && item is Emoji emoji)
+            {
+                if (emoji.CustomEmoji)
+                    return GuildEmoji;
+                else
+                    return UnicodeEmoji;
+            }
+
+            return null;
+        }
+    }
+}
