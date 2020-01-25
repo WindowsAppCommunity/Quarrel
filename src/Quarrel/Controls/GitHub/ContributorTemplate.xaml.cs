6fbf069ce2e43db53eabf263ee472c72cd93bbb4
Avishai Dernis <avid29@live.com>
2020-01-25 07:56:38 -0500

    Improved ContributorTemplate

---
+++ src/Quarrel/Controls/GitHub/ContributorTemplate.xaml.cs
@@ -0,0 +1,32 @@
+﻿using GitHubAPI.Models;
+using Windows.UI.Xaml.Controls;
+using UICompositionAnimationsLegacy.Helpers.PointerEvents;
+using Windows.Devices.Input;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Quarrel.Controls.GitHub
+{
+    public sealed partial class ContributorTemplate : UserControl
+    {
+        public ContributorTemplate()
+        {
+            this.InitializeComponent();
+            this.DataContextChanged += (s, e) =>
+            {
+                this.Bindings.Update();
+            };
+            this.ManageControlPointerStates(TogglePointerVisualStates);
+        }
+
+        /// <summary>
+        /// Executes the necessary animations when the pointer goes over/out of the control
+        /// </summary>
+        private void TogglePointerVisualStates(PointerDeviceType pointer, bool on)
+        {
+            if (pointer == PointerDeviceType.Mouse) (on ? ShowHover : HideHover).Begin();
+        }
+
+        public Contributor ViewModel => DataContext as Contributor;
+    }
+}
