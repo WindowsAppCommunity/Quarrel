a4fdf7d36ba91029a660aa90167b220218650f4f
Avishai Dernis <avid29@live.com>
2020-01-30 10:45:31 -0500

    ChannelListControl comments

--- src/Quarrel/Controls/Shell/Views/ChannelListControl.xaml.cs
+++ src/Quarrel/Controls/Shell/Views/ChannelListControl.xaml.cs
@@ -1,16 +1,21 @@
 ﻿using Quarrel.ViewModels;
 using Windows.UI.Xaml.Controls;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
-
 namespace Quarrel.Controls.Shell.Views
 {
+    /// <summary>
+    /// Control to handle ChannelLsit
+    /// </summary>
     public sealed partial class ChannelListControl : UserControl
     {
         public ChannelListControl()
         {
             this.InitializeComponent();
         }
+        
+        /// <summary>
+        /// Access app's main data
+        /// </summary>
         public MainViewModel ViewModel => App.ViewModelLocator.Main;
     }
 }
