60ec9536ea0aed9b4385b260b142e6ab1ff14326
Avishai Dernis <avid29@live.com>
2020-01-29 21:41:42 -0500

    Fixed File Upload

--- src/Quarrel/Converters/Base/StreamToBitmapImageConverter.cs
+++ src/Quarrel/Converters/Base/StreamToBitmapImageConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media.Imaging;
 
@@ -12,7 +13,7 @@ namespace Quarrel.Converters.Base
             if (value is Stream stream)
             {
                 var bitmapimage = new BitmapImage();
-                bitmapimage.SetSource(stream.AsRandomAccessStream());
+                bitmapimage.SetSource(stream.AsRandomAccessStream().CloneStream());
                 return bitmapimage;
             }
             return null;
