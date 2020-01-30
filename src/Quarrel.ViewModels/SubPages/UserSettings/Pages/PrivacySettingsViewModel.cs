b62ebfc11270501a01c555826f5573835d378a75
Avishai Dernis <avid29@live.com>
2020-01-30 00:37:36 -0500

    Fixed Privacy Settings applying

--- src/Quarrel.ViewModels/SubPages/UserSettings/Pages/PrivacySettingsViewModel.cs
+++ src/Quarrel.ViewModels/SubPages/UserSettings/Pages/PrivacySettingsViewModel.cs
@@ -1,23 +1,26 @@
 ﻿using DiscordAPI.API.User.Models;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
+using Quarrel.ViewModels.Messages.Gateway;
 using Quarrel.ViewModels.Services.Discord.CurrentUser;
 using Quarrel.ViewModels.Services.Discord.Rest;
+using Quarrel.ViewModels.Services.DispatcherHelper;
 
 namespace Quarrel.ViewModels.SubPages.UserSettings.Pages
 {
     public class PrivacySettingsViewModel : ViewModelBase
     {
-        public ICurrentUserService CurrentUsersService
-        {
-            get => SimpleIoc.Default.GetInstance<ICurrentUserService>();
-        }
-
+        private ICurrentUserService CurrentUsersService => SimpleIoc.Default.GetInstance<ICurrentUserService>();
+        
         public bool FilterAll
         {
             get => CurrentUsersService.CurrentUserSettings.ExplicitContentFilter == 2;
             set
             {
+                // Being set to false, don't update
+                if (!value)
+                    return;
+
                 ModifyUserSettings modify = new ModifyUserSettings(CurrentUsersService.CurrentUserSettings);
                 modify.ExplicitContentFilter = 2;
                 ApplyChanges(modify);
@@ -29,6 +32,10 @@ namespace Quarrel.ViewModels.SubPages.UserSettings.Pages
             get => CurrentUsersService.CurrentUserSettings.ExplicitContentFilter == 1;
             set
             {
+                // Being set to false, don't update
+                if (!value)
+                    return;
+
                 ModifyUserSettings modify = new ModifyUserSettings(CurrentUsersService.CurrentUserSettings);
                 modify.ExplicitContentFilter = 1;
                 ApplyChanges(modify);
@@ -40,6 +47,10 @@ namespace Quarrel.ViewModels.SubPages.UserSettings.Pages
             get => CurrentUsersService.CurrentUserSettings.ExplicitContentFilter == 0;
             set
             {
+                // Being set to false, don't update
+                if (!value)
+                    return;
+
                 ModifyUserSettings modify = new ModifyUserSettings(CurrentUsersService.CurrentUserSettings);
                 modify.ExplicitContentFilter = 0;
                 ApplyChanges(modify);
