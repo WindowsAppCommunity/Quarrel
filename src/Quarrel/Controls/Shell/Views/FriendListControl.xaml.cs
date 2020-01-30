57d353723cfb0ffdbe0475e4a41c0ac9c34fa6f6
Avishai Dernis <avid29@live.com>
2020-01-30 10:49:52 -0500

    More FriendListControl comments

--- src/Quarrel/Controls/Shell/Views/FriendListControl.xaml.cs
+++ src/Quarrel/Controls/Shell/Views/FriendListControl.xaml.cs
@@ -4,6 +4,9 @@ using Windows.UI.Xaml.Controls;
 
 namespace Quarrel.Controls.Shell.Views
 {
+    /// <summary>
+    /// Control for FriendsList page on DM Guild
+    /// </summary>
     public sealed partial class FriendListControl : UserControl
     {
         public FriendListControl()
