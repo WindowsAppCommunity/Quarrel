4b36baba01d3d0fbf0a1494481f03915f08ee0b5
Avishai Dernis <avid29@live.com>
2020-01-30 11:08:12 -0500

    Partial MemberList comments

--- src/Quarrel/Controls/Shell/Views/MemberListControl.xaml.cs
+++ src/Quarrel/Controls/Shell/Views/MemberListControl.xaml.cs
@@ -10,10 +10,11 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
-
 namespace Quarrel.Controls.Shell.Views
 {
+    /// <summary>
+    /// Control to handle member list
+    /// </summary>
     public sealed partial class MemberListControl : UserControl
     {
         public MemberListControl()
@@ -26,8 +27,14 @@ namespace Quarrel.Controls.Shell.Views
             });
         }
 
+        /// <summary>
+        /// Access app's main data
+        /// </summary>
         public MainViewModel ViewModel => App.ViewModelLocator.Main;
 
+        /// <summary>
+        /// Zooms to approipate header when Header selected from Semantic Out view
+        /// </summary>
         private void SemanticZoom_ViewChangeStarted(object sender, SemanticZoomViewChangedEventArgs e)
         {
             if (e.IsSourceZoomedInView == false)
@@ -36,6 +43,13 @@ namespace Quarrel.Controls.Shell.Views
                 e.DestinationItem.Item = ViewModel.CurrentBindableMembers.FirstOrDefault(x => x is BindableGuildMemberGroup group && group.Model.Id == sourceItem.Model.Id);
             }
         }
+        
+        /// <summary>
+        /// Finds first child of type <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">Type of child to find</typeparam>
+        /// <param name="root">Item must be a child of root</param>
+        /// <returns>First item on Visual Stack, under root of type <typeparamref name="T"/></returns>
         public static T FindChildOfType<T>(DependencyObject root) where T : class
         {
             var queue = new Queue<DependencyObject>();
