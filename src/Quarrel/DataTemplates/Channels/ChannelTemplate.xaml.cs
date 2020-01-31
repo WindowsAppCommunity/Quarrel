a69c72f7d8295894c8b96b0e377eb32ed6cf8bdf
Avishai Dernis <avid29@live.com>
2020-01-30 21:35:39 -0500

    More ChannelTemplate comments

--- src/Quarrel/DataTemplates/Channels/ChannelTemplate.xaml.cs
+++ src/Quarrel/DataTemplates/Channels/ChannelTemplate.xaml.cs
@@ -13,8 +13,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
-
 namespace Quarrel.DataTemplates.Channels
 {
     public sealed partial class ChannelTemplate
