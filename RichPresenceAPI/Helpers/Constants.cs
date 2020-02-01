67f25cd51b2ae3779dbdf201f31cda0ed013a3c4
Avishai Dernis <avid29@live.com>
2020-02-01 11:21:49 -0500

    New QuarrelRichPresence API

---
+++ RichPresenceAPI/Helpers/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quarrel.RichPresence.Helpers
+{
+    public static class Constants
+    {
+        public static class ConnectionServiceRequests
+        {
+            public const string SetActivity = "SET_ACTIVITY";
+        }
+    }
+}
---
+++ RichPresenceAPI/Models/RichGame.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace Quarrel.RichPresence.Models
+{
+    /// <summary>
+    /// Detailed game state
+    /// </summary>
+    public class RichGame : Game
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("timestamps")]
+        public TimeStamps TimeStamps { get; set; }
+
+        [JsonProperty("application_id")]
+        public string ApplicationId { get; set; }
+
+        [JsonProperty("details")]
+        public string Details { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("party")]
+        public Party Party { get; set; }
+
+        [JsonProperty("assets")]
+        public Assets Assets { get; set; }
+
+        [JsonProperty("secrets")]
+        public Secrets Secrets { get; set; }
+
+        [JsonProperty("flags")]
+        public int Flags { get; set; }
+
+        [JsonProperty("instance")]
+        public bool Instance { get; set; }
+
+        [JsonProperty("session_id")]
+        public string SessionId { get; set; }
+    }
+}
