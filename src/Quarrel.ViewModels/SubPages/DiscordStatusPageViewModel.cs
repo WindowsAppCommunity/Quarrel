0d84052b1de410ea40d2243d23c61a05150ca7b3
Avishai Dernis <avid29@live.com>
2020-02-05 19:44:47 -0500

    Moved DiscordStatusPage to ViewModel

---
+++ src/Quarrel.ViewModels/SubPages/DiscordStatusPageViewModel.cs
@@ -0,0 +1,200 @@
+﻿using DiscordStatusAPI.API;
+using DiscordStatusAPI.API.Status;
+using DiscordStatusAPI.Models;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Quarrel.ViewModels.SubPages
+{
+
+    #region Classes
+
+    public class ComplexComponent
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Time { get; set; }
+        public List<SimpleComponent> Items { get; set; }
+    }
+
+    public class SimpleComponent
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+
+    #endregion
+
+    public class DiscordStatusPageViewModel : ViewModelBase
+    {
+        #region Constructors
+
+        public DiscordStatusPageViewModel()
+        {
+            Setup();
+        }
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler StatusLoaded;
+
+        public event EventHandler ChartDataLoaded;
+
+        #endregion
+
+        #region Methods
+
+        private async void Setup()
+        {
+            RestFactory factory = new RestFactory();
+            StatusService = factory.GetStatusService();
+
+            Status = await StatusService.GetStatus();
+
+            FailedToLoad = Status == null;
+
+            if (Status == null)
+            {
+                return;
+            }
+
+            if (Status.Incidents != null)
+            {
+                foreach (var incident in Status.Incidents)
+                {
+                    if (incident.Status != "resolved")
+                    {
+                        List<SimpleComponent> updates = new List<SimpleComponent>();
+                        for (int i = 0; i < incident.IncidentUpdates.Length; i++)
+                        {
+                            updates.Add(new SimpleComponent()
+                            {
+                                Status = incident.IncidentUpdates[i].Status,
+                                Description = incident.IncidentUpdates[i].Body,
+                                Name = incident.IncidentUpdates[i].UpdatedAt.ToString("t")
+                            });
+                        }
+                        ComplexComponent component = new ComplexComponent()
+                        {
+                            Name = incident.Name,
+                            Status = incident.Status,
+                            Items = updates
+                        };
+
+                        if (!string.IsNullOrWhiteSpace(component.Name))
+                            Incidents.Add(component);
+                    }
+                }
+            }
+
+            if (Status.Components != null)
+            {
+                foreach (var component in Status.Components)
+                {
+                    SimpleComponent sc = new SimpleComponent()
+                    {
+                        Name = component.Name,
+                        Status = component.Status.Replace("_", " "),
+                        Description = component.Description
+                    };
+                    Components.Add(sc);
+                }
+            }
+
+            ShowMetrics("day");
+        }
+
+        public async void ShowMetrics(string duration)
+        {
+            var metrics = await StatusService.GetMetrics(duration);
+            if (metrics != null && metrics.Metrics != null && metrics.Metrics.Length > 0)
+            {
+
+                var metric = metrics.Metrics[0];
+                Data.Clear();
+                DataValues.Clear();
+                Max = 0;
+                Min = 0;
+                for (var i = 0; i < metric.Data.Length; i++)
+                {
+                    DataValues.Add(i, metric.Data[i]);
+                    Data.Add(metric.Data[i].Value);
+                    if (metric.Data[i].Value > Max)
+                    {
+                        Max = metric.Data[i].Value;
+                    }
+                    if (metric.Data[i].Value < Min || Min == 0)
+                    {
+                        Min = metric.Data[i].Value;
+                    }
+                }
+                ChartDataLoaded?.Invoke(this, null);
+            }
+        }
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Indicates if the status was able to load
+        /// </summary>
+        public bool FailedToLoad
+        {
+            get => _FailedToLoad;
+            set => Set(ref _FailedToLoad, value);
+        }
+        private bool _FailedToLoad;
+
+        /// <summary>
+        /// Outage index information
+        /// </summary>
+        public Index Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+        private Index _Status;
+
+        /// <summary>
+        /// Access to Discord Status API
+        /// </summary>
+        private IStatusService StatusService;
+
+        /// <summary>
+        /// Lowest value on chart
+        /// </summary>
+        public double Min = 0;
+
+        /// <summary>
+        /// Highest value on chart
+        /// </summary>
+        public double Max = 0;
+
+        /// <summary>
+        /// Ping times to graph
+        /// </summary>
+        public readonly List<double> Data = new List<double>();
+
+        /// <summary>
+        /// Precise ping times
+        /// </summary>
+        public Dictionary<int, Datum> DataValues = new Dictionary<int, Datum>();
+
+        /// <summary>
+        /// List of Incidents 
+        /// </summary>
+        public ObservableCollection<ComplexComponent> Incidents { get; set; } = new ObservableCollection<ComplexComponent>();
+
+        /// <summary>
+        /// Status notifications
+        /// </summary>
+        public ObservableCollection<SimpleComponent> Components { get; set; } = new ObservableCollection<SimpleComponent>();
+        #endregion
+    }
+}
--- src/Quarrel/SubPages/DiscordStatusPage.xaml.cs
+++ src/Quarrel/SubPages/DiscordStatusPage.xaml.cs
@@ -1,217 +1,76 @@
-﻿using System;
+﻿using DiscordStatusAPI.Models;
+using Microsoft.Graphics.Canvas.Text;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using Microsoft.Toolkit.Uwp.UI.Animations;
+using Quarrel.Helpers;
+using Quarrel.SubPages.Interfaces;
+using Quarrel.ViewModels.SubPages;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
+using System.Numerics;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
-using Microsoft.Graphics.Canvas.UI.Xaml;
-using System.Numerics;
-using Microsoft.Graphics.Canvas.Text;
-using Microsoft.Toolkit.Uwp.UI.Animations;
-using Quarrel.Helpers;
-using DiscordStatusAPI.Models;
-using DiscordStatusAPI.API.Status;
-using DiscordStatusAPI.API;
-using Quarrel.SubPages.Interfaces;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Quarrel.SubPages
 {
     public sealed partial class DiscordStatusPage : UserControl, IConstrainedSubPage
     {
-        #region Classes
-
-        public class ComplexComponent
-        {
-            public string Description { get; set; }
-            public string Name { get; set; }
-            public string Status { get; set; }
-            public string Time { get; set; }
-            public List<SimpleComponent> Items { get; set; }
-            public SolidColorBrush Color { get; set; }
-        }
-        public class SimpleComponent
-        {
-            public string Description { get; set; }
-            public string Name { get; set; }
-            public string Status { get; set; }
-            public SolidColorBrush Color { get; set; }
-        }
-
-        #endregion
-
-        #region Constructors
         public DiscordStatusPage()
         {
             this.InitializeComponent();
-            Setup();
-        }
-        #endregion
-
-        #region Methods
-        private async void Setup()
-        {
-            RestFactory factory = new RestFactory();
-            StatusService = factory.GetStatusService();
-
-            status = await StatusService.GetStatus();
-
-            if (status == null)
+            DataContext = new DiscordStatusPageViewModel();
+            ViewModel.ChartDataLoaded += (m, args) =>
             {
-                FailedToLoad.Visibility = Visibility.Visible;
-                return;
-            }
-            else
-            {
-                FailedToLoad.Visibility = Visibility.Collapsed;
-            }
-            if (status.Incidents != null)
-            {
-                foreach (var incident in status.Incidents)
-                {
-                    if (incident.Status != "resolved")
-                    {
-                        List<SimpleComponent> updates = new List<SimpleComponent>();
-                        for (int i = 0; i < incident.IncidentUpdates.Length; i++)
-                        {
-                            updates.Add(new SimpleComponent()
-                            {
-                                Status = incident.IncidentUpdates[i].Status,
-                                Description = incident.IncidentUpdates[i].Body,
-                                Name = incident.IncidentUpdates[i].UpdatedAt.ToString("t")
-                            });
-                        }
-                        ComplexComponent sc = new ComplexComponent()
-                        {
-                            Name = incident.Name,
-                            Status = incident.Status,
-                            Color = ColorFromStatus(incident.Status),
-                            Items = updates
-                        };
-
-                        if (!string.IsNullOrWhiteSpace(sc.Name))
-                            IncidentsPanel.Items.Add(sc);
-                    }
-                }
-            }
-
-            if (IncidentsPanel.Items.Count > 0)
-            {
-                IncidentsPanel.Visibility = Visibility.Visible;
-                IncidentsScroller.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                IncidentsPanel.Visibility = Visibility.Collapsed;
-                IncidentsScroller.Visibility = Visibility.Collapsed;
-            }
-
-            if (status.Status != null)
-            {
-                var statusBrush = ColorFromStatus(status.Status.Indicator);
-                statusColor = statusBrush.Color;
-                statusContainer.Background = statusBrush;
-                dayDuration.Foreground = statusBrush;
-                weekDuration.Foreground = statusBrush;
-                monthDuration.Foreground = statusBrush;
-                IncidentsScroller.Background = new SolidColorBrush(statusBrush.Color) { Opacity = 0.25 };
-                statusDescription.Text = status.Status.Description;
-                statusContainer.Visibility = Visibility.Visible;
-            }
-            if (status.Components != null)
-            {
-                foreach (var component in status.Components)
-                {
-                    SimpleComponent sc = new SimpleComponent()
-                    {
-                        Name = component.Name,
-                        Status = component.Status.Replace("_", " "),
-                        Color = ColorFromStatus(component.Status),
-                        Description = component.Description
-                    };
-                    ComponentsPanel.Items.Add(sc);
-
-                }
-            }
-            scale2.CenterY = grid.ActualHeight / 2;
-            scale2.CenterX = grid.ActualWidth / 2;
-            LoadIn.Begin();
-            ShowMetrics("day");
-        }
-
-        public SolidColorBrush ColorFromStatus(string status)
-        {
-            if (status == "operational" || status == "none")
-            {
-                return (SolidColorBrush)Application.Current.Resources["online"];
-            }
-            else if (status == "partial_outage" | status == "minor")
-            {
-                return (SolidColorBrush)Application.Current.Resources["idle"];
-            }
-            else
-            {
-                return (SolidColorBrush)Application.Current.Resources["dnd"];
-            }
-
-        }
-
-        private async void ShowMetrics(string duration)
-        {
-            if (ChangedMetricsDisplay)
-            {
-                HideChart.Begin();
-            }
-
-            ChangedMetricsDisplay = true;
-            var metrics = await StatusService.GetMetrics(duration);
-            if (metrics != null && metrics.Metrics != null && metrics.Metrics.Length > 0)
-            {
-
-                var metric = metrics.Metrics[0];
-                _data.Clear();
-                datavalues.Clear();
-                _max = 0;
-                _min = 0;
-                for (var i = 0; i < metric.Data.Length; i++)
-                {
-                    datavalues.Add(i, metric.Data[i]);
-                    _data.Add(metric.Data[i].Value);
-                    if (metric.Data[i].Value > _max)
-                    {
-                        _max = metric.Data[i].Value;
-                    }
-                    if (metric.Data[i].Value < _min || _min == 0)
-                    {
-                        _min = metric.Data[i].Value;
-                    }
-
-                }
                 chartCanvas.Invalidate();
                 if (HideChart.GetCurrentState() != ClockState.Stopped)
                 {
                     HideChart.Stop();
                 }
                 ShowChart.Begin();
+            };
+
+            // Change everythings color by status
+            ViewModel.StatusLoaded += (m, args) =>
+            {
+                var statusBrush = new SolidColorBrush(StatusColor);
+                StatusContainer.Background = statusBrush;
+                dayDuration.Foreground = statusBrush;
+                weekDuration.Foreground = statusBrush;
+                monthDuration.Foreground = statusBrush;
+                IncidentsScroller.Background = new SolidColorBrush(statusBrush.Color) { Opacity = 0.25 };
+            };
+        }
+
+        public DiscordStatusPageViewModel ViewModel => DataContext as DiscordStatusPageViewModel;
+
+        #region Methods
+
+        public Color ColorFromStatus(string status)
+        {
+            if (status == "operational" || status == "none")
+            {
+                return (Color)App.Current.Resources["onlineColor"];
+            }
+            else if (status == "partial_outage" | status == "minor")
+            {
+                return (Color)App.Current.Resources["idleColor"];
+            }
+            else
+            {
+                return (Color)App.Current.Resources["dndColor"];
             }
         }
 
         #region Chart Render
+
         private void CanvasControl_OnDraw(CanvasControl sender, CanvasDrawEventArgs args)
         {
-            _chartRenderer.RenderData(chartCanvas, args, statusColor, DataStrokeThickness, _data, false, _max);
+            _chartRenderer.RenderData(chartCanvas, args, StatusColor, DataStrokeThickness, ViewModel.Data, false, ViewModel.Max);
         }
 
         private void FrameworkElement_OnSizeChanged(object sender, SizeChangedEventArgs e)
@@ -221,35 +80,47 @@ namespace Quarrel.SubPages
             ShowChartDa.To = chartCanvas.ActualWidth;
             HideChartDa.From = chartCanvas.ActualWidth;
         }
+        
         #endregion
 
         #region Change Duration
+
+        /// <summary>
+        /// Selects and loads day graph
+        /// </summary>
         private void ShowDayMetrics(object sender, RoutedEventArgs e)
         {
-            ShowMetrics("day");
+            HideChart.Begin();
+            ViewModel.ShowMetrics("day");
             dayDuration.IsEnabled = false;
             weekDuration.IsEnabled = true;
             monthDuration.IsEnabled = true;
-            //Day
         }
 
+        /// <summary>
+        /// Selects and loads week graph
+        /// </summary>
         private void ShowWeekMetrics(object sender, RoutedEventArgs e)
         {
-            ShowMetrics("week");
+            HideChart.Begin();
+            ViewModel.ShowMetrics("week");
             dayDuration.IsEnabled = true;
             weekDuration.IsEnabled = false;
             monthDuration.IsEnabled = true;
-            //Week
         }
 
+        /// <summary>
+        /// Selects and loads month graph
+        /// </summary>
         private void ShowMonthMetrics(object sender, RoutedEventArgs e)
         {
-            ShowMetrics("month");
+            HideChart.Begin();
+            ViewModel.ShowMetrics("month");
             dayDuration.IsEnabled = true;
             weekDuration.IsEnabled = true;
             monthDuration.IsEnabled = false;
-            //Month
         }
+
         #endregion
 
         #region Indicator
@@ -262,13 +133,12 @@ namespace Quarrel.SubPages
 
         private void ChartIndicator_OnDraw(CanvasControl sender, CanvasDrawEventArgs args)
         {
-
             if (_chartRenderer.stepsize != 0)
             {
                 var location = Convert.ToInt32(Math.Round(CursorPosition / _chartRenderer.stepsize));
-                if (datavalues.ContainsKey(location))
+                if (ViewModel.DataValues.ContainsKey(location))
                 {
-                    var item = datavalues[location];
+                    var item = ViewModel.DataValues[location];
                     CanvasTextFormat format = new CanvasTextFormat { FontSize = 12.0f, WordWrapping = CanvasWordWrapping.NoWrap };
                     CanvasTextLayout textLayout = new CanvasTextLayout(args.DrawingSession, item.Value + "ms", format, 0.0f, 0.0f);
 
@@ -322,35 +192,24 @@ namespace Quarrel.SubPages
 
         #region Properties
 
-        // Outage index information
-        private Index status = null;
-
-        // Indicates if the chart has been changed
-        private bool ChangedMetricsDisplay = false;
-        
-        // Ping times to graph
-        Dictionary<int, Datum> datavalues = new Dictionary<int, Datum>();
-        private readonly List<double> _data = new List<double>();
-
-        // Width of line on graph
+        /// <summary>
+        /// Width of line on graph
+        /// </summary>
         private const float DataStrokeThickness = 1;
 
-        // Lowest value on chart
-        private double _min = 0;
-        
-        // Highest value on chart
-        private double _max = 0;
-        
-        // The position of the mouse on the chart, in Pixels
+        /// <summary>
+        /// The position of the mouse on the chart, in Pixels
+        /// </summary>
         private float CursorPosition;
 
-        // Accent Color based on Status
-        private Color statusColor = (Color)Application.Current.Resources["BlurpleColor"];
-        
-        // Access to Discord Status API
-        private IStatusService StatusService;
+        /// <summary>
+        /// Accent Color based on Status
+        /// </summary>
+        private Color StatusColor => ColorFromStatus(ViewModel.Status.Status.Indicator);
 
-        // Rendering help for the graph
+        /// <summary>
+        /// Rendering help for the graph
+        /// </summary>
         private readonly ChartRenderer _chartRenderer = new ChartRenderer();
 
         #endregion
