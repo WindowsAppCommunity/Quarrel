4ba4ff4758ed298d9e11acea57f75a8b5de97b55
Avishai Dernis <avid29@live.com>
2020-02-06 23:43:39 -0500

    Rich Presence type reflected in GuildMember item

---
+++ src/Quarrel/Converters/Profile/PresenceTypeToStatusConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Quarrel.Converters.Profile
+{
+    public class PresenceTypeToStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int iValue)
+            {
+                switch (iValue)
+                {
+                    case 0:
+                        return "Playing";
+                    case 1:
+                        return "Streaming";
+                    case 2:
+                        return "Listening to";
+                    case 3:
+                        return "Watching";
+                    default:
+                        return "";
+                }
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
