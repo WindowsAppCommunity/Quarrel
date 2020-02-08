57bc43943dc42a9c41fb8c62a10585954cc103a8
Avishai Dernis <avid29@live.com>
2020-02-08 06:59:41 -0500

    Set verification level

--- src/Quarrel.ViewModels/SubPages/GuildSettings/Pages/ModerationSettingsPageViewModel.cs
+++ src/Quarrel.ViewModels/SubPages/GuildSettings/Pages/ModerationSettingsPageViewModel.cs
@@ -1,5 +1,8 @@
-﻿using GalaSoft.MvvmLight;
+﻿using DiscordAPI.API.Guild.Models;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
 using Quarrel.ViewModels.Models.Bindables;
+using Quarrel.ViewModels.Services.Discord.Rest;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,6 +23,22 @@ namespace Quarrel.ViewModels.SubPages.GuildSettings.Pages
         }
         private BindableGuild _Guild;
 
+        #region Methods
+
+        public async void SetVerifcationLevel(int level)
+        {
+            ModifyGuild modify = new ModifyGuild()
+            {
+                Name = Guild.Model.Name,
+                AfkTimeout = Guild.Model.AfkTimeout,
+                ExplicitContentFilter = Guild.Model.ExplicitContentFilter,
+                VerificationLevel = level
+            };
+            await SimpleIoc.Default.GetInstance<IDiscordService>().GuildService.ModifyGuild(Guild.Model.Id, modify);
+        }
+
+        #endregion
+
         #region Properties
 
         #region Verfication Levels
@@ -27,22 +46,47 @@ namespace Quarrel.ViewModels.SubPages.GuildSettings.Pages
         public bool VerficationLevel0
         {
             get => Guild?.Model?.VerificationLevel == 0;
+            set
+            {
+                SetVerifcationLevel(0);
+                Guild.Model.VerificationLevel = 0;
+            }
         }
         public bool VerficationLevel1
         {
             get => Guild?.Model?.VerificationLevel == 1;
+            set
+            {
+                SetVerifcationLevel(1);
+                Guild.Model.VerificationLevel = 1;
+            }
         }
         public bool VerficationLevel2
         {
             get => Guild?.Model?.VerificationLevel == 2;
+            set
+            {
+                SetVerifcationLevel(2);
+                Guild.Model.VerificationLevel = 2;
+            }
         }
         public bool VerficationLevel3
         {
             get => Guild?.Model?.VerificationLevel == 3;
+            set
+            {
+                SetVerifcationLevel(3);
+                Guild.Model.VerificationLevel = 3;
+            }
         }
         public bool VerficationLevel4
         {
             get => Guild?.Model?.VerificationLevel == 4;
+            set
+            {
+                SetVerifcationLevel(4);
+                Guild.Model.VerificationLevel = 4;
+            }
         }
 
         #endregion
