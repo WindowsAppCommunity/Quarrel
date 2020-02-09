712009dfe4c90c145529da9c433940321aa47c03
Avishai Dernis <avid29@live.com>
2020-02-09 05:19:44 -0500

    Image Upload in OverviewSettings

---
+++ src/Quarrel.ViewModels/SubPages/GuildSettings/Pages/OverviewSettingsPageViewModel.cs
@@ -0,0 +1,64 @@
+﻿using DiscordAPI.API.Guild.Models;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Quarrel.ViewModels.Models.Bindables;
+using Quarrel.ViewModels.Services.Discord.Rest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quarrel.ViewModels.SubPages.GuildSettings.Pages
+{
+    public class OverviewSettingsPageViewModel : ViewModelBase
+    {
+        #region Constructor
+
+        public OverviewSettingsPageViewModel(BindableGuild guild)
+        {
+            Guild = guild;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async void UpdateIcon(string base64Icon)
+        {
+            ModifyGuildIcon modify = new ModifyGuildIcon(Guild.Model) { Icon = base64Icon };
+            try
+            {
+                var guild = await SimpleIoc.Default.GetInstance<IDiscordService>().GuildService.ModifyGuild(Guild.Model.Id, modify);
+                Guild.Model.Icon = guild.Icon;
+                Guild.RaisePropertyChanged(nameof(Guild.HasIcon));
+                Guild.RaisePropertyChanged(nameof(Guild.IconUrl));
+            }
+            // Mainly for Rate Limit
+            catch { }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public BindableGuild Guild
+        {
+            get => _Guild;
+            set => Set(ref _Guild, value);
+        }
+        private BindableGuild _Guild;
+
+        public string Name
+        {
+            get => Guild.Model.Name;
+            set
+            {
+                SimpleIoc.Default.GetInstance<IDiscordService>()
+                    .GuildService.ModifyGuild(Guild.Model.Id, new ModifyGuild(Guild.Model) { Name = value });
+                Guild.Model.Name = value;
+                Guild.RaisePropertyChanged(nameof(Guild.Model));
+            }
+        }
+
+        #endregion
+    }
+}
--- src/Quarrel/SubPages/GuildSettings/Pages/OverviewSettingsPage.xaml.cs
+++ src/Quarrel/SubPages/GuildSettings/Pages/OverviewSettingsPage.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using DiscordAPI.API.Guild.Models;
+using Microsoft.Advertising.Ads.Requests.AdBroker;
+using Quarrel.Helpers;
+using Quarrel.ViewModels.Models.Bindables;
+using Quarrel.ViewModels.SubPages.GuildSettings.Pages;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +31,32 @@ namespace Quarrel.SubPages.GuildSettings.Pages
         public OverviewSettingsPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            DataContext = new OverviewSettingsPageViewModel(e.Parameter as BindableGuild);
+        }
+
+        public OverviewSettingsPageViewModel ViewModel => DataContext as OverviewSettingsPageViewModel;
+
+        private async void UploadAvatar(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                ViewModel.UpdateIcon("data:" + file.ContentType + ";base64," +
+                    Convert.ToBase64String(await ImageParsing.FileToBytes(file)));
+            }
         }
     }
 }
