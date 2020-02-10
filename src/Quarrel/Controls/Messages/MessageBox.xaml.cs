d7ce664f48749dcd014ca28225acf69ca1a44fde
Avishai Dernis <avid29@live.com>
2020-02-10 02:06:38 -0500

    Paste Images to draft

--- src/Quarrel/Controls/Messages/MessageBox.xaml.cs
+++ src/Quarrel/Controls/Messages/MessageBox.xaml.cs
@@ -2,6 +2,8 @@
 using Refit;
 using System;
 using System.IO;
+using System.Runtime.CompilerServices;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
@@ -24,6 +26,10 @@ namespace Quarrel.Controls.Messages
 
         public MessageBoxViewModel ViewModel => DataContext as MessageBoxViewModel;
 
+        #region Methods
+
+        #region UI Events
+
         /// <summary>
         /// Select file and add to attachment List
         /// </summary>
@@ -35,12 +41,7 @@ namespace Quarrel.Controls.Messages
             picker.FileTypeFilter.Add(".jpeg");
             picker.FileTypeFilter.Add(".png");
             StorageFile file = await picker.PickSingleFileAsync();
-            if (file == null)
-            {
-                return;
-            }
-            var stream = await file.OpenStreamForReadAsync();
-            ViewModel.Attachments.Add(new StreamPart(stream, string.Format("{0}{1}", file.DisplayName, file.FileType), file.ContentType)); 
+            AddAttachment(file);
         }
 
         /// <summary>
@@ -50,5 +51,40 @@ namespace Quarrel.Controls.Messages
         {
             ViewModel.Attachments.Remove((sender as Button).DataContext as StreamPart);
         }
+
+        private async void PasteFromClipboard(object sender, TextControlPasteEventArgs e)
+        {
+            DataPackageView dataPackageView = Clipboard.GetContent();
+
+            if (dataPackageView.Contains(StandardDataFormats.StorageItems))
+            {
+                var param = await dataPackageView.GetStorageItemsAsync();
+                foreach (var file in param)
+                    AddAttachment(file as StorageFile);
+            }
+            else if (dataPackageView.Contains(StandardDataFormats.Bitmap))
+            {
+                var bmpStream = await dataPackageView.GetBitmapAsync();
+                ViewModel.Attachments.Add(new StreamPart((await bmpStream.OpenReadAsync()).AsStream(), "file.png"));
+            }
+        }
+
+        #endregion
+
+        #region Attachments
+
+        public async void AddAttachment(StorageFile file)
+        {
+            if (file == null)
+            {
+                return;
+            }
+            var stream = await file.OpenStreamForReadAsync();
+            ViewModel.Attachments.Add(new StreamPart(stream, string.Format("{0}{1}", file.DisplayName, file.FileType), file.ContentType));
+        }
+
+        #endregion
+
+        #endregion
     }
 }
