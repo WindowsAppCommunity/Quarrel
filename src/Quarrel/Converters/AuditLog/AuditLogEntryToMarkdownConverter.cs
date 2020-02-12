2cedf50c8e8b775b10a07f5f8f95350013145ac1
Avishai Dernis <avid29@live.com>
2020-02-12 05:02:54 -0500

    AuditLog formatting (unapplied)

--- src/Quarrel/Converters/AuditLog/AuditLogEntryToMarkdownConverter.cs
+++ src/Quarrel/Converters/AuditLog/AuditLogEntryToMarkdownConverter.cs
@@ -1,20 +1,47 @@
 ﻿using DiscordAPI.API.Guild.Models;
+using GalaSoft.MvvmLight.Ioc;
+using Quarrel.ViewModels.Models.Bindables;
+using Quarrel.ViewModels.Services.Discord.Channels;
+using Quarrel.ViewModels.Services.Discord.Guilds;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices.ComTypes;
+using Windows.ApplicationModel.Resources;
 using Windows.UI.Xaml.Data;
 
 namespace Quarrel.Converters.AuditLog
 {
     public class AuditLogEntryToMarkdownConverter : IValueConverter
     {
+        IChannelsService ChannelsService => SimpleIoc.Default.GetInstance<IChannelsService>();
+        IGuildsService GuildsService => SimpleIoc.Default.GetInstance<IGuildsService>();
+
+        public string ReplaceUser(string format, string userId)
+        {
+            string formattedUser = string.Format("<@{0}>", userId);
+            return format.Replace("<user>", formattedUser);
+        }
+
+        public string ReplaceChannel(string format, string channelId)
+        {
+            string formattedChannel = string.Format("<#{0}>", channelId);
+            return format.Replace("<channel>", formattedChannel);
+        }
+
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             if (value is AuditLogEntry entry)
             {
-                return ((AuditLogActionType)entry.ActionType).ToString();
+                string format = ResourceLoader.GetForCurrentView("AuditLog").GetString(((AuditLogActionType)entry.ActionType).ToString());
+                
+                
+                switch ((AuditLogActionType)entry.ActionType)
+                {
+                    case AuditLogActionType.ChannelCreate:
+                    case AuditLogActionType.ChannelUpdate:
+                    case AuditLogActionType.ChannelDelete:
+                        return ReplaceChannel(format, entry.TargetId);
+                }
+                return format;
             }
             return "Unknown action";
         }
