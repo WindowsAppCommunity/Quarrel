65868ef56400b2a02dce6d707d96a71827c58c01
Avishai Dernis <avid29@live.com>
2020-02-12 06:46:47 -0500

    Invite Formatting

--- src/Quarrel/Converters/AuditLog/AuditLogEntryToMarkdownConverter.cs
+++ src/Quarrel/Converters/AuditLog/AuditLogEntryToMarkdownConverter.cs
@@ -1,10 +1,8 @@
 ﻿using DiscordAPI.API.Guild.Models;
 using GalaSoft.MvvmLight.Ioc;
-using Quarrel.ViewModels.Models.Bindables;
 using Quarrel.ViewModels.Services.Discord.Channels;
 using Quarrel.ViewModels.Services.Discord.Guilds;
 using System;
-using System.Runtime.InteropServices.ComTypes;
 using Windows.ApplicationModel.Resources;
 using Windows.UI.Xaml.Data;
 
@@ -12,8 +10,14 @@ namespace Quarrel.Converters.AuditLog
 {
     public class AuditLogEntryToMarkdownConverter : IValueConverter
     {
+        #region Services
+
         IChannelsService ChannelsService => SimpleIoc.Default.GetInstance<IChannelsService>();
         IGuildsService GuildsService => SimpleIoc.Default.GetInstance<IGuildsService>();
+
+        #endregion
+
+        #region Methods
 
         public string ReplaceUser(string format, string userId)
         {
@@ -26,6 +30,20 @@ namespace Quarrel.Converters.AuditLog
             string formattedChannel = string.Format("<#{0}>", channelId);
             return format.Replace("<channel>", formattedChannel);
         }
+
+        public string ReplaceInvite(string format, Change[] changes, bool deleted)
+        {
+            foreach (var change in changes)
+            {
+                if (change.Key == "code")
+                {
+                    return format.Replace("<invite>", string.Format("**{0}**", (deleted ? change.OldValue : change.NewValue).ToString()));
+                }
+            }
+            return null;
+        }
+
+        #endregion
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
@@ -41,6 +59,11 @@ namespace Quarrel.Converters.AuditLog
                     case AuditLogActionType.ChannelUpdate:
                     case AuditLogActionType.ChannelDelete:
                         return ReplaceChannel(format, entry.TargetId);
+
+                    case AuditLogActionType.InviteCreate:
+                    case AuditLogActionType.InviteUpdate:
+                    case AuditLogActionType.InviteDelete:
+                        return ReplaceInvite(format, entry.Changes, (AuditLogActionType)entry.ActionType == AuditLogActionType.InviteDelete);
                 }
                 return format;
             }
