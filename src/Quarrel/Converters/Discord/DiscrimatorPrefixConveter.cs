b3e36af111cb19ceec3142075e33030ae1337dd9
Avishai Dernis <avid29@live.com>
2020-02-20 15:17:57 -0500

    DiscriminatorPrefixConverter StyleCop conformity

--- src/Quarrel/Converters/Discord/DiscrimatorPrefixConveter.cs
+++ src/Quarrel/Converters/Discord/DiscrimatorPrefixConveter.cs
@@ -1,19 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using System;
 using Windows.UI.Xaml.Data;
 
 namespace Quarrel.Converters.Discord
 {
+    /// <summary>
+    /// A converter that adds a <see langword="'#'"/> to the front of a <see cref="string"/>.
+    /// </summary>
     public sealed class DiscrimatorPrefixConveter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             return "#" + value.ToString();
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
