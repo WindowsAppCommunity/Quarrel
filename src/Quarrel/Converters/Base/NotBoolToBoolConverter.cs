2a6b7a1af85f0e9ab125736be4815ccc7fa725a3
Avishai Dernis <avid29@live.com>
2020-02-20 11:23:05 -0500

    NotBoolToBoolConverter StyleCop conformity

--- src/Quarrel/Converters/Base/NotBoolToBoolConverter.cs
+++ src/Quarrel/Converters/Base/NotBoolToBoolConverter.cs
@@ -1,19 +1,22 @@
-﻿using System;
-using Windows.UI.Xaml;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using System;
 using Windows.UI.Xaml.Data;
 
 namespace Quarrel.Converters.Base
 {
     /// <summary>
-    /// A converter that returns an inverted <see cref="Visibility"/> value for the input <see langword="bool"/> value
+    /// A converter that returns an inverted <see cref="bool"/>.
     /// </summary>
     public sealed class NotBoolToBoolConverter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             return !(bool)value;
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
