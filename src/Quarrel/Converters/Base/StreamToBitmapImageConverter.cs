209942bcddf0567c933c075c5c9d9a7b9a5d9611
Avishai Dernis <avid29@live.com>
2020-02-20 11:39:47 -0500

    StreamToBitmapImageConverter StyleCop conformity

--- src/Quarrel/Converters/Base/StreamToBitmapImageConverter.cs
+++ src/Quarrel/Converters/Base/StreamToBitmapImageConverter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using System;
 using System.IO;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml.Data;
@@ -6,19 +8,27 @@ using Windows.UI.Xaml.Media.Imaging;
 
 namespace Quarrel.Converters.Base
 {
+    /// <summary>
+    /// Converts a <see cref="Stream"/> to a <see cref="BitmapImage"/>.
+    /// </summary>
     public class StreamToBitmapImageConverter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             if (value is Stream stream)
             {
                 var bitmapimage = new BitmapImage();
+
+                // Clone as to not reserve
                 bitmapimage.SetSource(stream.AsRandomAccessStream().CloneStream());
                 return bitmapimage;
             }
+
             return null;
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
