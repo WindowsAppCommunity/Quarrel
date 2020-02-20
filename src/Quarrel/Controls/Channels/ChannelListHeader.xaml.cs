0760ef983551831c5d547b5d77a6070d16dee325
Avishai Dernis <avid29@live.com>
2020-02-19 23:48:54 -0500

    ChannelListHeader StyleCop conformity

--- src/Quarrel/Controls/Channels/ChannelListHeader.xaml.cs
+++ src/Quarrel/Controls/Channels/ChannelListHeader.xaml.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.Toolkit.Uwp.UI.Animations;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using Microsoft.Toolkit.Uwp.UI.Animations;
 using Quarrel.ViewModels.Models.Bindables;
 using Windows.UI.Xaml.Controls;
 
 namespace Quarrel.Controls.Channels
 {
     /// <summary>
-    /// Guild Button as header for ChannelList
+    /// Guild Button as header for ChannelList.
     /// </summary>
     public sealed partial class ChannelListHeader : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChannelListHeader"/> class.
+        /// </summary>
         public ChannelListHeader()
         {
             this.InitializeComponent();
@@ -21,20 +26,24 @@ namespace Quarrel.Controls.Channels
                 if (ViewModel != null)
                 {
                     if (ViewModel.Model.BannerUri == null)
+                    {
                         rootButton.Height = 48;
+                    }
                     else
+                    {
                         rootButton.Height = 64;
+                    }
                 }
             };
         }
 
         /// <summary>
-        /// Selected Guild
+        /// Gets the current Guild.
         /// </summary>
         public BindableGuild ViewModel => DataContext as BindableGuild;
 
         /// <summary>
-        /// Fades in Image when banner loads
+        /// Fades in Image when banner loads.
         /// </summary>
         private async void ImageEx_ImageExOpened(object sender, Microsoft.Toolkit.Uwp.UI.Controls.ImageExOpenedEventArgs e)
         {
