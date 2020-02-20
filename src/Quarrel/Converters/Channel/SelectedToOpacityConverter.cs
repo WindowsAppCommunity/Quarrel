73ce0255a5e98cd83cef05283e1379f3c20d07b6
Avishai Dernis <avid29@live.com>
2020-02-20 15:12:16 -0500

    SelectedToOpacityConverter StyleCop conformity

--- src/Quarrel/Converters/Channel/SelectedToOpacityConverter.cs
+++ src/Quarrel/Converters/Channel/SelectedToOpacityConverter.cs
@@ -4,15 +4,17 @@ using Windows.UI.Xaml.Data;
 namespace Quarrel.Converters.Channel
 {
     /// <summary>
-    /// 
+    /// A converter than returns <see langword="0.2"/> when <see langword="true"/> or <see langword="0.0"/> when <see langword="false"/>.
     /// </summary>
     public sealed class SelectedToOpacityConverter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             return (bool)value ? 0.2 : 0;
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
