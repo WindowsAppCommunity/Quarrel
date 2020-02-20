b52311d4fe5422317312455542c0f684ada2f753
Avishai Dernis <avid29@live.com>
2020-02-19 23:33:12 -0500

    RichPresenceControl StyleCop conformity

--- src/Quarrel/Controls/RichPresenceControl.xaml.cs
+++ src/Quarrel/Controls/RichPresenceControl.xaml.cs
@@ -1,26 +1,20 @@
 ﻿using DiscordAPI.Models;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Quarrel.Controls
 {
+    /// <summary>
+    /// Control to display a single Game.
+    /// </summary>
     public sealed partial class RichPresenceControl : UserControl
     {
+        private DispatcherTimer _timer = new DispatcherTimer();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RichPresenceControl"/> class.
+        /// </summary>
         public RichPresenceControl()
         {
             this.InitializeComponent();
@@ -28,26 +22,28 @@ namespace Quarrel.Controls
             {
                 if ((e.NewValue is Game game) && game.TimeStamps != null)
                 {
-                    Timer.Interval = new TimeSpan(10000);
-                    Timer.Tick += Timer_Tick;
-                    Timer.Start();
-                }else
+                    _timer.Interval = new TimeSpan(10000);
+                    _timer.Tick += Timer_Tick;
+                    _timer.Start();
+                }
+                else
                 {
-                    Timer.Tick -= Timer_Tick;
-                    Timer.Stop();
+                    _timer.Tick -= Timer_Tick;
+                    _timer.Stop();
                 }
 
                 this.Bindings.Update();
             };
         }
 
+        /// <summary>
+        /// Gets the game in view.
+        /// </summary>
+        public Game ViewModel => DataContext as Game;
+
         private void Timer_Tick(object sender, object e)
         {
             this.Bindings.Update();
         }
-
-        DispatcherTimer Timer = new DispatcherTimer();
-
-        public Game ViewModel => DataContext as Game;
     }
 }
