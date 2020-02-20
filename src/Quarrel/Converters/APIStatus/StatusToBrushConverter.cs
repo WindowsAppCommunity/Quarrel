bb5f3faaca2bec577894c111a1199d03da765524
Avishai Dernis <avid29@live.com>
2020-02-20 10:46:54 -0500

    StatusToBrushConverter

--- src/Quarrel/Converters/APIStatus/StatusToBrushConverter.cs
+++ src/Quarrel/Converters/APIStatus/StatusToBrushConverter.cs
@@ -1,12 +1,24 @@
-﻿using ColorThiefDotNet;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
 using System;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 
 namespace Quarrel.Converters.APIStatus
 {
+    /// <summary>
+    /// Converter for DiscordAPI operation status to a brush color.
+    /// </summary>
     public class StatusToBrushConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts DiscordAPI operation status to a brush color.
+        /// </summary>
+        /// <param name="value">DiscordAPI operational status as a string.</param>
+        /// <param name="targetType">Requested out type.</param>
+        /// <param name="parameter">Extra info.</param>
+        /// <param name="language">What language the user is using.</param>
+        /// <returns>A SolidColorBrush for the API status.</returns>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             string status = value.ToString();
@@ -25,6 +37,7 @@ namespace Quarrel.Converters.APIStatus
             }
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
