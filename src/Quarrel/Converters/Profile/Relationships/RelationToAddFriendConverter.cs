e7621822006f04ae4d90d826be4e3e91a09c46b8
Avishai Dernis <avid29@live.com>
2020-02-20 15:46:49 -0500

    RelationToAddFriendConverter StyleCop conformity

--- src/Quarrel/Converters/Profile/Relationships/RelationToAddFriendConverter.cs
+++ src/Quarrel/Converters/Profile/Relationships/RelationToAddFriendConverter.cs
@@ -1,20 +1,23 @@
-﻿using System;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Media;
 
 namespace Quarrel.Converters.Profile.Relationships
 {
     /// <summary>
-    /// A converter that returns an inverted <see cref="Visibility"/> value for the input <see langword="bool"/> value
+    /// A converter that returns <see cref="Visibility.Visible"/> if the user has no relation status.
     /// </summary>
     public sealed class RelationToAddFriendConverter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             return (int)value == 0 ? Visibility.Visible : Visibility.Collapsed;
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
