a368907348901ddec809c0a4134b6bbf48c655c9
Avishai Dernis <avid29@live.com>
2020-02-20 09:57:05 -0500

    FriendListControl StyleCop conformity

--- src/Quarrel/Controls/Shell/Views/FriendListControl.xaml.cs
+++ src/Quarrel/Controls/Shell/Views/FriendListControl.xaml.cs
@@ -1,21 +1,26 @@
-﻿using Quarrel.ViewModels;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using Quarrel.ViewModels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace Quarrel.Controls.Shell.Views
 {
     /// <summary>
-    /// Control for FriendsList page on DM Guild
+    /// Control for FriendsList page on DM Guild.
     /// </summary>
     public sealed partial class FriendListControl : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FriendListControl"/> class.
+        /// </summary>
         public FriendListControl()
         {
             this.InitializeComponent();
         }
 
         /// <summary>
-        /// Access app's main data
+        /// Gets the MainViewModel for the app.
         /// </summary>
         public MainViewModel ViewModel => App.ViewModelLocator.Main;
     }
