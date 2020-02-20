5e8be8c055264364e7bbc3b8b19083e9dcf8605b
Avishai Dernis <avid29@live.com>
2020-02-20 15:25:57 -0500

    PresenceToColorConverter StyleCop conformity

--- src/Quarrel/Converters/Discord/PresenseToColorConverter.cs
+++ src/Quarrel/Converters/Discord/PresenseToColorConverter.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using Windows.UI;
-using Windows.UI.Xaml;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using System;
 using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Media;
 
 namespace Quarrel.Converters.Discord
 {
     /// <summary>
-    /// A converter that returns an inverted <see cref="Visibility"/> value for the input <see langword="bool"/> value
+    /// A converter that returns a <see cref="Windows.UI.Color"/> based on a user's presence.
     /// </summary>
     public sealed class PresenseToColorConverter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return App.Current.Resources[((string)value ?? "offline")+ "Color"];
+            return App.Current.Resources[((string)value ?? "offline") + "Color"];
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
