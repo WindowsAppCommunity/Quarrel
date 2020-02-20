0161137c3a946c554265b884d870885136d048d0
Avishai Dernis <avid29@live.com>
2020-02-20 15:47:46 -0500

    RelationToBlockConverter StyleCop conformity

--- src/Quarrel/Converters/Profile/Relationships/RelationToBlockConverter.cs
+++ src/Quarrel/Converters/Profile/Relationships/RelationToBlockConverter.cs
@@ -1,20 +1,23 @@
-﻿using System;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Media;
 
 namespace Quarrel.Converters.Profile.Relationships
 {
     /// <summary>
-    /// A converter that returns an inverted <see cref="Visibility"/> value for the input <see langword="bool"/> value
+    /// A converter that returns a <see cref="Visibility"/> value if the user doesn't already have a blocked relation status.
     /// </summary>
     public sealed class RelationToBlockConverter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             return (int)value != 2 ? Visibility.Visible : Visibility.Collapsed;
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
