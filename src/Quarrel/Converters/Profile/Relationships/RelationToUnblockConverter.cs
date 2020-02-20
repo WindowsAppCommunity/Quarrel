b073bd2467d6c80a650e9e5e90da42e8da631c3d
Avishai Dernis <avid29@live.com>
2020-02-20 15:50:06 -0500

    RelationToUnblockConverter StyleCop conformity

--- src/Quarrel/Converters/Profile/Relationships/RelationToUnblockConverter.cs
+++ src/Quarrel/Converters/Profile/Relationships/RelationToUnblockConverter.cs
@@ -1,20 +1,23 @@
-﻿using System;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Media;
 
 namespace Quarrel.Converters.Profile.Relationships
 {
     /// <summary>
-    /// A converter that returns an inverted <see cref="Visibility"/> value for the input <see langword="bool"/> value
+    /// A converter that returns a <see cref="Visibility.Visible"/> if the user has a blocked relation status.
     /// </summary>
     public sealed class RelationToUnblockConverter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             return (int)value == 2 ? Visibility.Visible : Visibility.Collapsed;
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
