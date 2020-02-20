073bc990963b966c016e392eb46670917c170975
Avishai Dernis <avid29@live.com>
2020-02-20 15:14:40 -0500

    TypeToForegroundBrushConverter StyleCop conformity

--- src/Quarrel/Converters/Channel/TypeToForegroundBrushConverter.cs
+++ src/Quarrel/Converters/Channel/TypeToForegroundBrushConverter.cs
@@ -4,15 +4,17 @@ using Windows.UI.Xaml.Data;
 namespace Quarrel.Converters.Channel
 {
     /// <summary>
-    /// 
+    /// A converter than returns a foreground SolidColorBrush for the channel name based on type
     /// </summary>
-    public sealed class TypeToBrushConverter : IValueConverter
+    public sealed class TypeToForegroundBrushConverter : IValueConverter
     {
+        /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             return (int)value == 4 ? App.Current.Resources["SystemControlBackgroundAccentBrush"] : App.Current.Resources["Foreground"];
         }
 
+        /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
