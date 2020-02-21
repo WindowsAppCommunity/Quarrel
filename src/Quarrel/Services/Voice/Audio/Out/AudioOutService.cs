7facf8694f606fc30b0bfbe2f816447f0e567e35
Avishai Dernis <avid29@live.com>
2020-02-21 11:27:44 -0500

    AuditInService StyleCop conformity

--- src/Quarrel/Services/Voice/Audio/Out/AudioOutService.cs
+++ src/Quarrel/Services/Voice/Audio/Out/AudioOutService.cs
@@ -181,16 +181,4 @@ namespace Quarrel.Services.Voice.Audio.Out
 
         #endregion
     }
-
-    #region Dependencies
-
-    [ComImport]
-    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-
-    unsafe interface IMemoryBufferByteAccess
-    {
-        void GetBuffer(out byte* buffer, out uint capacity);
-    }
-    #endregion
 }
