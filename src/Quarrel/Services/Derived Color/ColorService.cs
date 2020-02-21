2ab2355a0118a97b0703c18b94b44e6bd5d8d774
Avishai Dernis <avid29@live.com>
2020-02-21 11:07:36 -0500

    ColorService StyleCop conformity

--- src/Quarrel/Services/Derived Color/ColorService.cs
+++ src/Quarrel/Services/Derived Color/ColorService.cs
@@ -11,30 +11,52 @@ using Windows.UI.Xaml.Media.Imaging;
 
 namespace Quarrel.Services.DerivedColor
 {
+    /// <summary>
+    /// A <see langword="class"/> that provides color parsing with Windows namespaces.
+    /// </summary>
     public class ColorService : IColorService
     {
-        SemaphoreSlim Mutex = new SemaphoreSlim(1, 1);
+        private SemaphoreSlim _mutex = new SemaphoreSlim(1, 1);
 
+        /// <summary>
+        /// Remembers aleady calculated user colors.
+        /// </summary>
+        private Dictionary<string, int> _userColorsCache = new Dictionary<string, int>();
+
+        /// <summary>
+        /// Gets an int form color for a user based on their discriminator and avatar.
+        /// </summary>
+        /// <param name="user">User color is for.</param>
+        /// <returns>An int form color.</returns>
         public async Task<int> GetUserColor(User user)
         {
-            await Mutex.WaitAsync();
+            await _mutex.WaitAsync();
             try
             {
-                if (string.IsNullOrEmpty(user.Avatar)) return ColorExtensions.GetDiscriminatorColor(user.Discriminator).ToInt();
+                if (string.IsNullOrEmpty(user.Avatar))
+                {
+                    return ColorExtensions.GetDiscriminatorColor(user.Discriminator).ToInt();
+                }
 
                 // TODO: Handle avatar change
-                if (userColorsCache.ContainsKey(user.Id)) return userColorsCache[user.Id];
+                if (_userColorsCache.ContainsKey(user.Id))
+                {
+                    return _userColorsCache[user.Id];
+                }
 
                 PictureAnalysis analysis = new PictureAnalysis();
                 try
                 {
                     await analysis.Analyse(new BitmapImage(user.AvatarUri), 128, 128);
                 }
-                catch { }
+                catch
+                {
+                }
+
                 if (analysis.ColorList.Count > 0)
                 {
-                    userColorsCache.Add(user.Id, analysis.ColorList[0].Color.ToInt());
-                    return userColorsCache[user.Id];
+                    _userColorsCache.Add(user.Id, analysis.ColorList[0].Color.ToInt());
+                    return _userColorsCache[user.Id];
                 }
                 else
                 {
@@ -43,15 +65,18 @@ namespace Quarrel.Services.DerivedColor
             }
             finally
             {
-                Mutex.Release();
+                _mutex.Release();
             }
         }
 
+        /// <summary>
+        /// Gets an int form color based on a user's status.
+        /// </summary>
+        /// <param name="status">User's status.</param>
+        /// <returns>An int color.</returns>
         public int GetStatusColor(string status)
         {
             return (App.Current.Resources[status ?? "offline"] as SolidColorBrush).Color.ToInt();
         }
-
-        Dictionary<string, int> userColorsCache = new Dictionary<string, int>();
     }
 }
