6c3332ba5c096490f5ec94d9bfd182b89d0ebae7
Avishai Dernis <avid29@live.com>
2020-02-20 20:14:45 -0500

    GuildMemberListItemTemplate StyleCop conformity

--- src/Quarrel/DataTemplates/Members/GuildMemberListItemTemplate.xaml.cs
+++ src/Quarrel/DataTemplates/Members/GuildMemberListItemTemplate.xaml.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Quarrel. All rights reserved.
 
 namespace Quarrel.DataTemplates.Members
 {
+    /// <summary>
+    /// A collection of Data Templates for Channel displaying.
+    /// </summary>
     public partial class GuildMemberListItemTemplate
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GuildMemberListItemTemplate"/> class.
+        /// </summary>
         public GuildMemberListItemTemplate()
         {
             this.InitializeComponent();
