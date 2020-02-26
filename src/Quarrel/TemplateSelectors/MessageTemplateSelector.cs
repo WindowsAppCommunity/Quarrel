0319f49fac333119e1e91ff881742c95f1eccd0b
Avishai Dernis <avid29@live.com>
2020-02-26 12:54:04 -0500

    MessageTemplateSelector StyleCop conformity

--- src/Quarrel/TemplateSelectors/MessageTemplateSelector.cs
+++ src/Quarrel/TemplateSelectors/MessageTemplateSelector.cs
@@ -1,24 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using Quarrel.ViewModels.Models.Bindables;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using DiscordAPI.Models;
-using Quarrel.ViewModels.Models.Bindables;
-using Microsoft.Advertising.WinRT.UI;
 
 namespace Quarrel.TemplateSelectors
 {
+    /// <summary>
+    /// A template selector for the message type.
+    /// </summary>
     public sealed class MessageTemplateSelector : DataTemplateSelector
     {
+        /// <summary>
+        /// Gets or sets the Message template.
+        /// </summary>
         public DataTemplate MessageTemplate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Action Message template.
+        /// </summary>
         public DataTemplate ActionMessageTemplate { get; set; }
 
+        /// <summary>
+        /// Selects a <see cref="DataTemplate"/> based on the details from <paramref name="item"/>.
+        /// </summary>
+        /// <param name="item">A <see cref="BindableMessage"/>.</param>
+        /// <param name="container">The parent of the resulting <see cref="DataTemplate"/>.</param>
+        /// <returns>A <see cref="DataTemplate"/> for the <paramref name="item"/>'s type.</returns>
         protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
         {
             if (container is FrameworkElement parent && item is BindableMessage msg)
             {
                 return msg.Model.Type == 0 ? MessageTemplate : ActionMessageTemplate;
             }
+
             return null;
         }
     }
