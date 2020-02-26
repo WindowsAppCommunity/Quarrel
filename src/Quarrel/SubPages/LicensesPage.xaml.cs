c7b62da6c2a6d0ce160036ec5dadb607ed27049b
Avishai Dernis <avid29@live.com>
2020-02-25 22:02:03 -0500

    LicesnsesPage StyleCop conformity

--- src/Quarrel/SubPages/LicensesPage.xaml.cs
+++ src/Quarrel/SubPages/LicensesPage.xaml.cs
@@ -1,67 +1,76 @@
-﻿using Quarrel.SubPages.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using Quarrel.SubPages.Interfaces;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Quarrel.SubPages
 {
+    /// <summary>
+    /// The sub page to show Quarrel and Quarrel's libraries licensing information.
+    /// </summary>
     public sealed partial class LicensesPage : UserControl, IConstrainedSubPage
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LicensesPage"/> class.
+        /// </summary>
         public LicensesPage()
         {
             this.InitializeComponent();
         }
 
+        /// <inheritdoc/>
+        public double MaxExpandedHeight { get; } = 512;
+
+        /// <inheritdoc/>
+        public double MaxExpandedWidth { get; } = 512;
+
         private void ToggleMIT(object sender, RoutedEventArgs e)
         {
             if (mitlicense.Visibility == Visibility.Visible)
+            {
                 mitlicense.Visibility = Visibility.Collapsed;
+            }
             else
+            {
                 mitlicense.Visibility = Visibility.Visible;
+            }
         }
 
         private void ToggleMSPL(object sender, RoutedEventArgs e)
         {
             if (mspllicense.Visibility == Visibility.Visible)
+            {
                 mspllicense.Visibility = Visibility.Collapsed;
+            }
             else
+            {
                 mspllicense.Visibility = Visibility.Visible;
+            }
         }
 
         private void ToggleConcentus(object sender, RoutedEventArgs e)
         {
             if (concentuslicense.Visibility == Visibility.Visible)
+            {
                 concentuslicense.Visibility = Visibility.Collapsed;
+            }
             else
+            {
                 concentuslicense.Visibility = Visibility.Visible;
+            }
         }
 
         private void ToggleAPACHEL(object sender, RoutedEventArgs e)
         {
             if (apachelicense.Visibility == Visibility.Visible)
+            {
                 apachelicense.Visibility = Visibility.Collapsed;
+            }
             else
+            {
                 apachelicense.Visibility = Visibility.Visible;
+            }
         }
-
-        #region IConstrainedSubPage
-
-        public double MaxExpandedHeight { get; } = 512;
-        public double MaxExpandedWidth { get; } = 512;
-
-        #endregion
     }
 }
