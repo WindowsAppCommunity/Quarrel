af936b38ce6d0fd3d85377d7b7064ab77daafd3f
Avishai Dernis <avid29@live.com>
2020-03-06 06:56:53 -0500

    MyAccountSettingsViewModel StyleCop conformity

--- src/Quarrel.ViewModels/SubPages/UserSettings/Pages/MyAccountSettingsViewModel.cs
+++ src/Quarrel.ViewModels/SubPages/UserSettings/Pages/MyAccountSettingsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using DiscordAPI.API.User.Models;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using DiscordAPI.API.User.Models;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Ioc;
@@ -7,86 +9,122 @@ using Quarrel.ViewModels.Services.Discord.Rest;
 
 namespace Quarrel.ViewModels.SubPages.UserSettings.Pages
 {
+    /// <summary>
+    /// MyAccount Settings page binding data.
+    /// </summary>
     public class MyAccountSettingsViewModel : ViewModelBase
     {
+        private string _email;
+        private string _username;
+        private bool _editingAccountInfo;
+        private string _newPassword;
+        private bool editingPassword;
+        private string base64Avatar = string.Empty;
+        private string avatarUrl;
+        private string password = null;
+        private RelayCommand enterAccountEditCommand;
+        private RelayCommand finalizeAccountEditCommand;
+        private RelayCommand cancelAccountEditCommand;
+        private RelayCommand enterPasswordEditCommand;
+        private RelayCommand deleteAvatar;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyAccountSettingsViewModel"/> class.
+        /// </summary>
         public MyAccountSettingsViewModel()
         {
             SetDefaults();
         }
 
-        public ICurrentUserService CurrentUsersService
-        {
-            get => SimpleIoc.Default.GetInstance<ICurrentUserService>();
-        }
-
-        private string email;
+        /// <summary>
+        /// Gets or sets the pending change in Email.
+        /// </summary>
         public string Email
         {
-            get => email;
-            set => Set(ref email, value);
+            get => _email;
+            set => Set(ref _email, value);
         }
 
-        private string username;
+        /// <summary>
+        /// Gets or sets the pending change in username.
+        /// </summary>
         public string Username
         {
-            get => username;
-            set => Set(ref username, value);
+            get => _username;
+            set => Set(ref _username, value);
         }
 
-        private bool editingAccountInfo;
+        /// <summary>
+        /// Gets or sets a value indicating whether account info is being modified.
+        /// </summary>
         public bool EditingAccountInfo
         {
-            get => editingAccountInfo;
-            set => Set(ref editingAccountInfo, value);
+            get => _editingAccountInfo;
+            set => Set(ref _editingAccountInfo, value);
         }
 
-        private string newPassword;
+        /// <summary>
+        /// Gets or sets the pending new password.
+        /// </summary>
         public string NewPassword
         {
-            get => newPassword;
-            set => Set(ref newPassword, value);
+            get => _newPassword;
+            set => Set(ref _newPassword, value);
         }
 
-
-        private bool editingPassword;
+        /// <summary>
+        /// Gets or sets a value indicating whether the password is being modified.
+        /// </summary>
         public bool EditingPassword
         {
             get => editingPassword;
             set => Set(ref editingPassword, value);
         }
 
-
-        private string base64Avatar = "";
+        /// <summary>
+        /// Gets or sets the Base64 string Avatar used to upload.
+        /// </summary>
+        /// <remarks>Empty string if unchanged.</remarks>
         public string Base64Avatar
         {
             get => base64Avatar;
             set => Set(ref base64Avatar, value);
         }
 
-        private string avatarUrl;
+        /// <summary>
+        /// Gets or sets the URL of the User's Avatar.
+        /// </summary>
         public string AvatarUrl
         {
             get => avatarUrl;
             set => Set(ref avatarUrl, value);
         }
 
-        public bool EditingAvatar => Base64Avatar != "";
+        /// <summary>
+        /// Gets a value indicating whether the Avatar has a pending change.
+        /// </summary>
+        public bool EditingAvatar => Base64Avatar != string.Empty;
 
-        private string password = null;
+        /// <summary>
+        /// Gets or sets the password used to authenticate user edits.
+        /// </summary>
         public string Password
         {
             get => password;
             set => Set(ref password, value);
         }
 
-
-        private RelayCommand enterAccountEditCommand;
+        /// <summary>
+        /// Gets a command to enter editing mode.
+        /// </summary>
         public RelayCommand EnterAccountEditCommand => enterAccountEditCommand = new RelayCommand(() =>
         {
             EditingAccountInfo = true;
         });
 
-        private RelayCommand finalizeAccountEditCommand;
+        /// <summary>
+        /// Gets a command to save changes and leaves editing mode.
+        /// </summary>
         public RelayCommand FinalizeAccountEditCommand => finalizeAccountEditCommand = new RelayCommand(() =>
         {
             ApplyChanges();
@@ -94,7 +132,9 @@ namespace Quarrel.ViewModels.SubPages.UserSettings.Pages
             EditingPassword = false;
         });
 
-        private RelayCommand cancelAccountEditCommand;
+        /// <summary>
+        /// Gets a command to cancel editing mode.
+        /// </summary>
         public RelayCommand CancelAccountEditCommand => cancelAccountEditCommand = new RelayCommand(() =>
         {
             SetDefaults();
@@ -102,18 +142,27 @@ namespace Quarrel.ViewModels.SubPages.UserSettings.Pages
             EditingPassword = false;
         });
 
-        private RelayCommand enterPasswordEditCommand;
+        /// <summary>
+        /// Gets a command to enter password editing mode.
+        /// </summary>
         public RelayCommand EnterPasswordEditCommand => enterPasswordEditCommand = new RelayCommand(() =>
         {
             EditingPassword = true;
         });
 
-        private RelayCommand deleteAvatar;
+        /// <summary>
+        /// Gets a command to adds Deleted Avatar to pending changes.
+        /// </summary>
         public RelayCommand DeleteAvatar => deleteAvatar = new RelayCommand(() =>
         {
             Base64Avatar = null;
         });
 
+        private ICurrentUserService CurrentUsersService => SimpleIoc.Default.GetInstance<ICurrentUserService>();
+
+        /// <summary>
+        /// Restores unedited values.
+        /// </summary>
         public void SetDefaults()
         {
             Email = CurrentUsersService.CurrentUser.Model.Email;
@@ -121,24 +170,32 @@ namespace Quarrel.ViewModels.SubPages.UserSettings.Pages
             AvatarUrl = CurrentUsersService.CurrentUser.Model.AvatarUrl;
         }
 
+        /// <summary>
+        /// Saves pending changes to the user.
+        /// </summary>
         public async void ApplyChanges()
         {
             ModifyUser modify = new ModifyUser();
             if (EditingAvatar)
+            {
                 modify = new ModifyUserAndAvatar()
                 {
                     Username = Username,
                     Avatar = Base64Avatar,
-                    NewPassword = newPassword,
-                    Password = password
+                    NewPassword = _newPassword,
+                    Password = password,
                 };
+            }
             else
+            {
                 modify = new ModifyUser()
                 {
                     Username = Username,
-                    NewPassword = newPassword,
-                    Password = password
+                    NewPassword = _newPassword,
+                    Password = password,
                 };
+            }
+
             CurrentUsersService.CurrentUser.Model = await SimpleIoc.Default.GetInstance<IDiscordService>().UserService.ModifyCurrentUser(modify);
         }
     }
