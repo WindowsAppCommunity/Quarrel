ea1fe1e042d2ef9f623759707a19b04645ee0259
Avishai Dernis <avid29@live.com>
2020-03-06 20:41:46 -0500

    Grouping StyleCop conformity

--- src/Quarrel.ViewModels/Extensions/System.Collections.ObjectModel/Grouping.cs
+++ src/Quarrel.ViewModels/Extensions/System.Collections.ObjectModel/Grouping.cs
@@ -1,43 +1,44 @@
-﻿using JetBrains.Annotations;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using JetBrains.Annotations;
+using Quarrel.ViewModels.Extensions.System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace System.Collections.ObjectModel
 {
     /// <summary>
-    /// A custom <see cref="ObservableCollection{T}"/> that implements the <see cref="IGrouping{TKey,TElement}"/> <see langword="interface"/>
+    /// A custom <see cref="ObservableCollection{T}"/> that implements the <see cref="IGrouping{TKey,TElement}"/> <see langword="interface"/>.
     /// </summary>
-    /// <typeparam name="TKey">They type of the key of each group</typeparam>
-    /// <typeparam name="TElement">The type of elements in each group</typeparam>
-    public class Grouping<TKey, TElement> : ObservableCollection<TElement>, IGrouping<TKey, TElement>, Grouping
+    /// <typeparam name="TKey">They type of the key of each group.</typeparam>
+    /// <typeparam name="TElement">The type of elements in each group.</typeparam>
+    public class Grouping<TKey, TElement> : ObservableCollection<TElement>, IGrouping<TKey, TElement>, IGrouping
     {
         /// <summary>
-        /// Gets the key associated with the current group
+        /// Initializes a new instance of the <see cref="Grouping{TKey, TElement}"/> class.
         /// </summary>
-        public TKey Key { get; }
-
-        object Grouping.Key => Key;
-
-        /// <summary>
-        /// Creates a new <see cref="Grouping{TKey, TElement}"/> instance with the specified key
-        /// </summary>
-        /// <param name="key">They key to use in the new instance</param>
+        /// <param name="key">They key to use in the new instance.</param>
         public Grouping(TKey key) => Key = key;
 
         /// <summary>
-        /// Creates a new <see cref="Grouping{TKey, TElement}"/> instance with the specified key and items collection
+        /// Initializes a new instance of the <see cref="Grouping{TKey, TElement}"/> class instance with the specified key and items collection.
         /// </summary>
-        /// <param name="key">The key to use in the new instance</param>
-        /// <param name="items">The initial collection of items to insert in the collection</param>
+        /// <param name="key">The key to use in the new instance.</param>
+        /// <param name="items">The initial collection of items to insert in the collection.</param>
         public Grouping(TKey key, [NotNull, ItemNotNull] IEnumerable<TElement> items) : this(key)
         {
-            foreach (TElement item in items) Add(item);
+            foreach (TElement item in items)
+            {
+                Add(item);
+            }
         }
-    }
-    public interface Grouping
-    {
-        object Key { get; }
-        int Count { get; }
 
+        /// <summary>
+        /// Gets the key associated with the current group.
+        /// </summary>
+        public TKey Key { get; }
+
+        /// <inheritdoc/>
+        object IGrouping.Key => Key;
     }
 }
