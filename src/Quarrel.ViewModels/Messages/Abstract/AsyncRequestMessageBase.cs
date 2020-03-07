8a65c945d3022f2707c5028df42def2e0b5c8679
Avishai Dernis <avid29@live.com>
2020-03-07 10:27:45 -0500

    RequestMessage StyleCop conformity

--- src/Quarrel.ViewModels/Messages/Abstract/AsyncRequestMessageBase.cs
+++ src/Quarrel.ViewModels/Messages/Abstract/AsyncRequestMessageBase.cs
@@ -1,31 +1,38 @@
-﻿using System.Threading.Tasks;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
 using JetBrains.Annotations;
+using System.Threading.Tasks;
 
 namespace Quarrel.ViewModels.Messages.Abstract
 {
     /// <summary>
-    /// A base <see langword="class"/> for an asynchronous request message
+    /// A base <see langword="class"/> for an asynchronous request message.
     /// </summary>
+    /// <typeparam name="TRequest">The type of request.</typeparam>
+    /// <typeparam name="TResponse">The type of response.</typeparam>
     public abstract class AsyncRequestMessageBase<TRequest, TResponse> : ValueChangedMessageBase<TRequest>
     {
-        /// <summary>
-        /// Creates a new instance with the given value
-        /// </summary>
-        /// <param name="value">The changed value</param>
-        protected AsyncRequestMessageBase(TRequest value) : base(value) { }
-
         // Private completion source to signal the autosave completion
-        private readonly TaskCompletionSource<TResponse> CompletionSource = new TaskCompletionSource<TResponse>();
+        private readonly TaskCompletionSource<TResponse> _completionSource = new TaskCompletionSource<TResponse>();
 
         /// <summary>
-        /// Gets a <see cref="Task{T}"/> that indicates the result of the current request message
+        /// Initializes a new instance of the <see cref="AsyncRequestMessageBase{TRequest, TResponse}"/> class.
+        /// </summary>
+        /// <param name="value">The changed value.</param>
+        protected AsyncRequestMessageBase(TRequest value) : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Gets a <see cref="Task{T}"/> that indicates the result of the current request message.
         /// </summary>
         [NotNull]
-        public Task<TResponse> Task => CompletionSource.Task;
+        public Task<TResponse> Task => _completionSource.Task;
 
         /// <summary>
-        /// Reports a result for the current request message
+        /// Reports a result for the current request message.
         /// </summary>
-        public void ReportResult(TResponse result) => CompletionSource.TrySetResult(result);
+        /// <param name="result">Result of request.</param>
+        public void ReportResult(TResponse result) => _completionSource.TrySetResult(result);
     }
 }
