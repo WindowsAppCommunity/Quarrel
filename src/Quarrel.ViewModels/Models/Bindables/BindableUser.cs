9f951ffc5911f0d3a166f63c6e41c9cbb72ac729
Avishai Dernis <avid29@live.com>
2020-03-11 10:42:50 -0400

    BindableUser StyleCop conformity

--- src/Quarrel.ViewModels/Models/Bindables/BindableUser.cs
+++ src/Quarrel.ViewModels/Models/Bindables/BindableUser.cs
@@ -1,45 +1,52 @@
-﻿using DiscordAPI.Models;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using DiscordAPI.Models;
 using JetBrains.Annotations;
 using Quarrel.ViewModels.Models.Bindables.Abstract;
 using System;
 
 namespace Quarrel.ViewModels.Models.Bindables
 {
+    /// <summary>
+    /// A Bindable wrapper for the <see cref="User"/> model.
+    /// </summary>
     public class BindableUser : BindableModelBase<User>, IEquatable<BindableUser>, IComparable<BindableUser>
     {
-        #region Constructors
+        private Presence _presence;
 
-        public BindableUser([NotNull] User model) : base(model) { }
-
-        #endregion
-
-        #region Properties
-
-        private Presence presence;
-        public Presence Presence
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BindableUser"/> class.
+        /// </summary>
+        /// <param name="model">The base <see cref="User"/> object.</param>
+        public BindableUser([NotNull] User model) : base(model)
         {
-            get => presence;
-            set => Set(ref presence, value);
         }
 
-        #endregion
+        /// <summary>
+        /// Gets or sets the user's presence.
+        /// </summary>
+        public Presence Presence
+        {
+            get => _presence;
+            set => Set(ref _presence, value);
+        }
 
-        #region Interfaces
+        /// <inheritdoc/>
         public bool Equals(BindableUser other)
         {
             return Model.Id == other.Model.Id;
         }
 
+        /// <inheritdoc/>
         public int CompareTo(BindableUser other)
         {
             return Model.Id.CompareTo(other.Model.Id);
         }
 
+        /// <inheritdoc/>
         public override int GetHashCode()
         {
             return Model.Id.GetHashCode();
         }
-
-        #endregion
     }
 }
