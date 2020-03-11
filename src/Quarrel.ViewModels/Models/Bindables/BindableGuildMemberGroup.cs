78040b9a9ad174034ff4820c33f42b3158bd6409
Avishai Dernis <avid29@live.com>
2020-03-11 09:11:12 -0400

    BindableGuildMemberGroup StyleCop conformity

--- src/Quarrel.ViewModels/Models/Bindables/BindableGuildMemberGroup.cs
+++ src/Quarrel.ViewModels/Models/Bindables/BindableGuildMemberGroup.cs
@@ -1,4 +1,6 @@
-﻿using DiscordAPI.Models;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using DiscordAPI.Models;
 using GalaSoft.MvvmLight.Ioc;
 using JetBrains.Annotations;
 using Quarrel.ViewModels.Models.Bindables.Abstract;
@@ -8,17 +10,32 @@ using System.Linq;
 
 namespace Quarrel.ViewModels.Models.Bindables
 {
+    /// <summary>
+    /// A Bindable wrapper for the <see cref="Group"/> model.
+    /// </summary>
     public class BindableGuildMemberGroup : BindableModelBase<Group>, IGuildMemberListItem
     {
-        private IGuildsService GuildsService = SimpleIoc.Default.GetInstance<IGuildsService>();
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BindableGuildMemberGroup"/> class.
+        /// </summary>
+        /// <param name="model">The base <see cref="Group"/> object.</param>
         public BindableGuildMemberGroup([NotNull] Group model) : base(model)
         {
         }
 
+        /// <summary>
+        /// Gets the number of members in the group.
+        /// </summary>
         public int Count => Model.Count;
 
+        /// <summary>
+        /// Gets the role of the group.
+        /// </summary>
         public Role Role => GuildsService.CurrentGuild.Model.Roles.FirstOrDefault(x => x.Id == Model.Id);
 
+        /// <summary>
+        /// Gets the name of the group.
+        /// </summary>
         public string Name
         {
             get
@@ -34,5 +51,7 @@ namespace Quarrel.ViewModels.Models.Bindables
                 }
             }
         }
+
+        private IGuildsService GuildsService => SimpleIoc.Default.GetInstance<IGuildsService>();
     }
 }
