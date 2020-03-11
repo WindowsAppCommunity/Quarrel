6255431c9f6f9459a1b0aed3b861db015e4af6e3
Avishai Dernis <avid29@live.com>
2020-03-11 08:06:19 -0400

    BindableFriend StyleCop conformity

--- src/Quarrel.ViewModels/Models/Bindables/BindableFriend.cs
+++ src/Quarrel.ViewModels/Models/Bindables/BindableFriend.cs
@@ -1,32 +1,47 @@
-﻿using DiscordAPI.Models;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using DiscordAPI.Models;
 using JetBrains.Annotations;
 using Quarrel.ViewModels.Models.Bindables.Abstract;
 
 namespace Quarrel.ViewModels.Models.Bindables
 {
+    /// <summary>
+    /// A Bindable wrapper for the <see cref="Friend"/> model.
+    /// </summary>
     public class BindableFriend : BindableModelBase<Friend>
     {
-        #region Constructors
-        public BindableFriend([NotNull] Friend friend) : base(friend) { }
-        
-        #endregion
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BindableFriend"/> class.
+        /// </summary>
+        /// <param name="friend">The base friend object.</param>
+        public BindableFriend([NotNull] Friend friend) : base(friend)
+        {
+        }
 
-        #region Properties
-
-        #region FriendType
-
+        /// <summary>
+        /// Gets a value indicating whether or not the user has no friend status.
+        /// </summary>
         public bool NotFriend => Model.Type == 0;
 
+        /// <summary>
+        /// Gets a value indicating whether or not the user is a friend.
+        /// </summary>
         public bool IsFriend => Model.Type == 1;
 
+        /// <summary>
+        /// Gets a value indicating whether or not the user is blocked.
+        /// </summary>
         public bool IsBlocked => Model.Type == 2;
 
+        /// <summary>
+        /// Gets a value indicating whether or not the user has an incoming friend request.
+        /// </summary>
         public bool IsIncoming => Model.Type == 3;
 
+        /// <summary>
+        /// Gets a value indicating whether or not the user has an outgoing friend request.
+        /// </summary>
         public bool IsOutgoing => Model.Type == 4;
-
-        #endregion
-
-        #endregion
     }
 }
