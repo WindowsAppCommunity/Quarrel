0e733825cc1b50ec2742d811a1a9c2a0d95371a2
Avishai Dernis <avid29@live.com>
2020-03-11 13:07:54 -0400

    CurrentUsersService StyleCop conformity

--- src/Quarrel.ViewModels/Services/Discord/CurrentUser/ICurrentUserService.cs
+++ src/Quarrel.ViewModels/Services/Discord/CurrentUser/ICurrentUserService.cs
@@ -1,14 +1,23 @@
-﻿using DiscordAPI.Models;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using DiscordAPI.Models;
 using Quarrel.ViewModels.Models.Bindables;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 
 namespace Quarrel.ViewModels.Services.Discord.CurrentUser
 {
+    /// <summary>
+    /// Manages the all information directly pertaining to the current user.
+    /// </summary>
     public interface ICurrentUserService
     {
+        /// <summary>
+        /// Gets the current user as a <see cref="BindableUser"/>.
+        /// </summary>
         BindableUser CurrentUser { get; }
 
+        /// <summary>
+        /// Gets the current user's discord settings.
+        /// </summary>
         UserSettings CurrentUserSettings { get; }
     }
 }
