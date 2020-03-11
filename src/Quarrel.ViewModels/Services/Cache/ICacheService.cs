b35054d6563e2607fc05a44c3e77dcc7e5484582
Avishai Dernis <avid29@live.com>
2020-03-11 12:49:36 -0400

    ICacheService StyleCop conformity

--- src/Quarrel.ViewModels/Services/Cache/ICacheService.cs
+++ src/Quarrel.ViewModels/Services/Cache/ICacheService.cs
@@ -1,4 +1,5 @@
 ﻿// Special thanks to Sergio Pedri for the basis of this design
+// Copyright (c) Quarrel. All rights reserved.
 
 using JetBrains.Annotations;
 using Quarrel.ViewModels.Services.Cache.Persistent;
@@ -7,18 +8,18 @@ using Quarrel.ViewModels.Services.Cache.Runtime;
 namespace Quarrel.ViewModels.Services.Cache
 {
     /// <summary>
-    /// The default <see langword="interface"/> for a comprehensive cache service, both runtime and persistent
+    /// The default <see langword="interface"/> for a comprehensive cache service, both runtime and persistent.
     /// </summary>
     public interface ICacheService
     {
         /// <summary>
-        /// Gets the <see cref="IRuntimeCacheService"/> instance that only stores items at runtime
+        /// Gets the <see cref="IRuntimeCacheService"/> instance that only stores items at runtime.
         /// </summary>
         [NotNull]
         IRuntimeCacheService Runtime { get; }
 
         /// <summary>
-        /// Gets the <see cref="IPersistentCacheService"/> instance that stores items in a persistent way
+        /// Gets the <see cref="IPersistentCacheService"/> instance that stores items in a persistent way.
         /// </summary>
         [NotNull]
         IPersistentCacheService Persistent { get; }
