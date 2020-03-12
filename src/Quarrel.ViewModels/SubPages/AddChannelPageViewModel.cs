5c3e5e7830dbdb877833002aa02ad6774e5a7c8e
Avishai Dernis <avid29@live.com>
2020-03-12 12:09:37 -0400

    AddChannelPageViewModel StyleCop conformity

--- src/Quarrel.ViewModels/SubPages/AddChannelPageViewModel.cs
+++ src/Quarrel.ViewModels/SubPages/AddChannelPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using DiscordAPI.API.Guild.Models;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using DiscordAPI.API.Guild.Models;
 using DiscordAPI.Models;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
@@ -8,80 +10,72 @@ using Quarrel.ViewModels.Services.Navigation;
 
 namespace Quarrel.ViewModels.SubPages
 {
+    /// <summary>
+    /// Add Channel page data.
+    /// </summary>
     public class AddChannelPageViewModel : ViewModelBase
     {
-        #region Constructors
+        private Guild _guild;
+        private RelayCommand _backCommand;
+        private RelayCommand _createChannelCommand;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddChannelPageViewModel"/> class.
+        /// </summary>
         public AddChannelPageViewModel()
         {
             if (SubFrameNavigationService.Parameter != null)
             {
-                guild = (Guild)SubFrameNavigationService.Parameter;
+                _guild = (Guild)SubFrameNavigationService.Parameter;
             }
         }
 
-        #endregion
-
-        #region Properties
-
-        #region Services
-
-        private ISubFrameNavigationService SubFrameNavigationService { get; } =
-            SimpleIoc.Default.GetInstance<ISubFrameNavigationService>();
-
-        #endregion
-
+        /// <summary>
+        /// Gets or sets the name of the drafted channel.
+        /// </summary>
         public string ChannelName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the type of the drafted channel.
+        /// </summary>
         public string SelectedChannelType { get; set; } = "Text Channel";
 
-        private Guild guild;
+        /// <summary>
+        /// Gets a command that creates the drafted channel.
+        /// </summary>
+        public RelayCommand CreateChannelCommand => _createChannelCommand = _createChannelCommand ?? new RelayCommand(() =>
+        {
+            CreateGuildChannel createChannel = null;
 
-        #endregion
-
-        #region Commands
-
-        private RelayCommand createChannelCommand;
-        public RelayCommand CreateChannelCommand =>
-            createChannelCommand = createChannelCommand ?? new RelayCommand(() =>
+            switch (SelectedChannelType)
             {
-                CreateGuildChannel createChannel = null;
+                case "Text Channel":
+                    createChannel = new CreateGuildChannel { Type = 0 };
+                    break;
 
-                switch (SelectedChannelType)
-                {
-                    case "Text Channel":
-                    {
-                        createChannel = new CreateGuildChannel {Type = 0};
-                    }
-                        break;
+                case "Voice Channel":
+                    // TODO: Add options for bitrate and userlimit
+                    createChannel = new CreateVoiceChannel { Type = 2, Bitrate = 64000, UserLimit = 0 };
+                    break;
+                default:
+                    return;
+            }
 
-                    case "Voice Channel":
-                    {
-                        // Todo: Add options for bitrate and userlimit
-                        createChannel = new CreateVoiceChannel {Type = 2, Bitrate= 64000, UserLimit = 0};
-                    }
-                        break;
-                    default:
-                        return;
+            createChannel.Name = ChannelName;
 
-                }
+            SimpleIoc.Default.GetInstance<IDiscordService>().GuildService
+                .CreateGuildChannel(_guild.Id, createChannel);
+            SubFrameNavigationService.GoBack();
+        });
 
+        /// <summary>
+        /// Gets a command that closes the page.
+        /// </summary>
+        public RelayCommand BackCommand => _backCommand = _backCommand ?? new RelayCommand(() =>
+        {
+            SubFrameNavigationService.GoBack();
+        });
 
-                createChannel.Name = ChannelName;
-
-                SimpleIoc.Default.GetInstance<IDiscordService>().GuildService
-                    .CreateGuildChannel(guild.Id, createChannel);
-                SubFrameNavigationService.GoBack();
-            });
-
-
-        private RelayCommand backCommand;
-        public RelayCommand BackCommand =>
-            backCommand = backCommand ?? new RelayCommand(() =>
-            {
-                SubFrameNavigationService.GoBack();
-            });
-
-        #endregion
+        private ISubFrameNavigationService SubFrameNavigationService => SimpleIoc.Default.GetInstance<ISubFrameNavigationService>();
     }
 }
