6f273a697192321f280fed9a5f7e2528ed0357b6
Avishai Dernis <avid29@live.com>
2020-03-12 11:29:12 -0400

    OverviewSettingsPageViewModel StyleCop conformity

--- src/Quarrel.ViewModels/SubPages/GuildSettings/Pages/OverviewSettingsPageViewModel.cs
+++ src/Quarrel.ViewModels/SubPages/GuildSettings/Pages/OverviewSettingsPageViewModel.cs
@@ -1,48 +1,35 @@
-﻿using DiscordAPI.API.Guild.Models;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using DiscordAPI.API.Guild.Models;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Ioc;
 using Quarrel.ViewModels.Models.Bindables;
 using Quarrel.ViewModels.Services.Discord.Rest;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Quarrel.ViewModels.SubPages.GuildSettings.Pages
 {
+    /// <summary>
+    /// Overview settings page data.
+    /// </summary>
     public class OverviewSettingsPageViewModel : ViewModelBase
     {
-        #region Constructor
+        private BindableGuild _guild;
+        private RelayCommand _deleteIcon;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OverviewSettingsPageViewModel"/> class.
+        /// </summary>
+        /// <param name="guild">The guild to modify.</param>
         public OverviewSettingsPageViewModel(BindableGuild guild)
         {
             Guild = guild;
         }
 
-        #endregion
-
-        #region Methods
-
-        public async void UpdateIcon(string base64Icon)
-        {
-            ModifyGuildIcon modify = new ModifyGuildIcon(Guild.Model) { Icon = base64Icon };
-            try
-            {
-                var guild = await SimpleIoc.Default.GetInstance<IDiscordService>().GuildService.ModifyGuild(Guild.Model.Id, modify);
-                Guild.Model.Icon = guild.Icon;
-                Guild.RaisePropertyChanged(nameof(Guild.HasIcon));
-                Guild.RaisePropertyChanged(nameof(Guild.IconUrl));
-            }
-            // Mainly for Rate Limit
-            catch { }
-        }
-
-        #endregion
-
-        #region Commands
-
-        private RelayCommand deleteIcon;
-        public RelayCommand DeleteIcon => deleteIcon = new RelayCommand(async () =>
+        /// <summary>
+        /// Gets a command that deletes the icon for the guild.
+        /// </summary>
+        public RelayCommand DeleteIcon => _deleteIcon = new RelayCommand(async () =>
         {
             ModifyGuildIcon modify = new ModifyGuildIcon(Guild.Model) { Icon = null };
             await SimpleIoc.Default.GetInstance<IDiscordService>().GuildService.ModifyGuild(Guild.Model.Id, modify);
@@ -50,20 +37,22 @@ namespace Quarrel.ViewModels.SubPages.GuildSettings.Pages
             Guild.Model.Icon = null;
             Guild.RaisePropertyChanged(nameof(Guild.HasIcon));
             Guild.RaisePropertyChanged(nameof(Guild.IconUrl));
+
             // TODO: Handle Guild Updated
         });
 
-        #endregion
-
-        #region Properties
-
+        /// <summary>
+        /// Gets or sets the guild being modified.
+        /// </summary>
         public BindableGuild Guild
         {
-            get => _Guild;
-            set => Set(ref _Guild, value);
+            get => _guild;
+            set => Set(ref _guild, value);
         }
-        private BindableGuild _Guild;
 
+        /// <summary>
+        /// Gets or sets the name of the guild.
+        /// </summary>
         public string Name
         {
             get => Guild.Model.Name;
@@ -74,10 +63,29 @@ namespace Quarrel.ViewModels.SubPages.GuildSettings.Pages
                 Guild.Model.Name = value;
                 Guild.RaisePropertyChanged(nameof(Guild.Model));
                 Guild.RaisePropertyChanged(nameof(Guild.DisplayText));
+
                 // TODO: Handle Guild Updated
             }
         }
 
-        #endregion
+        /// <summary>
+        /// Updates the icon for the guild.
+        /// </summary>
+        /// <param name="base64Icon">The new icon in base64 string format.</param>
+        public async void UpdateIcon(string base64Icon)
+        {
+            ModifyGuildIcon modify = new ModifyGuildIcon(Guild.Model) { Icon = base64Icon };
+            try
+            {
+                var guild = await SimpleIoc.Default.GetInstance<IDiscordService>().GuildService.ModifyGuild(Guild.Model.Id, modify);
+                Guild.Model.Icon = guild.Icon;
+                Guild.RaisePropertyChanged(nameof(Guild.HasIcon));
+                Guild.RaisePropertyChanged(nameof(Guild.IconUrl));
+            }
+            catch
+            {
+                // Mainly for Rate Limit
+            }
+        }
     }
 }
