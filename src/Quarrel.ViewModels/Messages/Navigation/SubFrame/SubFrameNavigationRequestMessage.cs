2e4babe57377ed731409e2a08a4c5a4013720194
Avishai Dernis <avid29@live.com>
2020-03-13 10:18:59 -0400

    SubFrame messages StyleCop conformity

--- src/Quarrel.ViewModels/Messages/Navigation/SubFrame/SubFrameNavigationRequestMessage.cs
+++ src/Quarrel.ViewModels/Messages/Navigation/SubFrame/SubFrameNavigationRequestMessage.cs
@@ -1,25 +1,33 @@
-﻿using JetBrains.Annotations;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using JetBrains.Annotations;
 
 namespace Quarrel.ViewModels.Messages.Navigation.SubFrame
 {
     /// <summary>
-    /// A message that signals a request to display a page in the subframe UI
+    /// A message that signals a request to display a page in the subframe UI.
     /// </summary>
     public sealed class SubFrameNavigationRequestMessage
     {
         /// <summary>
-        /// Gets the page to display
+        /// Initializes a new instance of the <see cref="SubFrameNavigationRequestMessage"/> class.
+        /// </summary>
+        /// <param name="subPage">The page to navigate to.</param>
+        private SubFrameNavigationRequestMessage([NotNull] object subPage) => SubPage = subPage;
+
+        /// <summary>
+        /// Gets the page to display.
         /// </summary>
         [NotNull]
         public object SubPage { get; }
 
-        private SubFrameNavigationRequestMessage([NotNull] object subPage) => SubPage = subPage;
-
         /// <summary>
-        /// Creates a new request message to the target sub page type
+        /// Creates a new request message to the target sub page type.
         /// </summary>
-        /// <param name="subPage">The secondary page to display</param>
-        [Pure, NotNull]
+        /// <param name="subPage">The secondary page to display.</param>
+        /// <returns>Creates a new <see cref="SubFrameNavigationRequestMessage"/>.</returns>
+        [Pure]
+        [NotNull]
         public static SubFrameNavigationRequestMessage To([NotNull] object subPage) => new SubFrameNavigationRequestMessage(subPage);
     }
 }
