ab3f10e9803c5786abeca2f984f17e7d533530de
Avishai Dernis <avid29@live.com>
2020-03-13 19:41:32 -0400

    GatewayNoteUpdatedMessage StyleCop conformity

--- src/Quarrel.ViewModels/Messages/Gateway/GatewayNoteUpdatedMessage.cs
+++ src/Quarrel.ViewModels/Messages/Gateway/GatewayNoteUpdatedMessage.cs
@@ -1,18 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Quarrel. All rights reserved.
 
 namespace Quarrel.ViewModels.Messages.Gateway
 {
+    /// <summary>
+    /// A message that indicates a note has been updated.
+    /// </summary>
     public sealed class GatewayNoteUpdatedMessage
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GatewayNoteUpdatedMessage"/> class.
+        /// </summary>
+        /// <param name="userId">The id of the user who's note updated.</param>
         public GatewayNoteUpdatedMessage(string userId)
         {
             UserId = userId;
         }
 
+        /// <summary>
+        /// Gets the id of the user who's note updated.
+        /// </summary>
         public string UserId { get; }
+
+        /// <summary>
+        /// Gets the new note.
+        /// </summary>
+        public string Note { get; }
     }
 }
