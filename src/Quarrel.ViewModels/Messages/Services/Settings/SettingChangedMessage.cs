beeec037cd14ab9b0ef3eb30267c2a98254f09cf
Avishai Dernis <avid29@live.com>
2020-03-13 10:15:22 -0400

    SettingsChangedMessage StyleCop conformity

--- src/Quarrel.ViewModels/Messages/Services/Settings/SettingChangedMessage.cs
+++ src/Quarrel.ViewModels/Messages/Services/Settings/SettingChangedMessage.cs
@@ -1,27 +1,34 @@
-﻿using Quarrel.ViewModels.Services.Settings;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using Quarrel.ViewModels.Services.Settings;
 
 namespace Quarrel.ViewModels.Messages.Services.Settings
 {
     /// <summary>
-    /// A message that notifies whenever a given setting has changed
+    /// A message that notifies whenever a given setting has changed.
     /// </summary>
-    /// <typeparam name="T">The setting type</typeparam>
+    /// <typeparam name="T">The setting type.</typeparam>
     public sealed class SettingChangedMessage<T>
     {
         /// <summary>
-        /// Gets the key of the setting that has changed
+        /// Initializes a new instance of the <see cref="SettingChangedMessage{T}"/> class.
         /// </summary>
-        public SettingKeys Key { get; }
-
-        /// <summary>
-        /// Gets the updated setting value
-        /// </summary>
-        public T Value { get; }
-
+        /// <param name="key">The key requested.</param>
+        /// <param name="value">The value for the key.</param>
         public SettingChangedMessage(SettingKeys key, T value)
         {
             Key = key;
             Value = value;
         }
+
+        /// <summary>
+        /// Gets the key of the setting that has changed.
+        /// </summary>
+        public SettingKeys Key { get; }
+
+        /// <summary>
+        /// Gets the updated setting value.
+        /// </summary>
+        public T Value { get; }
     }
 }
