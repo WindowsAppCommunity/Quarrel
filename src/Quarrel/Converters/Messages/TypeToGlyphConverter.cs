20c8e94a941845aaf93d713e306e3c19d40ab637
Avishai Dernis <avid29@live.com>
2020-03-21 19:38:44 -0400

    Added Filetype icon for hidden attachments

---
+++ src/Quarrel/Converters/Messages/TypeToGlyphConverter.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using System;
+using System.Text.RegularExpressions;
+using Windows.UI.Xaml.Data;
+
+namespace Quarrel.Converters.Messages
+{
+    /// <summary>
+    /// A converter that returns a humaized filesize based on number of byte.
+    /// </summary>
+    public sealed class TypeToGlyphConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var match = Regex.Match(value.ToString(), @"^.*\.(.*)$");
+            switch (match.Groups[1].Value.ToLower())
+            {
+                case "jpg":
+                case "jpeg":
+                case "png":
+                    return "";
+                case "gif":
+                case "mov":
+                case "mp4":
+                    return "";
+            }
+
+            return "";
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
