8fa10b5ba01a540c823d20a70cf28a2f8ffb187c
Matthew <40522069+matthew4850@users.noreply.github.com>
2020-03-28 16:24:37 +0000

    Improve AudioVisualizer performance and use arrays instead of multiple variables.

--- src/Quarrel/Controls/AudioVisualizer.xaml.cs
+++ src/Quarrel/Controls/AudioVisualizer.xaml.cs
@@ -3,12 +3,15 @@
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Graphics.Canvas.Brushes;
 using Microsoft.Graphics.Canvas.Geometry;
+using Microsoft.Graphics.Canvas.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
 using Quarrel.Helpers.AudioProcessing;
 using Quarrel.ViewModels.Services.Settings;
 using Quarrel.ViewModels.Services.Voice.Audio;
 using Quarrel.ViewModels.Services.Voice.Audio.In;
 using Quarrel.ViewModels.Services.Voice.Audio.Out;
 using System;
+using System.Linq;
 using System.Numerics;
 using Windows.UI;
 using Windows.UI.Xaml;
@@ -47,46 +50,26 @@ namespace Quarrel.Controls
         /// </summary>
         private float _halfPoint;
 
-        private readonly float _point0 = 0;
-        private float _point1 = 0;
-        private float _point2 = 0;
-        private float _point3 = 0;
-        private float _point4 = 0;
-        private float _point5 = 0;
-        private float _point6 = 0;
-        private float _point7 = 0;
-        private float _point8 = 0;
+        private const int Count = 9;
+
+        private float[] _points = new float[Count];
 
         /// <summary>
         /// Average value of points.
         /// </summary>
         private float average = 0;
 
-        private float _specPoint0 = 0;
-        private float _specPoint1 = 0;
-        private float _specPoint2 = 0;
-        private float _specPoint3 = 0;
-        private float _specPoint4 = 0;
-        private float _specPoint5 = 0;
-        private float _specPoint6 = 0;
-        private float _specPoint7 = 0;
-        private float _specPoint8 = 0;
+        private float[] _specPoints = new float[Count];
 
         /// <summary>
         /// Average value of Spec Points.
         /// </summary>
         private float _specPointAverage = 0;
 
-        private Smoother _smoother1;
-        private Smoother _smoother2;
-        private Smoother _smoother3;
-        private Smoother _smoother4;
-        private Smoother _smoother5;
-        private Smoother _smoother6;
-        private Smoother _smoother7;
-        private Smoother _smoother8;
-        private Smoother _smoother9;
+        private Smoother[] _smoothers = new Smoother[Count];
         private Smoother _averageSmoother;
+
+        private CanvasLinearGradientBrush _gradient;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AudioVisualizer"/> class.
@@ -117,15 +100,15 @@ namespace Quarrel.Controls
             {
                 BoundAudioService.AudioQueued += DataRecieved;
 
-                _smoother1 = new Smoother(4, 6);
-                _smoother2 = new Smoother(4, 12);
-                _smoother3 = new Smoother(4, 14);
-                _smoother4 = new Smoother(4, 14);
-                _smoother5 = new Smoother(4, 15);
-                _smoother6 = new Smoother(4, 16);
-                _smoother7 = new Smoother(4, 16);
-                _smoother8 = new Smoother(4, 15);
-                _smoother9 = new Smoother(4, 14);
+                _smoothers[0] = new Smoother(4, 6);
+                _smoothers[1] = new Smoother(4, 12);
+                _smoothers[2] = new Smoother(4, 14);
+                _smoothers[3] = new Smoother(4, 14);
+                _smoothers[4] = new Smoother(4, 15);
+                _smoothers[5] = new Smoother(4, 16);
+                _smoothers[6] = new Smoother(4, 16);
+                _smoothers[7] = new Smoother(4, 15);
+                _smoothers[8] = new Smoother(4, 14);
                 _averageSmoother = new Smoother(1000, 100);
 
                 _blurple = (Color)App.Current.Resources["BlurpleColor"];
@@ -152,15 +135,10 @@ namespace Quarrel.Controls
         private void FftDipose(object sender, RoutedEventArgs e)
         {
             // Clear FFT
-            _smoother1 = null;
-            _smoother2 = null;
-            _smoother3 = null;
-            _smoother4 = null;
-            _smoother5 = null;
-            _smoother6 = null;
-            _smoother7 = null;
-            _smoother8 = null;
-            _smoother9 = null;
+            for (int i = 0; i < Count; i++)
+            {
+                _smoothers[i] = null;
+            }
             _averageSmoother = null;
             initailized = false;
 
@@ -178,16 +156,16 @@ namespace Quarrel.Controls
             // Determine FFT data
             float[] fftData = HelperMethods.GetFftChannelData(e);
 
-            _specPoint0 = HelperMethods.Max(fftData, 0, 1);
-            _specPoint1 = HelperMethods.Max(fftData, 2, 3);
-            _specPoint2 = HelperMethods.Max(fftData, 3, 4);
-            _specPoint3 = HelperMethods.Max(fftData, 4, 5);
-            _specPoint4 = HelperMethods.Max(fftData, 5, 6);
-            _specPoint5 = HelperMethods.Max(fftData, 7, 8);
-            _specPoint6 = HelperMethods.Max(fftData, 9, 10);
-            _specPoint7 = HelperMethods.Max(fftData, 10, 12);
-            _specPoint8 = HelperMethods.Max(fftData, 14, 26);
-            _specPointAverage = (_specPoint0 + _specPoint1 + _specPoint2 + _specPoint3 + _specPoint4 + _specPoint5 + _specPoint6 + _specPoint7 + _specPoint8) / 9;
+            _specPoints[0] = HelperMethods.Max(fftData, 0, 1);
+            _specPoints[1] = HelperMethods.Max(fftData, 2, 3);
+            _specPoints[2] = HelperMethods.Max(fftData, 3, 4);
+            _specPoints[3] = HelperMethods.Max(fftData, 4, 5);
+            _specPoints[4] = HelperMethods.Max(fftData, 5, 6);
+            _specPoints[5] = HelperMethods.Max(fftData, 7, 8);
+            _specPoints[6] = HelperMethods.Max(fftData, 9, 10);
+            _specPoints[7] = HelperMethods.Max(fftData, 10, 12);
+            _specPoints[8] = HelperMethods.Max(fftData, 14, 26);
+            _specPointAverage = _specPoints.Average();
         }
 
         /// <summary>
@@ -229,7 +207,7 @@ namespace Quarrel.Controls
         /// <summary>
         /// Draws each frame of the Canvas.
         /// </summary>
-        private void CanvasAnimatedControl_Draw(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedDrawEventArgs args)
+        private void CanvasAnimatedControl_Draw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
         {
             if (SimpleIoc.Default.GetInstance<ISettingsService>().Roaming.GetValue<bool>(SettingKeys.ExpensiveRendering))
             {
@@ -246,52 +224,39 @@ namespace Quarrel.Controls
                         cpb.BeginFigure(0, _height);
 
                         // Initialize render points
-                        Vector2 p0;
-                        Vector2 p1;
-                        Vector2 p2;
-                        Vector2 p3;
-                        Vector2 p4;
-                        Vector2 p5;
-                        Vector2 p6;
-                        Vector2 p7;
-                        Vector2 p8;
+                        Vector2[] p = new Vector2[Count];
 
                         // Set render points
                         average = _averageSmoother.Smooth(_specPointAverage);
-                        p0 = new Vector2(_point0, _height - (Adjust(_smoother1.Smooth(_specPoint0)) * _height));
-                        p1 = new Vector2(_point1, _height - (Adjust(_smoother2.Smooth(_specPoint1)) * _height));
-                        p2 = new Vector2(_point2, _height - (Adjust(_smoother3.Smooth(_specPoint2)) * _height));
-                        p3 = new Vector2(_point3, _height - (Adjust(_smoother4.Smooth(_specPoint3)) * _height));
-                        p4 = new Vector2(_point4, _height - (Adjust(_smoother5.Smooth(_specPoint4)) * _height));
-                        p5 = new Vector2(_point5, _height - (Adjust(_smoother6.Smooth(_specPoint5)) * _height));
-                        p6 = new Vector2(_point6, _height - (Adjust(_smoother7.Smooth(_specPoint6)) * _height));
-                        p7 = new Vector2(_point7, _height - (Adjust(_smoother8.Smooth(_specPoint7)) * _height));
-                        p8 = new Vector2(_point8, _height - (Adjust(_smoother9.Smooth(_specPoint8)) * _height));
+                        for (int i = 0; i < Count; i++)
+                        {
+                            p[i] = new Vector2(_points[i], _height - (Adjust(_smoothers[i].Smooth(_specPoints[i])) * _height));
+                        }
 
                         // Render points
-                        cpb.AddLine(p0);
-                        cpb.AddCubicBezier(GetC1(p0), GetC2(p1), p1);
-                        cpb.AddCubicBezier(GetC1(p1), GetC2(p2), p2);
-                        cpb.AddCubicBezier(GetC1(p2), GetC2(p3), p3);
-                        cpb.AddCubicBezier(GetC1(p3), GetC2(p4), p4);
-                        cpb.AddCubicBezier(GetC1(p4), GetC2(p5), p5);
-                        cpb.AddCubicBezier(GetC1(p5), GetC2(p6), p6);
-                        cpb.AddCubicBezier(GetC1(p6), GetC2(p7), p7);
-                        cpb.AddCubicBezier(GetC1(p7), GetC2(p8), p8);
-                        cpb.AddLine(new Vector2(p8.X, _height));
+                        cpb.AddLine(p[0]);
+                        for (int i = 0; i < Count-1; i++)
+                        {
+                            cpb.AddCubicBezier(GetC1(p[i]), GetC2(p[i+1]), p[i + 1]);
+                        }
+
+                        cpb.AddLine(new Vector2(p[Count-1].X, _height));
                         cpb.EndFigure(CanvasFigureLoop.Closed);
 
-                        // Render
-                        CanvasLinearGradientBrush gradient = new CanvasLinearGradientBrush(sender, _transparentBlurple, _blurple)
-                        {
-                            EndPoint = new Vector2(0, _height + 48),
-                            StartPoint = new Vector2(0, -12),
-                        };
                         var path = CanvasGeometry.CreatePath(cpb);
-                        args.DrawingSession.FillGeometry(path, gradient);
+                        args.DrawingSession.FillGeometry(path, _gradient);
                     }
                 }
             }
+        }
+
+        private void CanvasAnimatedControl_OnCreateResources(CanvasAnimatedControl sender, CanvasCreateResourcesEventArgs args)
+        {
+            _gradient = new CanvasLinearGradientBrush(sender, _transparentBlurple, _blurple)
+            {
+                EndPoint = new Vector2(0, _height + 48),
+                StartPoint = new Vector2(0, -12),
+            };
         }
 
         /// <summary>
@@ -301,14 +266,10 @@ namespace Quarrel.Controls
         {
             _height = (float)e.NewSize.Height - 1;
             float segment = (float)e.NewSize.Width / 8;
-            _point1 = segment;
-            _point2 = segment * 2;
-            _point3 = segment * 3;
-            _point4 = segment * 4;
-            _point5 = segment * 5;
-            _point6 = segment * 6;
-            _point7 = segment * 7;
-            _point8 = segment * 8;
+            for (int i = 1; i < Count; i++)
+            {
+                _points[i] = segment * i;
+            }
             _halfPoint = segment / 2;
         }
 
