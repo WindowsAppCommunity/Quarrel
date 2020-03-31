dba49b3132b4bba44c99a3885aa59860b86fa19e
Avishai Dernis <avid29@live.com>
2020-03-31 10:06:43 -0400

    Moved GuildTemplate to DataTemplate

---
+++ src/Quarrel/DataTemplates/Guilds/GuildTemplate.xaml.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+namespace Quarrel.DataTemplates.Guilds
+{
+    /// <summary>
+    /// A collection of Data Templates for Guild displaying.
+    /// </summary>
+    public sealed partial class GuildTemplate
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GuildTemplate"/> class.
+        /// </summary>
+        public GuildTemplate()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
