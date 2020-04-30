9530c5361ee3be04a1a145746edb6922b4313613
Avishai Dernis <avid29@live.com>
2020-04-30 07:50:55 -0400

    Improved folder behavior and appearance

--- src/Quarrel.ViewModels/Services/Discord/Guilds/GuildsService.cs
+++ src/Quarrel.ViewModels/Services/Discord/Guilds/GuildsService.cs
@@ -188,10 +188,14 @@ namespace Quarrel.ViewModels.Services.Discord.Guilds
 
                     foreach (var folder in m.EventData.Settings.GuildFolders)
                     {
-                        AllGuildFolders.Add(new BindableGuildFolder(folder));
-                        foreach (string id in folder.GuildIds)
+                        AllGuildFolders.Add(new BindableGuildFolder(folder) { IsCollapsed = true });
+                        if (folder.GuildIds.Count() > 1)
                         {
-                            AllGuilds[id].FolderId = folder.Id;
+                            foreach (string id in folder.GuildIds)
+                            {
+                                AllGuilds[id].FolderId = folder.Id;
+                                AllGuilds[id].IsCollapsed = true;
+                            }
                         }
                     }
 
