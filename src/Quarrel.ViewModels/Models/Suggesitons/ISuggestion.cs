0ee6f754d4945ee2feb5529944f1007b2f9f5429
Avishai Dernis <avid29@live.com>
2020-05-04 15:35:29 -0400

    Added SuggestionTemplates and Models

---
+++ src/Quarrel.ViewModels/Models/Suggesitons/ISuggestion.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+namespace Quarrel.ViewModels.Models
+{
+    /// <summary>
+    /// Represents a suggestion for the MessageBox.
+    /// </summary>
+    public interface ISuggestion
+    {
+    }
+}
---
+++ src/Quarrel/TemplateSelectors/SuggestionTemplateSelector.cs
@@ -0,0 +1,42 @@
+﻿using Quarrel.ViewModels.Models;
+using Quarrel.ViewModels.Models.Suggesitons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Quarrel.TemplateSelectors
+{
+    /// <summary>
+    /// A template selector for the suggestion type to show.
+    /// </summary>
+    public sealed class SuggestionTemplateSelector : DataTemplateSelector
+    {
+        /// <summary>
+        /// Gets or sets the pure image attachment template.
+        /// </summary>
+        public DataTemplate UserSuggestionTemplate { get; set; }
+
+        /// <summary>
+        /// Selects a <see cref="DataTemplate"/> based on details from the <paramref name="item"/>.
+        /// </summary>
+        /// <param name="item">An <see cref="ISuggestion"/>.</param>
+        /// <param name="container">The parent of the resulting <see cref="DataTemplate"/>.</param>
+        /// <returns>A <see cref="DataTemplate"/> for the <paramref name="item"/>'s type.</returns>
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (container is FrameworkElement parent && item is ISuggestion attachment)
+            {
+                switch (item)
+                {
+                    case UserSuggestion _: return UserSuggestionTemplate;
+                }
+            }
+
+            return null;
+        }
+    }
+}
