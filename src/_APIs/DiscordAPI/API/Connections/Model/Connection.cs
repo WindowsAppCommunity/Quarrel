ae69101b5cd66a474b486c473319d4c54a11e60d
Avishai Dernis <avid29@live.com>
2020-05-10 22:06:14 -0400

    ConnectionService documentation

--- src/_APIs/DiscordAPI/API/Connections/Model/Connection.cs
+++ src/_APIs/DiscordAPI/API/Connections/Model/Connection.cs
@@ -1,9 +1,17 @@
-﻿using Newtonsoft.Json;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using Newtonsoft.Json;
 
 namespace DiscordAPI.API.Connections
 {
+    /// <summary>
+    /// An Oauth connection.
+    /// </summary>
     public class Connection
     {
+        /// <summary>
+        /// The connection's url.
+        /// </summary>
         [JsonProperty("url")]
         public string Url { get; set; }
     }
