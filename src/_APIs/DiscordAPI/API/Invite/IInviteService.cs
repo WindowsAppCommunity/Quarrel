53cae12fd8adad6f073091ef1c543bf476039251
Avishai Dernis <avid29@live.com>
2020-05-10 23:54:30 -0400

    IInviteService documentation

--- src/_APIs/DiscordAPI/API/Invite/IInviteService.cs
+++ src/_APIs/DiscordAPI/API/Invite/IInviteService.cs
@@ -1,17 +1,37 @@
-﻿using Refit;
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using Refit;
 using System.Threading.Tasks;
 
 namespace DiscordAPI.API.Invite
 {
+    /// <summary>
+    /// A service for invite REST calls.
+    /// </summary>
     public interface IInviteService
     {
+        /// <summary>
+        /// Get an invite by code.
+        /// </summary>
+        /// <param name="inviteCode">The invite's code.</param>
+        /// <returns>The invite.</returns>
         [Get("/v6/invites/{inviteCode}?with_counts=true")]
-        Task<DiscordAPI.Models.Invite> GetInvite([AliasAs("inviteCode")] string inviteCode);
+        Task<Models.Invite> GetInvite([AliasAs("inviteCode")] string inviteCode);
 
+        /// <summary>
+        /// Delete an invite by code.
+        /// </summary>
+        /// <param name="inviteCode">The invite's code.</param>
+        /// <returns>The deleted invite.</returns>
         [Delete("/v6/invites/{inviteCode}")]
-        Task<DiscordAPI.Models.Invite> DeleteInvite([AliasAs("inviteCode")] string inviteCode);
+        Task<Models.Invite> DeleteInvite([AliasAs("inviteCode")] string inviteCode);
 
+        /// <summary>
+        /// Accept an invite by code.
+        /// </summary>
+        /// <param name="inviteCode">The invite's code.</param>
+        /// <returns>The invite.</returns>
         [Post("/v6/invites/{inviteCode}")]
-        Task<DiscordAPI.Models.Invite> AcceptInvite([AliasAs("inviteCode")] string inviteCode);
+        Task<Models.Invite> AcceptInvite([AliasAs("inviteCode")] string inviteCode);
     }
 }
