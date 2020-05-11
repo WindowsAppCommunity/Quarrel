19826fdb17d7ac3f4b0eef068bc4fa25d90fb515
Avishai Dernis <avid29@live.com>
2020-05-10 22:31:00 -0400

    Suppression rules for Discord API

---
+++ src/_APIs/DiscordAPI/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1309:Field names should not begin with underscore", Justification = "Not in style")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "Not in style")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1128:Put constructor initializers on their own line", Justification = "Not in style")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1602:Enumeration items should be documented", Justification = "Not in style", Scope = "type")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1636:File header copyright text should match", Justification = "Outside code")]
