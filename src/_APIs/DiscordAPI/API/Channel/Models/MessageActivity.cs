85d34c08a2f8b8ea4cf757f045f861aaec53fe18
Avishai Dernis <avid29@live.com>
2020-05-10 20:46:26 -0400

    MessageUpsert StyleCop conformity

---
+++ src/_APIs/DiscordAPI/API/Channel/Models/MessageActivity.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using Newtonsoft.Json;
+
+namespace DiscordAPI.API.Channel.Models
+{
+    /// <summary>
+    /// Activity bound to message.
+    /// </summary>
+    public class MessageActivity
+    {
+        /// <summary>
+        /// Gets or sets the activity session id.
+        /// </summary>
+        [JsonProperty("session_id")]
+        public string SessionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the activity type.
+        /// </summary>
+        [JsonProperty("type")]
+        public int Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the party id.
+        /// </summary>
+        [JsonProperty("party_id")]
+        public string PartyId { get; set; }
+    }
+}
