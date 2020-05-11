d6dc9b15b7b70c468600c6dae2695e32ce290bb3
Avishai Dernis <avid29@live.com>
2020-05-11 00:12:01 -0400

    ILoginService documentation

---
+++ src/_APIs/DiscordAPI/API/Login/Models/LoginMFARequest.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Quarrel. All rights reserved.
+
+using Newtonsoft.Json;
+
+namespace DiscordAPI.API.Login.Models
+{
+    /// <summary>
+    /// A login request with MFA.
+    /// </summary>
+    public class LoginMFARequest
+    {
+        /// <summary>
+        /// MFA code.
+        /// </summary>
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// MRA ticket.
+        /// </summary>
+        [JsonProperty("ticket")]
+        public string Ticket { get; set; }
+    }
+}
