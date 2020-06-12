e0c50b52601042061508c840d48f0b6c4f66c6d0
Avishai Dernis <avid29@live.com>
2020-06-12 16:50:26 -0400

    NotNullToBool static converter

--- src/Quarrel/Converters/Base/NotNullToBoolConverter.cs
+++ src/Quarrel/Converters/Base/NotNullToBoolConverter.cs
@@ -1,39 +1,40 @@
 ﻿// Copyright (c) Quarrel. All rights reserved.
 
-using System;
-using Windows.UI.Xaml.Data;
-
 namespace Quarrel.Converters.Base
 {
     /// <summary>
     /// A converter that returns a <see langword="true"/> <see cref="bool"/> value indicating if the input <see langword="object"/> is **not** <see langword="null"/>.
     /// </summary>
-    public sealed class NotNullToBoolConverter : IValueConverter
+    public sealed class NotNullToBoolConverter
     {
-        /// <inheritdoc/>
-        public object Convert(object value, Type targetType, object parameter, string language)
+        /// <summary>
+        /// Check if a value is null.
+        /// </summary>
+        /// <param name="value">null.</param>
+        /// <returns>Whether or not item is null</returns>
+        public static bool Convert(object value)
         {
-            bool v;
-            if (value is string sValue)
-            {
-                v = !string.IsNullOrEmpty(sValue);
-            }
-            else if (value is int iValue)
-            {
-                v = iValue > 0;
-            }
-            else
-            {
-                v = value != null;
-            }
-
-            return v;
+            return value != null;
         }
 
-        /// <inheritdoc/>
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        /// <summary>
+        /// Check if a string is null.
+        /// </summary>
+        /// <param name="value">null.</param>
+        /// <returns>Whether or not item is null.</returns>
+        public static bool Convert(string value)
         {
-            throw new NotImplementedException();
+            return !string.IsNullOrEmpty(value);
+        }
+
+        /// <summary>
+        /// Check if a string is null.
+        /// </summary>
+        /// <param name="value">null.</param>
+        /// <returns>Whether or not item is null.</returns>
+        public static bool Convert(int value)
+        {
+            return value > 0;
         }
     }
 }
