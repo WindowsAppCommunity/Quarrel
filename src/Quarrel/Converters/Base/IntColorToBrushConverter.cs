186f5c3bb49dffd8e9727c421028776cab0230ae
Avishai Dernis <avid29@live.com>
2020-06-12 12:30:33 -0400

    InColorToBrush static converter

--- src/Quarrel/Converters/Base/IntColorToBrushConverter.cs
+++ src/Quarrel/Converters/Base/IntColorToBrushConverter.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Quarrel. All rights reserved.
 
 using QuarrelSmartColor.Extensions.Windows.UI;
-using System;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 
 namespace Quarrel.Converters.Base
@@ -10,18 +8,16 @@ namespace Quarrel.Converters.Base
     /// <summary>
     /// A converter a <see cref="SolidColorBrush"/> from an integer.
     /// </summary>
-    public sealed class IntColorToBrushConverter : IValueConverter
+    public sealed class IntColorToBrushConverter
     {
-        /// <inheritdoc/>
-        public object Convert(object value, Type targetType, object parameter, string language)
+        /// <summary>
+        /// Converts an int to a color.
+        /// </summary>
+        /// <param name="value">The integer color.</param>
+        /// <returns>The <see cref="SolidColorBrush"/> equivelent of the int.</returns>
+        public static SolidColorBrush Convert(int value)
         {
             return new SolidColorBrush(ColorExtensions.IntToColor((int)value));
-        }
-
-        /// <inheritdoc/>
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            throw new NotImplementedException();
         }
     }
 }
