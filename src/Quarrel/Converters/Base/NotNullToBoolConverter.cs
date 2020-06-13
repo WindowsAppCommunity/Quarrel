f09aee0e4f8b2e55d047bc444a4ad9d7c1534eaf
Adam Dernis <avid29@live.com>
2020-06-13 16:18:15 -0400

    Fixed Markdown with magic

--- src/Quarrel/Converters/Base/NotNullToBoolConverter.cs
+++ src/Quarrel/Converters/Base/NotNullToBoolConverter.cs
@@ -1,11 +1,14 @@
 ﻿// Copyright (c) Quarrel. All rights reserved.
 
+using System;
+using Windows.UI.Xaml.Data;
+
 namespace Quarrel.Converters.Base
 {
     /// <summary>
     /// A converter that returns a <see langword="true"/> <see cref="bool"/> value indicating if the input <see langword="object"/> is **not** <see langword="null"/>.
     /// </summary>
-    public sealed class NotNullToBoolConverter
+    public sealed class NotNullToBoolConverter : IValueConverter
     {
         /// <summary>
         /// Check if a value is null.
@@ -25,6 +28,23 @@ namespace Quarrel.Converters.Base
         public static bool ConvertString(string value)
         {
             return !string.IsNullOrEmpty(value);
+        }
+
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string sValue)
+            {
+                return ConvertString(sValue);
+            }
+
+            return Convert(value);
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- src/Quarrel/Converters/Base/notNullToVisibilityConverter.cs
+++ src/Quarrel/Converters/Base/notNullToVisibilityConverter.cs
@@ -1,13 +1,15 @@
 ﻿// Copyright (c) Quarrel. All rights reserved.
 
+using System;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
 
 namespace Quarrel.Converters.Base
 {
     /// <summary>
     /// A converter that returns a <see cref="Visibility.Visible"/> value if the input <see langword="object"/> is <see langword="null"/>.
     /// </summary>
-    public sealed class NotNullToVisibilityConverter
+    public sealed class NotNullToVisibilityConverter : IValueConverter
     {
         /// <summary>
         /// Checks if a object is null, and returns the inverse visibility.
@@ -27,6 +29,23 @@ namespace Quarrel.Converters.Base
         public static Visibility ConvertString(string value)
         {
             return string.IsNullOrEmpty(value) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string sValue)
+            {
+                return ConvertString(sValue);
+            }
+
+            return Convert(value);
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
         }
     }
 }
