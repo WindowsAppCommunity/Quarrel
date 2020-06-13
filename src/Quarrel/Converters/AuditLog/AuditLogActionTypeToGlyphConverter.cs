905b1a452f4e4056b1d083480662f5fecb756831
Avishai Dernis <avid29@live.com>
2020-06-13 06:47:38 -0400

    AuditLogActionTypeToGlyph static converter

--- src/Quarrel/Converters/AuditLog/AuditLogActionTypeToGlyphConverter.cs
+++ src/Quarrel/Converters/AuditLog/AuditLogActionTypeToGlyphConverter.cs
@@ -1,89 +1,75 @@
 ﻿// Copyright (c) Quarrel. All rights reserved.
 
 using DiscordAPI.API.Guild.Models;
-using System;
-using Windows.UI.Xaml.Data;
 
 namespace Quarrel.Converters.AuditLog
 {
     /// <summary>
     /// Converter for AuditLogAction to an icon.
     /// </summary>
-    public sealed class AuditLogActionTypeToGlyphConverter : IValueConverter
+    public sealed class AuditLogActionTypeToGlyphConverter
     {
         /// <summary>
         /// Converts AuditLogAction to an icon.
         /// </summary>
         /// <param name="value">AuditLogAction.</param>
-        /// <param name="targetType">Requested out type.</param>
-        /// <param name="parameter">Extra info.</param>
-        /// <param name="language">What language the user is using.</param>
         /// <returns>Glyph representing action.</returns>
-        public object Convert(object value, Type targetType, object parameter, string language)
+        public static string Convert(int value)
         {
-            if (value is int iValue)
+            switch ((AuditLogActionType)value)
             {
-                switch ((AuditLogActionType)iValue)
-                {
-                    case AuditLogActionType.ChannelCreate:
-                    case AuditLogActionType.ChannelUpdate:
-                    case AuditLogActionType.ChannelDelete:
-                        return "";
+                case AuditLogActionType.ChannelCreate:
+                case AuditLogActionType.ChannelUpdate:
+                case AuditLogActionType.ChannelDelete:
+                    return "";
 
-                    case AuditLogActionType.ChannelOverwriteCreate:
-                    case AuditLogActionType.ChannelOverwriteDelete:
-                    case AuditLogActionType.ChannelOverwriteUpdate:
-                        return "";
+                case AuditLogActionType.ChannelOverwriteCreate:
+                case AuditLogActionType.ChannelOverwriteDelete:
+                case AuditLogActionType.ChannelOverwriteUpdate:
+                    return "";
 
-                    case AuditLogActionType.EmojiCreate:
-                    case AuditLogActionType.EmojiDelete:
-                    case AuditLogActionType.EmojiUpdate:
-                        return "";
+                case AuditLogActionType.EmojiCreate:
+                case AuditLogActionType.EmojiDelete:
+                case AuditLogActionType.EmojiUpdate:
+                    return "";
 
-                    case AuditLogActionType.GuildUpdate:
-                        return "";
+                case AuditLogActionType.GuildUpdate:
+                    return "";
 
-                    case AuditLogActionType.InviteCreate:
-                    case AuditLogActionType.InviteDelete:
-                    case AuditLogActionType.InviteUpdate:
-                        return "";
+                case AuditLogActionType.InviteCreate:
+                case AuditLogActionType.InviteDelete:
+                case AuditLogActionType.InviteUpdate:
+                    return "";
 
-                    case AuditLogActionType.MemberBanAdd:
-                    case AuditLogActionType.MemberBanRemove:
-                    case AuditLogActionType.MemberKick:
-                        return "";
+                case AuditLogActionType.MemberBanAdd:
+                case AuditLogActionType.MemberBanRemove:
+                case AuditLogActionType.MemberKick:
+                    return "";
 
-                    case AuditLogActionType.MemberPrune:
-                        return "";
+                case AuditLogActionType.MemberPrune:
+                    return "";
 
-                    case AuditLogActionType.MemberRoleUpdate:
-                        return ""; // TODO: Change
+                case AuditLogActionType.MemberRoleUpdate:
+                    return ""; // TODO: Change
 
-                    case AuditLogActionType.MemberUpdate:
-                        return "";
+                case AuditLogActionType.MemberUpdate:
+                    return "";
 
-                    case AuditLogActionType.MessageDelete:
-                        return "";
+                case AuditLogActionType.MessageDelete:
+                    return "";
 
-                    case AuditLogActionType.RoleCreate:
-                    case AuditLogActionType.RoleDelete:
-                    case AuditLogActionType.RoleUpdate:
-                        return "";
+                case AuditLogActionType.RoleCreate:
+                case AuditLogActionType.RoleDelete:
+                case AuditLogActionType.RoleUpdate:
+                    return "";
 
-                    case AuditLogActionType.WebhookCreate:
-                    case AuditLogActionType.WebhookDelete:
-                    case AuditLogActionType.WebhookUpdate:
-                        return "";
-                }
+                case AuditLogActionType.WebhookCreate:
+                case AuditLogActionType.WebhookDelete:
+                case AuditLogActionType.WebhookUpdate:
+                    return "";
             }
 
             return "?";
-        }
-
-        /// <inheritdoc/>
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            throw new NotImplementedException();
         }
     }
 }
