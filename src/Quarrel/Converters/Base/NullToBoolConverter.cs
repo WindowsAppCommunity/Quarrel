df9e2a8d4ad956c1ab8b07dc0bd2b654625a72b6
Avishai Dernis <avid29@live.com>
2020-06-13 05:13:31 -0400

    NullToBool static converter

--- src/Quarrel/Converters/Base/NullToBoolConverter.cs
+++ src/Quarrel/Converters/Base/NullToBoolConverter.cs
@@ -1,35 +1,30 @@
 ﻿// Copyright (c) Quarrel. All rights reserved.
 
-using System;
-using Windows.UI.Xaml.Data;
-
 namespace Quarrel.Converters.Base
 {
     /// <summary>
     /// A converter that returns a <see langword="true"/> <see cref="bool"/> value indicating if the input <see langword="object"/> is <see langword="null"/>.
     /// </summary>
-    public sealed class NullToBoolConverter : IValueConverter
+    public sealed class NullToBoolConverter
     {
-        /// <inheritdoc/>
-        public object Convert(object value, Type targetType, object parameter, string language)
+        /// <summary>
+        /// Checks if a value us null.
+        /// </summary>
+        /// <param name="value">Item to check.</param>
+        /// <returns>Whether or not the item is null.</returns>
+        public static bool Convert(object value)
         {
-            bool v;
-            if (value is string sValue)
-            {
-                v = string.IsNullOrEmpty(sValue);
-            }
-            else
-            {
-                v = value == null;
-            }
-
-            return v;
+            return value == null;
         }
 
-        /// <inheritdoc/>
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        /// <summary>
+        /// Check if a string is null or empty.
+        /// </summary>
+        /// <param name="value">String to check.</param>
+        /// <returns>Whether or not the string is null or empty.</returns>
+        public static bool Convert(string value)
         {
-            throw new NotImplementedException();
+            return string.IsNullOrEmpty(value);
         }
     }
 }
