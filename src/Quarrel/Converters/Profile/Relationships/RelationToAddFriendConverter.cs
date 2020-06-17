3c1d67b2a5a4b4724546b78d4e2e947812b82d28
Adam Dernis <avid29@live.com>
2020-06-17 12:05:19 -0400

    Relations static converters

--- src/Quarrel/Converters/Profile/Relationships/RelationToAddFriendConverter.cs
+++ src/Quarrel/Converters/Profile/Relationships/RelationToAddFriendConverter.cs
@@ -1,26 +1,20 @@
 ﻿// Copyright (c) Quarrel. All rights reserved.
 
-using System;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Data;
-
 namespace Quarrel.Converters.Profile.Relationships
 {
     /// <summary>
-    /// A converter that returns <see cref="Visibility.Visible"/> if the user has no relation status.
+    /// A converter that returns true if the user has no relation status.
     /// </summary>
-    public sealed class RelationToAddFriendConverter : IValueConverter
+    public sealed class RelationToAddFriendConverter
     {
-        /// <inheritdoc/>
-        public object Convert(object value, Type targetType, object parameter, string language)
+        /// <summary>
+        /// Gets whether or not the relation status is 0.
+        /// </summary>
+        /// <param name="value">The relation status.</param>
+        /// <returns>Whether or not the relation status is 0</returns>
+        public static bool Convert(int value)
         {
-            return (int)value == 0 ? Visibility.Visible : Visibility.Collapsed;
-        }
-
-        /// <inheritdoc/>
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            throw new NotImplementedException();
+            return value == 0;
         }
     }
 }
