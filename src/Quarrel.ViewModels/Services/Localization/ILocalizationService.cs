ca27f3d555f23406669c309d76137a5ef3197952
Adam Dernis <avid29@live.com>
2022-03-30 01:15:59 -0400

    Localization Service

---
+++ src/Quarrel.ViewModels/Services/Localization/ILocalizationService.cs
@@ -0,0 +1,30 @@
+﻿// Adam Dernis © 2022
+
+namespace Quarrel.ViewModels.Services.Localization
+{
+    /// <summary>
+    /// An interface for a localization service used in the app.
+    /// </summary>
+    public interface ILocalizationService
+    {
+        /// <summary>
+        /// Gets the localized <see langword="string"/> for a given resource.
+        /// </summary>
+        /// <param name="key">The key of the resource.</param>
+        /// <returns>Localized <see langword="string"/> if valid, otherwise returns an empty <see langword="string"/>.</returns>
+        string this[string key] { get; }
+
+        /// <summary>
+        /// Gets the localized <see langword="string"/> for a given resource.
+        /// </summary>
+        /// <param name="key">The key of the resource.</param>
+        /// <param name="args">An object array that contains zero or more objects to format.</param>
+        /// <returns>Localized <see langword="string"/> if valid, otherwise returns an empty <see langword="string"/>.</returns>
+        string this[string key, params object[] args] { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether or not the current language is written right to left.
+        /// </summary>
+        bool IsRightToLeftLanguage { get; }
+    }
+}
---
+++ src/Quarrel/Services/Localization/LocalizationService.cs
@@ -0,0 +1,43 @@
+﻿// Adam Dernis © 2022
+
+using Quarrel.ViewModels.Services.Localization;
+using Windows.ApplicationModel.Resources;
+using Windows.ApplicationModel.Resources.Core;
+
+namespace Quarrel.Services.Localization
+{
+    /// <summary>
+    /// A service that recieves localization details.
+    /// </summary>
+    public class LocalizationService : ILocalizationService
+    {
+        private ResourceLoader? _loader;
+
+        private ResourceLoader Loader
+        {
+            get
+            {
+                if (_loader is null)
+                    _loader = ResourceLoader.GetForViewIndependentUse();
+
+                return _loader;
+            }
+        }
+
+        /// <inheritdoc/>
+        public string this[string key] => Loader.GetString(key);
+
+        /// <inheritdoc/>
+        public string this[string key, params object[] args] => string.Format(this[key], args);
+
+        /// <inheritdoc/>
+        public bool IsRightToLeftLanguage
+        {
+            get
+            {
+                string flowDirectionSetting = ResourceContext.GetForCurrentView().QualifierValues["LayoutDirection"];
+                return flowDirectionSetting == "RTL";
+            }
+        }
+    }
+}
