f308930df28ba7cc25fb4b077fb1dbf23de4731e
Adam Dernis <avid29@live.com>
2022-03-30 03:40:10 -0400

    ExtendedSplashScreen

---
+++ src/Quarrel/Controls/Shell/ExtendedSplashScreen.xaml.cs
@@ -0,0 +1,15 @@
+﻿// Adam Dernis © 2022
+
+using Windows.UI.Xaml.Controls;
+
+namespace Quarrel.Controls.Shell
+{
+    public sealed partial class ExtendedSplashScreen : UserControl
+    {
+        public ExtendedSplashScreen()
+        {
+            this.InitializeComponent();
+            QuarrelIcon.BeginAnimation();
+        }
+    }
+}
