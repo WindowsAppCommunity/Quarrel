4476d3c0022fae3c74ac83fd863aa83d9589dc82
Adam Dernis <avid29@live.com>
2022-04-04 03:15:14 -0400

    Improved localizing Login and Splash

--- src/Quarrel/Controls/Shell/ExtendedSplashScreen.xaml.cs
+++ src/Quarrel/Controls/Shell/ExtendedSplashScreen.xaml.cs
@@ -1,5 +1,7 @@
 ﻿// Adam Dernis © 2022
 
+using Microsoft.Extensions.DependencyInjection;
+using Quarrel.Services.Localization;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -7,12 +9,18 @@ namespace Quarrel.Controls.Shell
 {
     public sealed partial class ExtendedSplashScreen : UserControl
     {
+        private const string Connected = "ExtendedSplash/Connected";
+        private const string Connecting = "ExtendedSplash/Connecting";
+
         private static readonly DependencyProperty IsShowingProperty =
             DependencyProperty.Register(nameof(IsShowing), typeof(bool), typeof(ExtendedSplashScreen), new PropertyMetadata(false, OnIsShowingChanged));
+
+        private ILocalizationService _localizationService;
 
         public ExtendedSplashScreen()
         {
             this.InitializeComponent();
+            _localizationService = App.Current.Services.GetRequiredService<ILocalizationService>();
             this.Visibility = Visibility.Collapsed;
         }
 
@@ -25,11 +33,13 @@ namespace Quarrel.Controls.Shell
         private void BeginAnimation()
         {
             this.Visibility = Visibility.Visible;
+            StatusBlock.Text = _localizationService[Connecting];
             QuarrelIcon.BeginAnimation();
         }
 
         private void FinishAnimation()
         {
+            StatusBlock.Text = _localizationService[Connected];
             QuarrelIcon.FinishAnimation();
         }
 
