82754893ff024630b453007dc49b0f7155e65100
Adam Dernis <avid29@live.com>
2022-04-06 14:37:43 -0400

    Added simple PrivacyPolicy flyout on LoginPage

---
+++ src/Quarrel/Behaviors/OpenFlyoutAction.cs
@@ -0,0 +1,17 @@
+﻿// Adam Dernis © 2022
+
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Quarrel.Behaviors
+{
+    public class OpenFlyoutAction : DependencyObject, IAction
+    {
+        public object Execute(object sender, object parameter)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+            return null;
+        }
+    }
+}
