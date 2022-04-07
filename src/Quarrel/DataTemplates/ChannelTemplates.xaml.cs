764ff0f7e97901cfc4eb3172da9e7c53e993ca13
Adam Dernis <avid29@live.com>
2022-04-07 11:17:58 -0400

    Regrouped Channel and Guild data templates

---
+++ src/Quarrel/DataTemplates/ChannelTemplates.xaml.cs
@@ -0,0 +1,17 @@
+﻿// Adam Dernis © 2022
+
+using Windows.UI.Xaml;
+
+namespace Quarrel.DataTemplates
+{
+    public sealed partial class ChannelTemplates : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChannelTemplates"/> class.
+        /// </summary>
+        public ChannelTemplates()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/Quarrel/DataTemplates/GuildTemplates.xaml.cs
@@ -0,0 +1,17 @@
+﻿// Adam Dernis © 2022
+
+using Windows.UI.Xaml;
+
+namespace Quarrel.DataTemplates
+{
+    public sealed partial class GuildTemplates : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GuildTemplates"/> class.
+        /// </summary>
+        public GuildTemplates()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
