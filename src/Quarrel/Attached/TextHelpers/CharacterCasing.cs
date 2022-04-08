753736228bc4fdd9f8dcf4bf40c0cb9ad2b9c62c
Adam Dernis <avid29@live.com>
2022-04-07 22:10:01 -0400

    Added CharacterCasing for category channels

---
+++ src/Quarrel/Attached/TextHelpers/CharacterCasing.cs
@@ -0,0 +1,63 @@
+﻿// Adam Dernis © 2022
+
+using Quarrel.Converters;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Quarrel.Attached
+{
+    /// <summary>
+    /// <see href="https://stackoverflow.com/a/15615736/"/>
+    /// </summary>
+    public partial class TextHelpers : DependencyObject
+    {
+        private static bool _mutex; // This is nasty
+
+        /// <summary>
+        /// Gets the <see cref="CharacterCasing"/> value for this dependency object.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static CharacterCasing GetCharacterCasing(DependencyObject obj) => (CharacterCasing)obj.GetValue(CharacterCasingProperty);
+
+        /// <summary>
+        /// Sets the <see cref="CharacterCasing"/> value for this dependency object.
+        /// </summary>
+        public static void SetCharacterCasing(DependencyObject obj, CharacterCasing value) => obj.SetValue(CharacterCasingProperty, value);
+
+        /// <summary>
+        /// Backing dependency property for CharacterCasing attached property.
+        /// </summary>
+        public static readonly DependencyProperty CharacterCasingProperty =
+            DependencyProperty.RegisterAttached("CharacterCasing",
+                typeof(TextHelpers),
+                typeof(CharacterCasing),
+                new PropertyMetadata(CharacterCasing.Normal, OnCharacterCasingChanged));
+
+        private static void OnCharacterCasingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var val = (CharacterCasing)e.NewValue;
+
+            DependencyProperty prop = d switch
+            {
+                TextBlock txt => TextBlock.TextProperty,
+                TextBox tbox => TextBox.PlaceholderTextProperty,
+                ButtonBase hBtn => ButtonBase.ContentProperty,
+                PivotItem pvi => PivotItem.HeaderProperty,
+                _ => throw new ArgumentException(),
+            };
+
+            d.RegisterPropertyChangedCallback(prop, (s, e) =>
+            {
+                if (_mutex)
+                    return;
+
+                _mutex = true;
+                d.SetValue(prop, CaseConverter.Convert((string)d.GetValue(prop), val));
+                _mutex = false;
+            });
+        }
+    }
+}
---
+++ src/Quarrel/Converters/CaseConverter.cs
@@ -0,0 +1,57 @@
+﻿// Adam Dernis © 2022
+
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml.Controls;
+
+namespace Quarrel.Converters
+{
+    /// <summary>
+    /// A converter that changes the casing of a string.
+    /// </summary>
+    public class CaseConverter
+    {
+        /// <summary>
+        /// Gets or sets the character casing to convert an input string to.
+        /// </summary>
+        public CharacterCasing Case { get; set; }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CaseConverter"/> class.
+        /// </summary>
+        public CaseConverter()
+        {
+            Case = CharacterCasing.Upper;
+        }
+
+        /// <summary>
+        /// Converts a string to a certain character casing.
+        /// </summary>
+        /// <param name="value">The string to convert.</param>
+        /// <param name="characterCasing">The result's character casing.</param>
+        /// <returns>String <paramref name="value"/> in the specified character casing.</returns>
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string Convert(string? value, CharacterCasing characterCasing)
+        {
+            if (value != null)
+            {
+                switch (characterCasing)
+                {
+                    case CharacterCasing.Upper:
+                        return value.ToUpper();
+
+                    case CharacterCasing.Lower:
+                        return value.ToLower();
+
+                    case CharacterCasing.Normal:
+                    default:
+                        return value;
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
