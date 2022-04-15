2e9d9cfb5463090f4f81fae980d284e7ee8921ea
Adam Dernis <avid29@live.com>
2022-04-15 15:43:21 -0400

    WIP QuipService

---
+++ src/Quarrel.ViewModels/Services/Quips/IQuipService.cs
@@ -0,0 +1,17 @@
+﻿// Quarrel © 2022
+
+using Quarrel.Services.Quips.Model;
+
+namespace Quarrel.Services.Quips
+{
+    /// <summary>
+    /// An interface for a service that loads quips for a context.
+    /// </summary>
+    public interface IQuipService
+    {
+        /// <summary>
+        /// Gets a quip.
+        /// </summary>
+        Quip GetQuip();
+    }
+}
