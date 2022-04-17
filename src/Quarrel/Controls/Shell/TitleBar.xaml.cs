76853cb9baef8c1e28c4be243953a7d48625c6da
Adam Dernis <avid29@live.com>
2022-04-16 21:48:22 -0400

    Extended into TitleBar

---
+++ src/Quarrel/Controls/Shell/TitleBar.xaml.cs
@@ -0,0 +1,27 @@
+﻿// Quarrel © 2022
+
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Quarrel.Controls.Shell
+{
+    public sealed partial class TitleBar : UserControl
+    {
+        public TitleBar()
+        {
+            this.InitializeComponent();
+            SetupTitleBar();
+        }
+        
+        private void SetupTitleBar()
+        {
+            ApplicationView.GetForCurrentView().TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            ApplicationView.GetForCurrentView().TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            Window.Current.SetTitleBar(this);
+        }
+    }
+}
