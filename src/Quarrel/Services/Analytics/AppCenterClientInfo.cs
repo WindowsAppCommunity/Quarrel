1d8fcbd121607a4b81f0ea8264504a5e3391108b
Adam Dernis <avid29@live.com>
2022-04-20 17:28:58 -0400

    AppService initialization and token loading

---
+++ src/Quarrel/Services/Analytics/AppCenterClientInfo.cs
@@ -0,0 +1,12 @@
+﻿// Quarrel © 2022
+
+using System.Text.Json.Serialization;
+
+namespace Quarrel.Services.Analytics
+{
+    public class AppCenterClientInfo
+    {
+        [JsonPropertyName("secret")]
+        public string? Secret {get; set; }
+    }
+}
