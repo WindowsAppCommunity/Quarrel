26e6568e78653d3902dc6e972d771a1716b15693
Adam Dernis <avid29@live.com>
2022-04-21 05:57:04 -0400

    Fixed DiscordStatus description/status swap

---
+++ src/Quarrel.ViewModels/ViewModels/SubPages/DiscordStatus/Models/BindableComponent.cs
@@ -0,0 +1,38 @@
+﻿// Quarrel © 2022
+
+namespace Quarrel.ViewModels.SubPages.DiscordStatus.Models
+{
+    /// <summary>
+    /// A component of the discord api.
+    /// </summary>
+    public class BindableComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BindableComponent"/> class.
+        /// </summary>
+        public BindableComponent(
+            string name,
+            string status,
+            string? description)
+        {
+            Name = name;
+            Description = description;
+            Status = status;
+        }
+
+        /// <summary>
+        /// Gets the name of the component.
+        /// </summary>
+        public string Name { get; }
+        
+        /// <summary>
+        /// Gets the description of the component.
+        /// </summary>
+        public string Description { get; }
+        
+        /// <summary>
+        /// Gets the status of the component.
+        /// </summary>
+        public string Status { get; }
+    }
+}
---
+++ src/Quarrel.ViewModels/ViewModels/SubPages/DiscordStatus/Models/BindableIncident.cs
@@ -0,0 +1,40 @@
+﻿// Quarrel © 2022
+
+using System.Collections.Generic;
+
+namespace Quarrel.ViewModels.SubPages.DiscordStatus.Models
+{
+    /// <summary>
+    /// An incident including its affected components.
+    /// </summary>
+    public class BindableIncident
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BindableIncident"/> class.
+        /// </summary>
+        public BindableIncident(
+            string name,
+            string status,
+            List<BindableComponent> items)
+        {
+            Name = name;
+            Status = status;
+            Items = items;
+        }
+
+        /// <summary>
+        /// Gets the name of the incident.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the status of the incident.
+        /// </summary>
+        public string Status { get; }
+
+        /// <summary>
+        /// Gets a list of the affected components of the incident.
+        /// </summary>
+        public List<BindableComponent> Items { get; }
+    }
+}
