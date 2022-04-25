e1d843647efec6b26e0595e865d52da6eadc84ba
Adam Dernis <avid29@live.com>
2022-04-25 11:37:56 -0400

    Added color to info message icon

--- src/Quarrel/Converters/DataTemplates/Messages/InfoMessageIconConverter.cs
+++ src/Quarrel/Converters/DataTemplates/Messages/InfoMessageIconConverter.cs
@@ -4,16 +4,23 @@ using Discord.API.Models.Enums.Messages;
 
 namespace Quarrel.Converters.DataTemplates.Messages
 {
-    public class MessageIconConverter
+    public class InfoMessageIconConverter
     {
         public static string Convert(MessageType type)
         {
             return type switch
             {
                 MessageType.Call => "",
+
                 MessageType.ChannelPinnedMessage => "",
+
                 MessageType.ChannelIconChange or
                 MessageType.ChannelNameChange => "",
+
+                MessageType.GuildMemberJoin or
+                MessageType.RecipientAdd => "",
+
+                MessageType.RecipientRemove => "",
                 _ => "?",
             };
         }
