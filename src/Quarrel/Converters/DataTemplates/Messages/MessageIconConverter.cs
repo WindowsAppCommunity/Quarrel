2ff4d1d2416b4158da9de77f529e58b179ff4c2d
Adam Dernis <avid29@live.com>
2022-04-25 11:24:38 -0400

    Message template selector

---
+++ src/Quarrel/Converters/DataTemplates/Messages/MessageIconConverter.cs
@@ -0,0 +1,21 @@
+﻿// Quarrel © 2022
+
+using Discord.API.Models.Enums.Messages;
+
+namespace Quarrel.Converters.DataTemplates.Messages
+{
+    public class MessageIconConverter
+    {
+        public static string Convert(MessageType type)
+        {
+            return type switch
+            {
+                MessageType.Call => "",
+                MessageType.ChannelPinnedMessage => "",
+                MessageType.ChannelIconChange or
+                MessageType.ChannelNameChange => "",
+                _ => "?",
+            };
+        }
+    }
+}
---
+++ src/Quarrel/DataTemplates/MessageTemplates.xaml.cs
@@ -0,0 +1,14 @@
+﻿// Quarrel © 2022
+
+using Windows.UI.Xaml;
+
+namespace Quarrel.DataTemplates
+{
+    public sealed partial class MessageTemplates : ResourceDictionary
+    {
+        public MessageTemplates()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
