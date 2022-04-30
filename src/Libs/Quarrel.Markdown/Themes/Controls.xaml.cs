64ed28899d7277840c468321591c2aff3d4e8d35
Matthew <40522069+matthew4850@users.noreply.github.com>
2022-04-30 21:23:37 +0100

    Massively improve inline code blocks and spoilers.

---
+++ src/Libs/Quarrel.Markdown/Themes/Controls.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Quarrel.Markdown.Themes
+{
+    partial class Controls : ResourceDictionary
+    {
+        public Controls()
+        {
+            InitializeComponent();
+        }
+    }
+}
