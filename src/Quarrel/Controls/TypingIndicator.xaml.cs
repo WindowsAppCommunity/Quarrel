4e37abbd376e6fe35f2c508cb9a650133e192534
Adam Dernis <avid29@live.com>
2022-05-01 09:34:32 -0400

    Copied TypingIndicator into new project

---
+++ src/Quarrel/Controls/TypingIndicator.xaml.cs
@@ -0,0 +1,15 @@
+﻿// Quarrel © 2022
+
+using Windows.UI.Xaml.Controls;
+
+namespace Quarrel.Controls
+{
+    public sealed partial class TypingIndicator : UserControl
+    {
+        public TypingIndicator()
+        {
+            this.InitializeComponent();
+            Typing.Begin();
+        }
+    }
+}
