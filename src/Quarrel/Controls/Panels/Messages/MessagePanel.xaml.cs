86f49f0050213f7351ae0ad6f702f419fb1550e5
Adam Dernis <avid29@live.com>
2022-05-09 15:49:18 -0400

    Added events for when the message view is scroller to the bottom and top

--- src/Quarrel/Controls/Panels/Messages/MessagePanel.xaml.cs
+++ src/Quarrel/Controls/Panels/Messages/MessagePanel.xaml.cs
@@ -1,13 +1,17 @@
 ﻿// Quarrel © 2022
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Uwp.UI;
 using Quarrel.ViewModels.Panels;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace Quarrel.Controls.Panels.Messages
 {
     public sealed partial class MessagePanel : UserControl
     {
+        private const double ScrollPadding = 100;
+
         public MessagePanel()
         {
             this.InitializeComponent();
@@ -15,5 +19,25 @@ namespace Quarrel.Controls.Panels.Messages
         }
 
         public MessagesViewModel ViewModel => (MessagesViewModel)DataContext;
+
+        private void ListView_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListView messageList = (ListView)sender;
+            var scrollViewer = messageList.FindDescendant<ScrollViewer>();
+            scrollViewer.ViewChanged += OnViewChanged;
+        }
+
+        private void OnViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            var viewer = (ScrollViewer)sender;
+            if (viewer.VerticalOffset <= ScrollPadding)
+            {
+                // TODO: Scrolled to top
+            }
+            else if (viewer.VerticalOffset >= viewer.ScrollableHeight - ScrollPadding)
+            {
+                // TODO: Scrolled to bottom
+            }
+        }
     }
 }
