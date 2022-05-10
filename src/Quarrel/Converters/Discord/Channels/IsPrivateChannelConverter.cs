19679e9b27d4dff02312176f65e432e45419ea4a
Adam Dernis <avid29@live.com>
2022-05-10 15:30:19 -0400

    Added converter to only show call button visibility in private channels

---
+++ src/Quarrel/Converters/Discord/Channels/IsPrivateChannelConverter.cs
@@ -0,0 +1,15 @@
+﻿// Quarrel © 2022
+
+using Quarrel.Bindables.Channels.Abstract;
+using Quarrel.Bindables.Channels.Interfaces;
+
+namespace Quarrel.Converters.Discord.Channels
+{
+    public class IsPrivateChannelConverter
+    {
+        public static bool Convert(IBindableChannel channel)
+        {
+            return channel is BindablePrivateChannel;
+        }
+    }
+}
