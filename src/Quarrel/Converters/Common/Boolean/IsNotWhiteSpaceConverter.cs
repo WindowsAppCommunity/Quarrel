52c39b901299849f29eff8da86dc4e4b2956999f
Adam Dernis <avid29@live.com>
2022-05-18 05:04:32 -0400

    Disabled send box when draft is empty or whitespace

---
+++ src/Quarrel/Converters/Common/Boolean/IsNotWhiteSpaceConverter.cs
@@ -0,0 +1,9 @@
+﻿// Quarrel © 2022
+
+namespace Quarrel.Converters.Common.Boolean
+{
+    public class IsNotWhiteSpaceConverter
+    {
+        public static bool Convert(string? item1) => !string.IsNullOrWhiteSpace(item1);
+    }
+}
