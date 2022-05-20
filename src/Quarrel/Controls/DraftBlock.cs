aa498bdef0dfc120c4b858389d4b88dcbc13214f
Adam Dernis <avid29@live.com>
2022-05-20 07:11:55 -0400

    Added DraftBlock control

---
+++ src/Quarrel/Controls/DraftBlock.cs
@@ -0,0 +1,37 @@
+﻿// Quarrel © 2022
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Quarrel.Controls
+{
+    public sealed class DraftBlock : ContentControl
+    {
+        public static readonly DependencyProperty IsDraftedProperty = DependencyProperty.Register(
+            nameof(IsDrafted), typeof(bool), typeof(DraftBlock), new PropertyMetadata(false, OnIsDraftedUpdated));
+
+        public DraftBlock()
+        {
+            this.DefaultStyleKey = typeof(DraftBlock);
+        }
+
+        public bool IsDrafted
+        {
+            get => (bool)GetValue(IsDraftedProperty);
+            set => SetValue(IsDraftedProperty, value);
+        }
+
+        private static void OnIsDraftedUpdated(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            DraftBlock draftBlock = sender as DraftBlock;
+            if ((bool)args.NewValue)
+            {
+                VisualStateManager.GoToState(draftBlock, "Drafted", true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(draftBlock, "Normal", true);
+            }
+        }
+    }
+}
