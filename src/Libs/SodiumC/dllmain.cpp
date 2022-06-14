89fb74fd943ffc17c45bd5afc92542153e3afc13
Matthew <40522069+matthew4850@users.noreply.github.com>
2022-06-14 01:17:09 +0100

    Add sodium library.

---
+++ src/Libs/SodiumC/dllmain.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "sodium.h"
+
+BOOL APIENTRY DllMain(HMODULE /* hModule */, DWORD ul_reason_for_call, LPVOID /* lpReserved */)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+		if (_sodium_alloc_init() == -1) {
+			return false;
+		}
+		break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
---
+++ src/Libs/SodiumC/pch.h
@@ -0,0 +1,2 @@
+﻿#pragma once
+#include<Windows.h>
