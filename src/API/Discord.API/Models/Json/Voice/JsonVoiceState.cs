86846414e1d8e87fd7fe4e21d32ca28af64e1edb
Matthew <40522069+matthew4850@users.noreply.github.com>
2022-06-15 17:05:48 +0100

    Improve voice state model.

--- src/API/Discord.API/Models/Json/Voice/JsonVoiceState.cs
+++ src/API/Discord.API/Models/Json/Voice/JsonVoiceState.cs
@@ -24,28 +24,28 @@ namespace Discord.API.Models.Json.Voice
         public JsonGuildMember? Member { get; set; }
 
         [JsonPropertyName("session_id")]
-        public string? SessionId { get; set; }
+        public string SessionId { get; set; }
 
         [JsonPropertyName("deaf")]
-        public bool? Deaf { get; set; }
+        public bool Deaf { get; set; }
 
         [JsonPropertyName("mute")]
-        public bool? Mute { get; set; }
+        public bool Mute { get; set; }
 
         [JsonPropertyName("self_deaf")]
-        public bool? SelfDeaf { get; set; }
+        public bool SelfDeaf { get; set; }
 
         [JsonPropertyName("self_mute")]
-        public bool? SelfMute { get; set; }
-
-        [JsonPropertyName("suppress")]
-        public bool? Suppress { get; set; }
+        public bool SelfMute { get; set; }
 
         [JsonPropertyName("self_stream")]
         public bool? SelfStream { get; set; }
 
         [JsonPropertyName("self_video")]
-        public bool? SelfVideo { get; set; }
+        public bool SelfVideo { get; set; }
+
+        [JsonPropertyName("suppress")]
+        public bool Suppress { get; set; }
 
         [JsonPropertyName("request_to_speak_timestamp")]
         public DateTimeOffset? RequestToSpeakTimestamp { get; set; }
