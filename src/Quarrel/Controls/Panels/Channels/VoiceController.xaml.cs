94ea8945ee2d11f07177f7ba8745abe123df168a
Adam Dernis <avid29@live.com>
2022-06-15 16:58:54 -0400

    Added VoiceController UI component

---
+++ src/Quarrel/Controls/Panels/Channels/VoiceController.xaml.cs
@@ -0,0 +1,19 @@
+﻿// Quarrel © 2022
+
+using Microsoft.Extensions.DependencyInjection;
+using Quarrel.ViewModels.Panels;
+using Windows.UI.Xaml.Controls;
+
+namespace Quarrel.Controls.Panels.Channels
+{
+    public sealed partial class VoiceController : UserControl
+    {
+        public VoiceController()
+        {
+            this.InitializeComponent();
+            DataContext = App.Current.Services.GetRequiredService<VoiceViewModel>();
+        }
+
+        public VoiceViewModel ViewModel => (VoiceViewModel)DataContext;
+    }
+}
