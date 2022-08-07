0b506a39fab31f368ff9141f8c1785d1a85caf76
Adam Dernis <avid29@live.com>
2022-08-07 09:18:40 -0400

    Emoji templates

---
+++ src/Quarrel/Selectors/Messages/EmojiTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿// Quarrel © 2022
+
+using Quarrel.Client.Models.Emojis;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Quarrel.Selectors.Messages
+{
+    public class EmojiTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate? EmojiTemplate { get; set; }
+
+        public DataTemplate? DiscordEmojiTemplate { get; set; }
+
+        protected override DataTemplate? SelectTemplateCore(object item, DependencyObject container)
+        {
+            return item is DiscordEmoji ? DiscordEmojiTemplate : EmojiTemplate;
+        }
+    }
+}
